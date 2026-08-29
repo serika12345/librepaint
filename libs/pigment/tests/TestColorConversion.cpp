efa2ca096eddfbf6012eb05ab53df41ca0a48ccc
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-08-29 22:29:09 +0900

    Preserve color conversion behavior with focused contracts

--- libs/pigment/tests/TestColorConversion.cpp
+++ libs/pigment/tests/TestColorConversion.cpp
@@ -5,7 +5,36 @@
 #include "TestColorConversion.h"
 #include "KoColorConversions.h"
 
-#include <simpletest.h>
+#include <QTest>
+
+namespace
+{
+
+bool isClose(qreal actual, qreal expected, qreal tolerance = 1e-6)
+{
+    return qAbs(actual - expected) <= tolerance;
+}
+
+} // namespace
+
+void TestColorConversion::testIntegerRGBHSV()
+{
+    int h = 0;
+    int s = 0;
+    int v = 0;
+    rgb_to_hsv(255, 0, 0, &h, &s, &v);
+    QCOMPARE(h, 0);
+    QCOMPARE(s, 255);
+    QCOMPARE(v, 255);
+
+    int r = 0;
+    int g = 0;
+    int b = 0;
+    hsv_to_rgb(h, s, v, &r, &g, &b);
+    QCOMPARE(r, 255);
+    QCOMPARE(g, 0);
+    QCOMPARE(b, 0);
+}
 
 void TestColorConversion::testRGBHSV()
 {
@@ -195,6 +224,179 @@ void TestColorConversion::testRGBHSL()
     QCOMPARE(r, 0.5f);
     QCOMPARE(g, 0.25f);
     QCOMPARE(b, 0.75f);
+}
+
+void TestColorConversion::testFloatingHLS()
+{
+    float h = 0.0f;
+    float l = 0.0f;
+    float s = 0.0f;
+    rgb_to_hls(255, 0, 0, &h, &l, &s);
+    QCOMPARE(h, 0.0f);
+    QCOMPARE(l, 0.5f);
+    QCOMPARE(s, 1.0f);
+
+    QCOMPARE(hue_value(0.2f, 0.8f, 420.0f), 0.8f);
+
+    quint8 r = 0;
+    quint8 g = 0;
+    quint8 b = 0;
+    hls_to_rgb(h, l, s, &r, &g, &b);
+    QCOMPARE(r, quint8(255));
+    QCOMPARE(g, quint8(0));
+    QCOMPARE(b, quint8(0));
+}
+
+void TestColorConversion::testIntegerHLS()
+{
+    int h = 0;
+    int l = 0;
+    int s = 0;
+    rgb_to_hls(128, 128, 128, &h, &l, &s);
+    QCOMPARE(h, 0);
+    QCOMPARE(l, 128);
+    QCOMPARE(s, 0);
+
+    quint8 r = 0;
+    quint8 g = 0;
+    quint8 b = 0;
+    hls_to_rgb(h, l, s, &r, &g, &b);
+    QCOMPARE(r, quint8(128));
+    QCOMPARE(g, quint8(128));
+    QCOMPARE(b, quint8(128));
+}
+
+void TestColorConversion::testHSI()
+{
+    qreal h = 0.0;
+    qreal s = 0.0;
+    qreal i = 0.0;
+    RGBToHSI(0.2, 0.4, 0.6, &h, &s, &i);
+
+    qreal r = 0.0;
+    qreal g = 0.0;
+    qreal b = 0.0;
+    HSIToRGB(h, s, i, &r, &g, &b);
+    QVERIFY(isClose(r, 0.2));
+    QVERIFY(isClose(g, 0.4));
+    QVERIFY(isClose(b, 0.6));
+}
+
+void TestColorConversion::testHSY()
+{
+    qreal h = 0.0;
+    qreal s = 0.0;
+    qreal y = 0.0;
+    RGBToHSY(0.2, 0.4, 0.6, &h, &s, &y);
+
+    qreal r = 0.0;
+    qreal g = 0.0;
+    qreal b = 0.0;
+    HSYToRGB(h, s, y, &r, &g, &b);
+    QVERIFY(isClose(r, 0.2));
+    QVERIFY(isClose(g, 0.4));
+    QVERIFY(isClose(b, 0.6));
+}
+
+void TestColorConversion::testHCI()
+{
+    qreal h = 0.0;
+    qreal c = 0.0;
+    qreal i = 0.0;
+    RGBToHCI(1.0, 0.0, 0.0, &h, &c, &i);
+    QCOMPARE(h, 0.0);
+    QCOMPARE(c, 1.0);
+    QVERIFY(isClose(i, 1.0 / 3.0));
+
+    qreal r = 0.0;
+    qreal g = 0.0;
+    qreal b = 0.0;
+    HCIToRGB(h, c, i, &r, &g, &b);
+    QVERIFY(isClose(r, 1.0));
+    QVERIFY(isClose(g, 0.0));
+    QVERIFY(isClose(b, 0.0));
+}
+
+void TestColorConversion::testHCY()
+{
+    qreal h = 0.0;
+    qreal c = 0.0;
+    qreal y = 0.0;
+    RGBToHCY(0.2, 0.4, 0.6, &h, &c, &y);
+
+    qreal r = 0.0;
+    qreal g = 0.0;
+    qreal b = 0.0;
+    HCYToRGB(h, c, y, &r, &g, &b);
+    QVERIFY(isClose(r, 0.2));
+    QVERIFY(isClose(g, 0.4));
+    QVERIFY(isClose(b, 0.6));
+}
+
+void TestColorConversion::testYUV()
+{
+    qreal y = 0.0;
+    qreal u = 0.0;
+    qreal v = 0.0;
+    RGBToYUV(0.2, 0.4, 0.6, &y, &u, &v);
+
+    qreal r = 0.0;
+    qreal g = 0.0;
+    qreal b = 0.0;
+    YUVToRGB(y, u, v, &r, &g, &b);
+    QVERIFY(isClose(r, 0.2));
+    QVERIFY(isClose(g, 0.4));
+    QVERIFY(isClose(b, 0.6));
+}
+
+void TestColorConversion::testLabLCH()
+{
+    qreal L = 0.0;
+    qreal C = 0.0;
+    qreal H = 0.0;
+    LabToLCH(0.4, 0.3, 0.6, &L, &C, &H);
+
+    qreal l = 0.0;
+    qreal a = 0.0;
+    qreal b = 0.0;
+    LCHToLab(L, C, H, &l, &a, &b);
+    QVERIFY(isClose(l, 0.4));
+    QVERIFY(isClose(a, 0.3));
+    QVERIFY(isClose(b, 0.6));
+}
+
+void TestColorConversion::testXYZxyY()
+{
+    qreal x = 0.0;
+    qreal y = 0.0;
+    qreal yY = 0.0;
+    XYZToxyY(0.2, 0.3, 0.5, &x, &y, &yY);
+
+    qreal X = 0.0;
+    qreal Y = 0.0;
+    qreal Z = 0.0;
+    xyYToXYZ(x, y, yY, &X, &Y, &Z);
+    QVERIFY(isClose(X, 0.2));
+    QVERIFY(isClose(Y, 0.3));
+    QVERIFY(isClose(Z, 0.5));
+}
+
+void TestColorConversion::testCMYCMYK()
+{
+    qreal c = 0.2;
+    qreal m = 0.4;
+    qreal y = 0.6;
+    qreal k = 0.0;
+    CMYToCMYK(&c, &m, &y, &k);
+    QVERIFY(isClose(c, 0.0));
+    QVERIFY(isClose(m, 0.25));
+    QVERIFY(isClose(y, 0.5));
+    QVERIFY(isClose(k, 0.2));
+
+    CMYKToCMY(&c, &m, &y, &k);
+    QVERIFY(isClose(c, 0.2));
+    QVERIFY(isClose(m, 0.4));
+    QVERIFY(isClose(y, 0.6));
 }
 
 QTEST_GUILESS_MAIN(TestColorConversion)
