f9b0a7ef47cbabc2626a56a81e16f307ae72851c
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-08-29 17:06:37 +0900

    Keep highlighted buttons aligned with parent colors

--- libs/widgetutils/KisHighlightedToolButton.h
+++ libs/widgetutils/KisHighlightedToolButton.h
@@ -18,6 +18,9 @@ public:
     KisHighlightedToolButton(QWidget *parent = 0)
         : QToolButton(parent)
     {
+        if (parent) {
+            parent->installEventFilter(this);
+        }
     }
 
     void changeEvent(QEvent *event) override {
@@ -39,6 +42,13 @@ protected:
     }
 
 private:
+    bool eventFilter(QObject *watched, QEvent *event) override {
+        if (watched == parentWidget() && event->type() == QEvent::PaletteChange) {
+            updatePalette();
+        }
+        return QToolButton::eventFilter(watched, event);
+    }
+
     void updatePalette() {
         QWidget *parent = this->parentWidget();
         if (parent) {
---
+++ libs/widgetutils/tests/KisHighlightedToolButtonContractTest.cpp
@@ -0,0 +1,105 @@
+/*
+ * SPDX-FileCopyrightText: 2026 LibrePaint contributors
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "KisHighlightedToolButton.h"
+
+#include <QCoreApplication>
+#include <QTest>
+#include <QWidget>
+
+namespace
+{
+QPalette paletteWithButtonColors(const QColor &button, const QColor &highlight)
+{
+    QPalette palette;
+    palette.setColor(QPalette::Button, button);
+    palette.setColor(QPalette::Highlight, highlight);
+    return palette;
+}
+} // namespace
+
+class KisHighlightedToolButtonContractTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void checkStateSelectsParentPaletteColor();
+    void ownPaletteChangeRestoresCurrentParentColor();
+    void parentPaletteChangeRefreshesCurrentState_data();
+    void parentPaletteChangeRefreshesCurrentState();
+};
+
+void KisHighlightedToolButtonContractTest::checkStateSelectsParentPaletteColor()
+{
+    const QColor buttonColor(31, 47, 59);
+    const QColor highlightColor(83, 109, 137);
+    QWidget parent;
+    parent.setPalette(paletteWithButtonColors(buttonColor, highlightColor));
+
+    KisHighlightedToolButton button(&parent);
+    button.setCheckable(true);
+
+    QCOMPARE(button.palette().color(QPalette::Button), buttonColor);
+
+    button.setChecked(true);
+    QCOMPARE(button.palette().color(QPalette::Button), highlightColor);
+
+    button.setChecked(false);
+    QCOMPARE(button.palette().color(QPalette::Button), buttonColor);
+}
+
+void KisHighlightedToolButtonContractTest::ownPaletteChangeRestoresCurrentParentColor()
+{
+    const QColor buttonColor(37, 53, 71);
+    const QColor highlightColor(89, 107, 131);
+    QWidget parent;
+    parent.setPalette(paletteWithButtonColors(buttonColor, highlightColor));
+
+    KisHighlightedToolButton button(&parent);
+    button.setCheckable(true);
+    button.setChecked(true);
+
+    QPalette unrelatedPalette;
+    unrelatedPalette.setColor(QPalette::Button, QColor(211, 223, 227));
+    button.setPalette(unrelatedPalette);
+
+    QCOMPARE(button.palette().color(QPalette::Button), highlightColor);
+}
+
+void KisHighlightedToolButtonContractTest::parentPaletteChangeRefreshesCurrentState_data()
+{
+    QTest::addColumn<bool>("checked");
+
+    QTest::newRow("unchecked") << false;
+    QTest::newRow("checked") << true;
+}
+
+void KisHighlightedToolButtonContractTest::parentPaletteChangeRefreshesCurrentState()
+{
+    QFETCH(bool, checked);
+
+    const QColor initialButtonColor(17, 29, 43);
+    const QColor initialHighlightColor(61, 79, 101);
+    const QColor updatedButtonColor(113, 127, 149);
+    const QColor updatedHighlightColor(167, 181, 199);
+    QWidget parent;
+    parent.setPalette(paletteWithButtonColors(initialButtonColor, initialHighlightColor));
+
+    KisHighlightedToolButton button(&parent);
+    button.setCheckable(true);
+    button.setChecked(!checked);
+    button.setChecked(checked);
+
+    QCOMPARE(button.palette().color(QPalette::Button), checked ? initialHighlightColor : initialButtonColor);
+
+    parent.setPalette(paletteWithButtonColors(updatedButtonColor, updatedHighlightColor));
+    QCoreApplication::processEvents();
+
+    QCOMPARE(button.palette().color(QPalette::Button), checked ? updatedHighlightColor : updatedButtonColor);
+}
+
+QTEST_MAIN(KisHighlightedToolButtonContractTest)
+
+#include "KisHighlightedToolButtonContractTest.moc"
