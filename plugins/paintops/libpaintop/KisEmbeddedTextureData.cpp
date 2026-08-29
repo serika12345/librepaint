6f502b8ed7e319ed2406a77a756952709942f415
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-08-29 15:41:40 +0900

    Preserve texture option persistence behavior

--- plugins/paintops/libpaintop/KisEmbeddedTextureData.cpp
+++ plugins/paintops/libpaintop/KisEmbeddedTextureData.cpp
@@ -6,7 +6,6 @@
 #include "KisEmbeddedTextureData.h"
 
 #include <QFileInfo>
-#include <kis_properties_configuration.h>
 #include <KoResourceLoadResult.h>
 #include <KisResourcesInterface.h>
 
@@ -78,23 +77,4 @@ KoResourceLoadResult KisEmbeddedTextureData::loadLinkedPattern(KisResourcesInter
     }
 
     return result;
-}
-
-bool KisEmbeddedTextureData::read(const KisPropertiesConfiguration *setting)
-{
-    md5Base64 = setting->getString("Texture/Pattern/PatternMD5");
-    md5sum = setting->getString("Texture/Pattern/PatternMD5Sum");
-    fileName = QFileInfo(setting->getString("Texture/Pattern/PatternFileName")).fileName();
-    name = setting->getString("Texture/Pattern/Name");
-    patternBase64 = setting->getString("Texture/Pattern/Pattern");
-
-    return true;
-}
-
-void KisEmbeddedTextureData::write(KisPropertiesConfiguration *setting) const
-{
-    setting->setProperty("Texture/Pattern/PatternMD5", md5Base64);
-    setting->setProperty("Texture/Pattern/PatternMD5Sum", md5sum);
-    setting->setProperty("Texture/Pattern/PatternFileName", fileName);
-    setting->setProperty("Texture/Pattern/Name", name);
 }
