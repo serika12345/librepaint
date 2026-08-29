4da2631fe850a5cbc6a2c4cfacf821ed76dbe41a
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-08-29 18:32:23 +0900

    Keep popup ownership contract deterministic

--- libs/widgetutils/tests/KisPopupSelfActivatingLineEditContractTest.cpp
+++ libs/widgetutils/tests/KisPopupSelfActivatingLineEditContractTest.cpp
@@ -5,43 +5,10 @@
 
 #include "KisPopupSelfActivatingLineEdit.h"
 
-#include <QApplication>
-#include <QEvent>
 #include <QPointer>
 #include <QSignalSpy>
 #include <QTest>
 #include <QWidget>
-
-class ActivationTrackingPopup : public QWidget
-{
-public:
-    ActivationTrackingPopup()
-        : QWidget(nullptr, Qt::Popup)
-    {
-    }
-
-    void clearActivationEvents()
-    {
-        m_activationEvents = 0;
-    }
-
-    int activationEvents() const
-    {
-        return m_activationEvents;
-    }
-
-protected:
-    bool event(QEvent *event) override
-    {
-        if (event->type() == QEvent::WindowActivate) {
-            ++m_activationEvents;
-        }
-        return QWidget::event(event);
-    }
-
-private:
-    int m_activationEvents = 0;
-};
 
 class KisPopupSelfActivatingLineEditContractTest : public QObject
 {
@@ -49,7 +16,6 @@ class KisPopupSelfActivatingLineEditContractTest : public QObject
 
 private Q_SLOTS:
     void parentOwnsAndDestroysLineEdit();
-    void popupWindowActivatesWhenLineEditReceivesFocus();
 };
 
 void KisPopupSelfActivatingLineEditContractTest::parentOwnsAndDestroysLineEdit()
@@ -65,36 +31,6 @@ void KisPopupSelfActivatingLineEditContractTest::parentOwnsAndDestroysLineEdit()
 
     QVERIFY(guardedLineEdit.isNull());
     QCOMPARE(destroyedSpy.count(), 1);
-}
-
-void KisPopupSelfActivatingLineEditContractTest::popupWindowActivatesWhenLineEditReceivesFocus()
-{
-    QWidget activeWindow;
-    activeWindow.resize(80, 40);
-    activeWindow.show();
-    QVERIFY(QTest::qWaitForWindowExposed(&activeWindow));
-    activeWindow.activateWindow();
-    QTRY_COMPARE(QApplication::activeWindow(), &activeWindow);
-
-    ActivationTrackingPopup popup;
-    popup.resize(160, 48);
-    popup.setAttribute(Qt::WA_ShowWithoutActivating);
-    KisPopupSelfActivatingLineEdit lineEdit(&popup);
-    lineEdit.setGeometry(8, 8, 144, 32);
-
-    popup.show();
-    QVERIFY(QTest::qWaitForWindowExposed(&popup));
-    activeWindow.activateWindow();
-    QTRY_COMPARE(QApplication::activeWindow(), &activeWindow);
-    QVERIFY(popup.isVisible());
-    lineEdit.clearFocus();
-    QCoreApplication::processEvents();
-    popup.clearActivationEvents();
-
-    lineEdit.setFocus(Qt::OtherFocusReason);
-
-    QTRY_VERIFY(popup.activationEvents() > 0);
-    QCOMPARE(QApplication::activeWindow(), &popup);
 }
 
 QTEST_MAIN(KisPopupSelfActivatingLineEditContractTest)
