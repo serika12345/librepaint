caba9245070382a1fe475ed172d373f2711771cb
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-08-30 19:59:38 +0900

    フォント資源所有権の契約を軽量化

--- libs/flake/tests/TestFontLibraryResourceUtils.h
+++ libs/flake/tests/TestFontLibraryResourceUtils.h
@@ -1,5 +1,6 @@
 /*
  *  SPDX-FileCopyrightText: 2017 Dmitry Kazakov <dimula73@gmail.com>
+ *  SPDX-FileCopyrightText: 2026 LibrePaint contributors
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
  */
@@ -8,20 +9,18 @@
 #define TESTLIBRARYRESOURCEUTILS_H
 
 #include <QObject>
-#include <simpletest.h>
 
 class TestFontLibraryResourceUtils : public QObject
 {
     Q_OBJECT
 private Q_SLOTS:
-    void initTestCase();
-    void testCreation();
-    void testCopy();
-    void testCopyAssignment();
-    void testMove();
-    void testReset();
-    void testAssignNull();
-    void testFailedDestruction();
+    void nullPointersDoNotInvokeTheDeleter();
+    void copyAssignmentAndMoveReleaseAtTheFinalOwner();
+    void resetReleasesThePreviousAndCurrentResources();
+    void sanityCheckedDeleterReportsOnlyFailures();
+    void fontconfigAliasesKeepTheirDestroyFunctions();
+    void freeTypeAliasesKeepTheirCheckedDestroyFunctions();
+    void harfBuzzAliasesKeepTheirDestroyFunctions();
 };
 
 #endif // TESTLIBRARYRESOURCEUTILS_H
