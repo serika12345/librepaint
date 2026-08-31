9a69b934eebfeaeaccf417278769e5ea6681b57c
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-01 05:36:43 +0900

    時間範囲の値契約を限定対象へ分離

--- libs/image/kis_time_span.cpp
+++ libs/image/kis_time_span.cpp
@@ -7,9 +7,6 @@
 #include "kis_time_span.h"
 
 #include <QDebug>
-#include "kis_keyframe_channel.h"
-#include "kis_node.h"
-#include "kis_layer_utils.h"
 #include <KisStaticInitializer.h>
 
 KIS_DECLARE_STATIC_INITIALIZER {
@@ -21,75 +18,6 @@ QDebug operator<<(QDebug dbg, const KisTimeSpan &r)
     dbg.nospace() << "KisTimeSpan(" << r.start() << ", " << r.end() << ")";
 
     return dbg.space();
-}
-
-KisTimeSpan KisTimeSpan::calculateIdenticalFramesRecursive(const KisNode *node, int time)
-{
-    KisTimeSpan range = KisTimeSpan::infinite(0);
-
-    KisLayerUtils::recursiveApplyNodes(node,
-        [&range, time] (const KisNode *node) {
-            if (node->visible()) {
-                range &= calculateNodeIdenticalFrames(node, time);
-            }
-    });
-
-    return range;
-}
-
-KisTimeSpan KisTimeSpan::calculateAffectedFramesRecursive(const KisNode *node, int time)
-{
-    KisTimeSpan range;
-
-    KisLayerUtils::recursiveApplyNodes(node,
-        [&range, time] (const KisNode *node) {
-            if (node->visible()) {
-                range |= calculateNodeIdenticalFrames(node, time);
-            }
-    });
-
-    return range;
-}
-
-KisTimeSpan KisTimeSpan::calculateNodeIdenticalFrames(const KisNode *node, int time)
-{
-    KisTimeSpan range = KisTimeSpan::infinite(0);
-
-    const QMap<QString, KisKeyframeChannel*> channels =
-        node->keyframeChannels();
-
-    Q_FOREACH (const KisKeyframeChannel *channel, channels) {
-        // Intersection
-        range &= channel->identicalFrames(time);
-    }
-
-    return range;
-}
-
-KisTimeSpan KisTimeSpan::calculateNodeAffectedFrames(const KisNode *node, int time)
-{
-    KisTimeSpan range;
-
-    if (!node->visible()) return range;
-
-    const QMap<QString, KisKeyframeChannel*> channels =
-        node->keyframeChannels();
-
-    // TODO: channels should report to the image which channel exactly has changed
-    //       to avoid the dirty range to be stretched into infinity!
-
-    if (channels.isEmpty() ||
-        !channels.contains(KisKeyframeChannel::Raster.id())) {
-        range = KisTimeSpan::infinite(0);
-        return range;
-    }
-
-    Q_FOREACH (const KisKeyframeChannel *channel, channels) {
-        // Union
-        range |= channel->affectedFrames(time);
-    }
-
-    return range;
 }
 
 namespace KisDomUtils {
