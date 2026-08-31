84a47eff9dd86c113a039e73bb2ed68acdb26e4b
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-01 03:43:20 +0900

    Fix dialog state and signal behavior

--- libs/widgets/tests/KoDialogEnumContractTest.cpp
+++ libs/widgets/tests/KoDialogEnumContractTest.cpp
@@ -5,7 +5,20 @@
 
 #include <KoDialog.h>
 
+#include <QPointer>
+#include <QPushButton>
+#include <QSignalSpy>
+#include <QStyle>
 #include <QTest>
+#include <QVBoxLayout>
+
+#include <KGuiItem>
+#include <khelpclient.h>
+
+void KHelpClient::invokeHelp(const QString &, const QString &)
+{
+    qFatal("The KoDialog contract must not launch external help");
+}
 
 #include <array>
 #include <utility>
@@ -18,11 +31,16 @@ private Q_SLOTS:
     void buttonCodesRemainStableFlags();
     void popupModesRemainStableFlags();
     void captionFlagsRemainStableAliases();
+    void constructionLayoutCaptionAndLifetimeRemainStable();
+    void buttonStateAndPresentationRoundTrip();
+    void buttonActivationDispatchesStableSignals();
+    void mainDetailsAndHelpStateRemainIndependent();
+    void visibilityLayoutAndDeferredLifetimeTransitions();
 };
 
 void KoDialogEnumContractTest::buttonCodesRemainStableFlags()
 {
-    const std::array<std::pair<KoDialog::ButtonCode, int>, 16> codes {{
+    const std::array<std::pair<KoDialog::ButtonCode, int>, 16> codes{{
         {KoDialog::None, 0x00000000},
         {KoDialog::Help, 0x00000001},
         {KoDialog::Default, 0x00000002},
@@ -73,6 +91,162 @@ void KoDialogEnumContractTest::captionFlagsRemainStableAliases()
     QVERIFY(flags.testFlag(KoDialog::ModifiedCaption));
 }
 
-QTEST_GUILESS_MAIN(KoDialogEnumContractTest)
+void KoDialogEnumContractTest::constructionLayoutCaptionAndLifetimeRemainStable()
+{
+    QWidget parent;
+    QPointer<KoDialog> dialog = new KoDialog(&parent);
+    QCOMPARE(dialog->parentWidget(), &parent);
+    QVERIFY(dialog->mainWidget());
+    QCOMPARE(dialog->mainWidget()->parentWidget(), dialog.data());
+    QVERIFY(!dialog->isDetailsWidgetVisible());
+
+    dialog->setPlainCaption(QStringLiteral("Plain"));
+    QCOMPARE(dialog->windowTitle(), QStringLiteral("Plain"));
+    dialog->setCaption(QStringLiteral("Document"));
+    QVERIFY(dialog->windowTitle().contains(QStringLiteral("Document")));
+    dialog->setCaption(QStringLiteral("Changed"), true);
+    QVERIFY(dialog->windowTitle().contains(QStringLiteral("Changed")));
+    QVERIFY(!KoDialog::makeStandardCaption(QStringLiteral("Caption"), dialog).isEmpty());
+    const int margin = KoDialog::marginHint();
+    const int spacing = KoDialog::spacingHint();
+    const int groupSpacing = KoDialog::groupSpacingHint();
+    QCOMPARE(KoDialog::marginHint(), margin);
+    QCOMPARE(KoDialog::spacingHint(), spacing);
+    QCOMPARE(KoDialog::groupSpacingHint(), groupSpacing);
+
+    dialog->setInitialSize(QSize(120, 80));
+    dialog->incrementInitialSize(QSize(20, 10));
+    QVERIFY(dialog->minimumSizeHint().width() >= 0);
+    QVERIFY(dialog->sizeHint().height() >= 0);
+    delete dialog;
+    QVERIFY(dialog.isNull());
+}
+
+void KoDialogEnumContractTest::buttonStateAndPresentationRoundTrip()
+{
+    KoDialog dialog;
+    dialog.setButtons(KoDialog::Ok | KoDialog::Apply | KoDialog::Cancel | KoDialog::Help);
+    QVERIFY(dialog.button(KoDialog::Ok));
+    QVERIFY(dialog.button(KoDialog::Apply));
+    QVERIFY(dialog.button(KoDialog::Cancel));
+    dialog.setDefaultButton(KoDialog::Apply);
+    QCOMPARE(dialog.defaultButton(), KoDialog::Apply);
+    dialog.setEscapeButton(KoDialog::Cancel);
+    dialog.setButtonFocus(KoDialog::Ok);
+
+    dialog.enableButton(KoDialog::Ok, false);
+    QVERIFY(!dialog.isButtonEnabled(KoDialog::Ok));
+    dialog.enableButtonOk(true);
+    dialog.enableButtonApply(false);
+    dialog.enableButtonCancel(false);
+    QVERIFY(dialog.isButtonEnabled(KoDialog::Ok));
+    QVERIFY(!dialog.isButtonEnabled(KoDialog::Apply));
+    QVERIFY(!dialog.isButtonEnabled(KoDialog::Cancel));
+    dialog.showButton(KoDialog::Apply, false);
+    QVERIFY(dialog.button(KoDialog::Apply)->isHidden());
+
+    const QIcon icon = dialog.style()->standardIcon(QStyle::SP_DialogApplyButton);
+    dialog.setButtonText(KoDialog::Ok, QStringLiteral("Proceed"));
+    dialog.setButtonIcon(KoDialog::Ok, icon);
+    dialog.setButtonToolTip(KoDialog::Ok, QStringLiteral("tip"));
+    dialog.setButtonWhatsThis(KoDialog::Ok, QStringLiteral("what"));
+    QCOMPARE(dialog.buttonText(KoDialog::Ok), QStringLiteral("Proceed"));
+    QCOMPARE(dialog.buttonIcon(KoDialog::Ok).cacheKey(), icon.cacheKey());
+    QCOMPARE(dialog.buttonToolTip(KoDialog::Ok), QStringLiteral("tip"));
+    QCOMPARE(dialog.buttonWhatsThis(KoDialog::Ok), QStringLiteral("what"));
+    dialog.setButtonGuiItem(KoDialog::Ok, KGuiItem(QStringLiteral("Item")));
+    QCOMPARE(dialog.buttonText(KoDialog::Ok), QStringLiteral("Item"));
+    dialog.setButtonsOrientation(Qt::Vertical);
+    dialog.showButtonSeparator(true);
+}
+
+void KoDialogEnumContractTest::buttonActivationDispatchesStableSignals()
+{
+    struct SignalCase {
+        KoDialog::ButtonCode code;
+        const char *signal;
+    };
+    const SignalCase cases[] = {
+        {KoDialog::Apply, SIGNAL(applyClicked())},
+        {KoDialog::Try, SIGNAL(tryClicked())},
+        {KoDialog::Default, SIGNAL(defaultClicked())},
+        {KoDialog::Reset, SIGNAL(resetClicked())},
+        {KoDialog::User1, SIGNAL(user1Clicked())},
+        {KoDialog::User2, SIGNAL(user2Clicked())},
+        {KoDialog::User3, SIGNAL(user3Clicked())},
+        {KoDialog::Help, SIGNAL(helpClicked())},
+        {KoDialog::Ok, SIGNAL(okClicked())},
+        {KoDialog::Yes, SIGNAL(yesClicked())},
+        {KoDialog::No, SIGNAL(noClicked())},
+        {KoDialog::Cancel, SIGNAL(cancelClicked())},
+        {KoDialog::Close, SIGNAL(closeClicked())},
+    };
+    for (const SignalCase &entry : cases) {
+        KoDialog dialog;
+        QSignalSpy specific(&dialog, entry.signal);
+        QSignalSpy general(&dialog, &KoDialog::buttonClicked);
+        QVERIFY(
+            QMetaObject::invokeMethod(&dialog, "slotButtonClicked", Qt::DirectConnection, Q_ARG(int, int(entry.code))));
+        QCOMPARE(specific.count(), 1);
+        QCOMPARE(general.count(), 1);
+        QCOMPARE(general.first().first().toInt(), int(entry.code));
+    }
+}
+
+void KoDialogEnumContractTest::mainDetailsAndHelpStateRemainIndependent()
+{
+    KoDialog dialog;
+    QWidget *main = new QWidget;
+    dialog.setMainWidget(main);
+    QCoreApplication::processEvents();
+    QCOMPARE(dialog.mainWidget(), main);
+    QCOMPARE(main->parentWidget(), &dialog);
+
+    QWidget *details = new QWidget;
+    QSignalSpy detailsSpy(&dialog, &KoDialog::aboutToShowDetails);
+    dialog.setDetailsWidget(details);
+    dialog.setDetailsWidgetVisible(true);
+    QVERIFY(dialog.isDetailsWidgetVisible());
+    QCOMPARE(detailsSpy.count(), 1);
+    dialog.setDetailsWidgetVisible(false);
+    QVERIFY(!dialog.isDetailsWidgetVisible());
+
+    dialog.setHelpLinkText(QStringLiteral("Local help"));
+    QCOMPARE(dialog.helpLinkText(), QStringLiteral("Local help"));
+    dialog.enableLinkedHelp(true);
+    dialog.enableLinkedHelp(false);
+    dialog.setHelp(QString(), QString());
+}
+
+void KoDialogEnumContractTest::visibilityLayoutAndDeferredLifetimeTransitions()
+{
+    QPointer<KoDialog> dialog = new KoDialog;
+    QWidget *content = new QWidget;
+    auto *layout = new QVBoxLayout(content);
+    layout->addWidget(new QWidget(content));
+    KoDialog::resizeLayout(layout, 3, 4);
+    QCOMPARE(layout->contentsMargins().left(), 3);
+    QCOMPARE(layout->spacing(), 4);
+    KoDialog::resizeLayout(content, 5, 6);
+    QCOMPARE(layout->contentsMargins().left(), 5);
+    QCOMPARE(layout->spacing(), 6);
+    dialog->setMainWidget(content);
+
+    QSignalSpy hiddenSpy(dialog, &KoDialog::hidden);
+    QSignalSpy finishedSpy(dialog, &KoDialog::finished);
+    QSignalSpy layoutSpy(dialog, &KoDialog::layoutHintChanged);
+    dialog->show();
+    QCoreApplication::processEvents();
+    dialog->hide();
+    QCoreApplication::processEvents();
+    QCOMPARE(hiddenSpy.count(), 1);
+    QVERIFY(finishedSpy.count() >= 1);
+    QCOMPARE(layoutSpy.count(), 0);
+    dialog->delayedDestruct();
+    QCoreApplication::sendPostedEvents(nullptr, QEvent::DeferredDelete);
+    QVERIFY(dialog.isNull());
+}
+
+QTEST_MAIN(KoDialogEnumContractTest)
 
 #include "KoDialogEnumContractTest.moc"
