4d25d57254755d5cbf7ccf1db3df60f55ac015a4
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-08-31 22:48:08 +0900

    Lock decomposed matrix value semantics

--- libs/global/tests/KisTransformComponentsContractTest.cpp
+++ libs/global/tests/KisTransformComponentsContractTest.cpp
@@ -8,6 +8,7 @@
 #include <QTransform>
 
 #include "KisTransformComponents.h"
+#include "kis_algebra_2d.h"
 
 void kis_assert_exception(const char *, const char *, int)
 {
@@ -54,6 +55,37 @@ QTransform projectiveTransform(qreal x, qreal y)
     return QTransform(1.0, 0.0, x, 0.0, 1.0, y, 0.0, 0.0, 1.0);
 }
 
+QTransform composedTransform(qreal scaleX,
+                             qreal scaleY,
+                             qreal shear,
+                             qreal angle,
+                             qreal dx,
+                             qreal dy,
+                             const QTransform &projective = QTransform())
+{
+    return QTransform::fromScale(scaleX, scaleY) * shearTransform(shear) * rotationTransform(angle)
+        * QTransform::fromTranslate(dx, dy) * projective;
+}
+
+void compareReal(qreal actual, qreal expected, qreal tolerance = 1e-9)
+{
+    QVERIFY2(qAbs(actual - expected) < tolerance,
+             qPrintable(QStringLiteral("actual=%1 expected=%2").arg(actual, 0, 'g', 16).arg(expected, 0, 'g', 16)));
+}
+
+void compareTransform(const QTransform &actual, const QTransform &expected, qreal tolerance = 1e-9)
+{
+    compareReal(actual.m11(), expected.m11(), tolerance);
+    compareReal(actual.m12(), expected.m12(), tolerance);
+    compareReal(actual.m13(), expected.m13(), tolerance);
+    compareReal(actual.m21(), expected.m21(), tolerance);
+    compareReal(actual.m22(), expected.m22(), tolerance);
+    compareReal(actual.m23(), expected.m23(), tolerance);
+    compareReal(actual.m31(), expected.m31(), tolerance);
+    compareReal(actual.m32(), expected.m32(), tolerance);
+    compareReal(actual.m33(), expected.m33(), tolerance);
+}
+
 QString debugText(KisTransformComponent component)
 {
     QString text;
@@ -85,6 +117,11 @@ private Q_SLOTS:
     void transformClassificationDistinguishesEveryComponent();
     void comparisonReportsUnchangedComponents();
     void debugOutputIsStable();
+    void decomposedMatrixDefaultsToValidIdentity();
+    void affineDecompositionExposesEveryComponent();
+    void partialTransformsRecomposeTheAffineInput();
+    void projectiveDecompositionPreservesPerspective();
+    void identityAndDegenerateInputsDefineValidityBoundary();
 };
 
 void KisTransformComponentsContractTest::flagsRepresentEveryTransformComponent()
@@ -151,6 +188,93 @@ void KisTransformComponentsContractTest::debugOutputIsStable()
     QCOMPARE(debugText(KisTransformComponents()), QStringLiteral("KisTransformComponents()"));
     QCOMPARE(debugText(KisAlgebra2D::makeFullTransformComponents()),
              QStringLiteral("KisTransformComponents(Translate | Scale | Rotate | Shear | Project)"));
+}
+
+void KisTransformComponentsContractTest::decomposedMatrixDefaultsToValidIdentity()
+{
+    const KisAlgebra2D::DecomposedMatrix matrix;
+
+    QVERIFY(matrix.isValid());
+    compareReal(matrix.scaleX, 1.0);
+    compareReal(matrix.scaleY, 1.0);
+    compareReal(matrix.shearXY, 0.0);
+    compareReal(matrix.angle, 0.0);
+    compareReal(matrix.dx, 0.0);
+    compareReal(matrix.dy, 0.0);
+    compareReal(matrix.proj[0], 0.0);
+    compareReal(matrix.proj[1], 0.0);
+    compareReal(matrix.proj[2], 1.0);
+
+    compareTransform(matrix.scaleTransform(), QTransform());
+    compareTransform(matrix.shearTransform(), QTransform());
+    compareTransform(matrix.rotateTransform(), QTransform());
+    compareTransform(matrix.translateTransform(), QTransform());
+    compareTransform(matrix.projectTransform(), QTransform());
+    compareTransform(matrix.transform(), QTransform());
+}
+
+void KisTransformComponentsContractTest::affineDecompositionExposesEveryComponent()
+{
+    const QTransform input = composedTransform(2.0, 3.0, 0.25, 30.0, 7.0, -11.0);
+    const KisAlgebra2D::DecomposedMatrix matrix(input);
+
+    QVERIFY(matrix.isValid());
+    compareReal(matrix.scaleX, 2.0);
+    compareReal(matrix.scaleY, 3.0);
+    compareReal(matrix.shearXY, 0.25);
+    compareReal(matrix.angle, 30.0);
+    compareReal(matrix.dx, 7.0);
+    compareReal(matrix.dy, -11.0);
+    compareReal(matrix.proj[0], 0.0);
+    compareReal(matrix.proj[1], 0.0);
+    compareReal(matrix.proj[2], 1.0);
+}
+
+void KisTransformComponentsContractTest::partialTransformsRecomposeTheAffineInput()
+{
+    const QTransform input = composedTransform(2.0, 3.0, 0.25, 30.0, 7.0, -11.0);
+    const KisAlgebra2D::DecomposedMatrix matrix(input);
+
+    compareTransform(matrix.scaleTransform(), QTransform::fromScale(2.0, 3.0));
+    compareTransform(matrix.shearTransform(), shearTransform(0.25));
+    compareTransform(matrix.rotateTransform(), rotationTransform(30.0));
+    compareTransform(matrix.translateTransform(), QTransform::fromTranslate(7.0, -11.0));
+    compareTransform(matrix.projectTransform(), QTransform());
+    compareTransform(matrix.transform(), input);
+}
+
+void KisTransformComponentsContractTest::projectiveDecompositionPreservesPerspective()
+{
+    const QTransform perspective = projectiveTransform(0.01, -0.02);
+    const QTransform input = composedTransform(2.0, 3.0, 0.25, 30.0, 7.0, -11.0, perspective);
+    const KisAlgebra2D::DecomposedMatrix matrix(input);
+
+    QVERIFY(matrix.isValid());
+    compareReal(matrix.scaleX, 2.0);
+    compareReal(matrix.scaleY, 3.0);
+    compareReal(matrix.shearXY, 0.25);
+    compareReal(matrix.angle, 30.0);
+    compareReal(matrix.dx, 7.0);
+    compareReal(matrix.dy, -11.0);
+    compareReal(matrix.proj[0], 0.01);
+    compareReal(matrix.proj[1], -0.02);
+    compareReal(matrix.proj[2], 1.0);
+    compareTransform(matrix.projectTransform(), perspective);
+    compareTransform(matrix.transform(), input);
+}
+
+void KisTransformComponentsContractTest::identityAndDegenerateInputsDefineValidityBoundary()
+{
+    const KisAlgebra2D::DecomposedMatrix identity{QTransform()};
+    QVERIFY(identity.isValid());
+    compareTransform(identity.transform(), QTransform());
+
+    const KisAlgebra2D::DecomposedMatrix zeroDeterminant(QTransform::fromScale(0.0, 1.0));
+    QVERIFY(!zeroDeterminant.isValid());
+
+    const KisAlgebra2D::DecomposedMatrix zeroProjectiveDenominator(
+        QTransform(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0));
+    QVERIFY(!zeroProjectiveDenominator.isValid());
 }
 
 QTEST_GUILESS_MAIN(KisTransformComponentsContractTest)
