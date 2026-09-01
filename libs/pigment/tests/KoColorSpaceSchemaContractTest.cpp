2639c31e0c4b8224fbd9a30cb2bf480880eb52b1
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-01 15:32:09 +0900

    局所画素とアルファ操作の型契約を固定

--- libs/pigment/tests/KoColorSpaceSchemaContractTest.cpp
+++ libs/pigment/tests/KoColorSpaceSchemaContractTest.cpp
@@ -26,6 +26,11 @@ private Q_SLOTS:
     void channelRepresentationSignaturesRemainStable();
     void profileCompatibilityAndIndependencePolicyRemainStable();
     void gamutMetadataSignaturesRemainStable();
+    void pixelOpacityAndTransparencySignaturesRemainStable();
+    void alphaMaskSignaturesRemainStable();
+    void grayBrushColoringSignaturesRemainStable();
+    void pixelToneAdjustmentSignaturesRemainStable();
+    void pixelMeasurementSignaturesRemainStable();
 };
 
 void KoColorSpaceSchemaContractTest::colorSpaceIdentitySignaturesRemainStable()
@@ -87,6 +92,64 @@ void KoColorSpaceSchemaContractTest::gamutMetadataSignaturesRemainStable()
     ASSERT_KO_COLOR_SPACE_SIGNATURE(estimatedTRCXYY, QPolygonF (KoColorSpace::*)() const);
     ASSERT_KO_COLOR_SPACE_SIGNATURE(gamutXYY, QPolygonF (KoColorSpace::*)() const);
     ASSERT_KO_COLOR_SPACE_SIGNATURE(lumaCoefficients, QVector<qreal> (KoColorSpace::*)() const);
+}
+
+void KoColorSpaceSchemaContractTest::pixelOpacityAndTransparencySignaturesRemainStable()
+{
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(copyOpacityU8, void (KoColorSpace::*)(quint8 *, quint8 *, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(multiplyAlpha, void (KoColorSpace::*)(quint8 *, quint8, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(opacityF, qreal (KoColorSpace::*)(const quint8 *) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(opacityU8, quint8 (KoColorSpace::*)(const quint8 *) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(setOpacity, void (KoColorSpace::*)(quint8 *, qreal, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(setOpacity, void (KoColorSpace::*)(quint8 *, quint8, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(transparentColor, void (KoColorSpace::*)(quint8 *, quint32) const);
+}
+
+void KoColorSpaceSchemaContractTest::alphaMaskSignaturesRemainStable()
+{
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(applyAlphaNormedFloatMask,
+                                    void (KoColorSpace::*)(quint8 *, const float *, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(applyAlphaU8Mask, void (KoColorSpace::*)(quint8 *, const quint8 *, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(applyInverseAlphaU8Mask,
+                                    void (KoColorSpace::*)(quint8 *, const quint8 *, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(applyInverseNormedFloatMask,
+                                    void (KoColorSpace::*)(quint8 *, const float *, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(fillInverseAlphaNormedFloatMaskWithColor,
+                                    void (KoColorSpace::*)(quint8 *, const float *, const quint8 *, qint32) const);
+}
+
+void KoColorSpaceSchemaContractTest::grayBrushColoringSignaturesRemainStable()
+{
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(fillGrayBrushWithColor,
+                                    void (KoColorSpace::*)(quint8 *, const QRgb *, quint8 *, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(fillGrayBrushWithColorAndLightnessOverlay,
+                                    void (KoColorSpace::*)(quint8 *, const QRgb *, quint8 *, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(fillGrayBrushWithColorAndLightnessWithStrength,
+                                    void (KoColorSpace::*)(quint8 *, const QRgb *, quint8 *, qreal, qint32) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(modulateLightnessByGrayBrush,
+                                    void (KoColorSpace::*)(quint8 *, const QRgb *, qreal, qint32) const);
+}
+
+void KoColorSpaceSchemaContractTest::pixelToneAdjustmentSignaturesRemainStable()
+{
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(decreaseHue, void (KoColorSpace::*)(quint8 *, qreal) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(decreaseLuminosity, void (KoColorSpace::*)(quint8 *, qreal) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(decreaseSaturation, void (KoColorSpace::*)(quint8 *, qreal) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(increaseBlue, void (KoColorSpace::*)(quint8 *, qreal) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(increaseGreen, void (KoColorSpace::*)(quint8 *, qreal) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(increaseHue, void (KoColorSpace::*)(quint8 *, qreal) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(increaseLuminosity, void (KoColorSpace::*)(quint8 *, qreal) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(increaseRed, void (KoColorSpace::*)(quint8 *, qreal) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(increaseSaturation, void (KoColorSpace::*)(quint8 *, qreal) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(increaseYellow, void (KoColorSpace::*)(quint8 *, qreal) const);
+}
+
+void KoColorSpaceSchemaContractTest::pixelMeasurementSignaturesRemainStable()
+{
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(difference, quint8 (KoColorSpace::*)(const quint8 *, const quint8 *) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(differenceA, quint8 (KoColorSpace::*)(const quint8 *, const quint8 *) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(intensity8, quint8 (KoColorSpace::*)(const quint8 *) const);
+    ASSERT_KO_COLOR_SPACE_SIGNATURE(intensityF, qreal (KoColorSpace::*)(const quint8 *) const);
 }
 
 QTEST_GUILESS_MAIN(KoColorSpaceSchemaContractTest)
