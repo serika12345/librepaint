844177712a3af74dfe30871176844344703c6e74
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-01 17:19:24 +0900

    SVG文字カーソルの公開契約を固定する

--- plugins/tools/svgtexttool/tests/SvgTextCursorEnumContractTest.cpp
+++ plugins/tools/svgtexttool/tests/SvgTextCursorEnumContractTest.cpp
@@ -8,6 +8,14 @@
 #include <QTest>
 
 #include <array>
+#include <type_traits>
+#include <utility>
+
+namespace
+{
+#define ASSERT_SVG_TEXT_CURSOR_SIGNATURE(method, signature)                                                            \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&SvgTextCursor::method)), signature>)
+} // namespace
 
 class SvgTextCursorEnumContractTest : public QObject
 {
@@ -16,6 +24,11 @@ class SvgTextCursorEnumContractTest : public QObject
 private Q_SLOTS:
     void movementValuesRemainStable();
     void typeSettingHandleValuesRemainStable();
+    void cursorPositionAndNavigationSignaturesRemainStable();
+    void cursorSelectionSignaturesRemainStable();
+    void cursorEditingSignaturesRemainStable();
+    void cursorInputAndFocusSignaturesRemainStable();
+    void cursorEditingStateSignaturesRemainStable();
 };
 
 void SvgTextCursorEnumContractTest::movementValuesRemainStable()
@@ -67,6 +80,69 @@ void SvgTextCursorEnumContractTest::typeSettingHandleValuesRemainStable()
     for (std::size_t index = 0; index < handles.size(); ++index) {
         QCOMPARE(int(handles[index]), int(index));
     }
+}
+
+void SvgTextCursorEnumContractTest::cursorPositionAndNavigationSignaturesRemainStable()
+{
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(getPos, int (SvgTextCursor::*)());
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(getAnchor, int (SvgTextCursor::*)());
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(setPos, void (SvgTextCursor::*)(int, int));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(setPosToPoint, void (SvgTextCursor::*)(QPointF, bool));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(moveCursor, void (SvgTextCursor::*)(SvgTextCursor::MoveMode, bool));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(setVisualMode, void (SvgTextCursor::*)(bool));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(updateModifiers, void (SvgTextCursor::*)(Qt::KeyboardModifiers));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(notifyCursorPosChanged, void (SvgTextCursor::*)(int, int));
+
+    static_assert(
+        std::is_same_v<decltype(std::declval<SvgTextCursor &>().setPosToPoint(std::declval<QPointF>())), void>);
+    static_assert(
+        std::is_same_v<decltype(std::declval<SvgTextCursor &>().moveCursor(std::declval<SvgTextCursor::MoveMode>())),
+                       void>);
+    static_assert(std::is_same_v<decltype(std::declval<SvgTextCursor &>().setVisualMode()), void>);
+}
+
+void SvgTextCursorEnumContractTest::cursorSelectionSignaturesRemainStable()
+{
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(hasSelection, bool (SvgTextCursor::*)());
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(deselectText, void (SvgTextCursor::*)());
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(removeSelection, void (SvgTextCursor::*)());
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(copy, void (SvgTextCursor::*)() const);
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(paste, bool (SvgTextCursor::*)());
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(selectionChanged, void (SvgTextCursor::*)());
+}
+
+void SvgTextCursorEnumContractTest::cursorEditingSignaturesRemainStable()
+{
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(insertText, void (SvgTextCursor::*)(QString));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(insertRichText, void (SvgTextCursor::*)(KoSvgTextShape *, bool));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(removeText,
+                                     void (SvgTextCursor::*)(SvgTextCursor::MoveMode, SvgTextCursor::MoveMode));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(removeLastCodePoint, void (SvgTextCursor::*)());
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(keyPressEvent, void (SvgTextCursor::*)(QKeyEvent *));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(setPasteRichTextByDefault, void (SvgTextCursor::*)(bool));
+
+    static_assert(
+        std::is_same_v<decltype(std::declval<SvgTextCursor &>().insertRichText(static_cast<KoSvgTextShape *>(nullptr))),
+                       void>);
+    static_assert(std::is_same_v<decltype(std::declval<SvgTextCursor &>().setPasteRichTextByDefault()), void>);
+}
+
+void SvgTextCursorEnumContractTest::cursorInputAndFocusSignaturesRemainStable()
+{
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(inputMethodQuery, QVariant (SvgTextCursor::*)(Qt::InputMethodQuery) const);
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(inputMethodEvent, void (SvgTextCursor::*)(QInputMethodEvent *));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(focusIn, void (SvgTextCursor::*)());
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(focusOut, void (SvgTextCursor::*)());
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(setCaretSetting, void (SvgTextCursor::*)(int, int, int, bool));
+
+    static_assert(std::is_same_v<decltype(std::declval<SvgTextCursor &>().setCaretSetting()), void>);
+}
+
+void SvgTextCursorEnumContractTest::cursorEditingStateSignaturesRemainStable()
+{
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(setTypeSettingModeActive, void (SvgTextCursor::*)(bool));
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(isAddingCommand, bool (SvgTextCursor::*)() const);
+    ASSERT_SVG_TEXT_CURSOR_SIGNATURE(notifyMarkupChanged, void (SvgTextCursor::*)());
 }
 
 QTEST_GUILESS_MAIN(SvgTextCursorEnumContractTest)
