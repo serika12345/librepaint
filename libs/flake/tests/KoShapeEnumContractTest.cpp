850189e362a1455dc6e92300d958db77720df9d2
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-02 10:16:17 +0900

    図形factoryの公開スキーマを固定

--- libs/flake/tests/KoShapeEnumContractTest.cpp
+++ libs/flake/tests/KoShapeEnumContractTest.cpp
@@ -4,10 +4,29 @@
  */
 
 #include <KoShape.h>
+#include <KoShapeFactoryBase.h>
 
 #include <QTest>
 
 #include <array>
+#include <type_traits>
+
+namespace
+{
+using ShapeElementList = QList<QPair<QString, QStringList>>;
+
+#define ASSERT_SHAPE_FACTORY_SIGNATURE(method, signature)                                                              \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KoShapeFactoryBase::method)), signature>)
+
+class ShapeFactoryConstructorProbe : public KoShapeFactoryBase
+{
+protected:
+    ShapeFactoryConstructorProbe(const QString &id, const QString &name)
+        : KoShapeFactoryBase(id, name)
+    {
+    }
+};
+} // namespace
 
 class KoShapeEnumContractTest : public QObject
 {
@@ -17,11 +36,16 @@ private Q_SLOTS:
     void changeTypeValuesRemainStable();
     void paintOrderValuesRemainStable();
     void childZOrderValuesRemainStable();
+    void shapeTemplateValueSchemaRemainsStable();
+    void shapeFactoryIdentityAndLifecycleSignaturesRemainStable();
+    void shapeFactoryMetadataSignaturesRemainStable();
+    void shapeFactoryLoadingAndResourceSignaturesRemainStable();
+    void shapeFactoryCreationSignaturesRemainStable();
 };
 
 void KoShapeEnumContractTest::changeTypeValuesRemainStable()
 {
-    const std::array<KoShape::ChangeType, 20> changes {{
+    const std::array<KoShape::ChangeType, 20> changes{{
         KoShape::PositionChanged,
         KoShape::RotationChanged,
         KoShape::ScaleChanged,
@@ -61,6 +85,74 @@ void KoShapeEnumContractTest::childZOrderValuesRemainStable()
     QCOMPARE(int(KoShape::ChildZDefault), 0);
     QCOMPARE(int(KoShape::ChildZParentChild), int(KoShape::ChildZDefault));
     QCOMPARE(int(KoShape::ChildZPassThrough), 1);
+}
+
+void KoShapeEnumContractTest::shapeTemplateValueSchemaRemainsStable()
+{
+    static_assert(std::is_same_v<decltype(KoShapeTemplate::id), QString>);
+    static_assert(std::is_same_v<decltype(KoShapeTemplate::templateId), QString>);
+    static_assert(std::is_same_v<decltype(KoShapeTemplate::name), QString>);
+    static_assert(std::is_same_v<decltype(KoShapeTemplate::family), QString>);
+    static_assert(std::is_same_v<decltype(KoShapeTemplate::toolTip), QString>);
+    static_assert(std::is_same_v<decltype(KoShapeTemplate::iconName), QString>);
+    static_assert(std::is_same_v<decltype(KoShapeTemplate::properties), const KoProperties *>);
+
+    KoShapeTemplate value;
+    QVERIFY(value.id.isEmpty());
+    QVERIFY(value.templateId.isEmpty());
+    QVERIFY(value.name.isEmpty());
+    QVERIFY(value.family.isEmpty());
+    QVERIFY(value.toolTip.isEmpty());
+    QVERIFY(value.iconName.isEmpty());
+    QCOMPARE(value.properties, nullptr);
+
+    value.name = QStringLiteral("source");
+    const auto *propertiesMarker = reinterpret_cast<const KoProperties *>(&value);
+    value.properties = propertiesMarker;
+    KoShapeTemplate copy = value;
+    value.name = QStringLiteral("changed");
+    QCOMPARE(copy.name, QStringLiteral("source"));
+    QCOMPARE(copy.properties, propertiesMarker);
+}
+
+void KoShapeEnumContractTest::shapeFactoryIdentityAndLifecycleSignaturesRemainStable()
+{
+    static_assert(std::is_class_v<KoShapeFactoryBase>);
+    static_assert(std::is_abstract_v<KoShapeFactoryBase>);
+    static_assert(std::is_base_of_v<QObject, KoShapeFactoryBase>);
+    static_assert(std::has_virtual_destructor_v<KoShapeFactoryBase>);
+    static_assert(std::is_abstract_v<ShapeFactoryConstructorProbe>);
+}
+
+void KoShapeEnumContractTest::shapeFactoryMetadataSignaturesRemainStable()
+{
+    ASSERT_SHAPE_FACTORY_SIGNATURE(family, QString (KoShapeFactoryBase::*)() const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(hidden, bool (KoShapeFactoryBase::*)() const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(iconName, QString (KoShapeFactoryBase::*)() const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(id, QString (KoShapeFactoryBase::*)() const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(loadingPriority, int (KoShapeFactoryBase::*)() const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(name, QString (KoShapeFactoryBase::*)() const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(templates, QList<KoShapeTemplate> (KoShapeFactoryBase::*)() const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(toolTip, QString (KoShapeFactoryBase::*)() const);
+}
+
+void KoShapeEnumContractTest::shapeFactoryLoadingAndResourceSignaturesRemainStable()
+{
+    ASSERT_SHAPE_FACTORY_SIGNATURE(newDocumentResourceManager,
+                                   void (KoShapeFactoryBase::*)(KoDocumentResourceManager *) const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(odfElements, ShapeElementList (KoShapeFactoryBase::*)() const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(supports,
+                                   bool (KoShapeFactoryBase::*)(const QDomElement &, KoShapeLoadingContext &) const);
+}
+
+void KoShapeEnumContractTest::shapeFactoryCreationSignaturesRemainStable()
+{
+    ASSERT_SHAPE_FACTORY_SIGNATURE(createDefaultShape,
+                                   KoShape * (KoShapeFactoryBase::*)(KoDocumentResourceManager *) const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(
+        createShape,
+        KoShape * (KoShapeFactoryBase::*)(const KoProperties *, KoDocumentResourceManager *) const);
+    ASSERT_SHAPE_FACTORY_SIGNATURE(createShapeOptionPanels, QList<KoShapeConfigWidgetBase *> (KoShapeFactoryBase::*)());
 }
 
 QTEST_GUILESS_MAIN(KoShapeEnumContractTest)
