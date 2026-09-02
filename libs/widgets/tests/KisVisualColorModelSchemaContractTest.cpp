7ff31e60a3839d5c3e61d1ee7bbe1943aa95eef2
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-02 17:52:45 +0900

    色入力の公開スキーマを固定

--- libs/widgets/tests/KisVisualColorModelSchemaContractTest.cpp
+++ libs/widgets/tests/KisVisualColorModelSchemaContractTest.cpp
@@ -4,10 +4,25 @@
  */
 
 #include "KisVisualColorModel.h"
+#include "kis_color_input.h"
 
 #include <QTest>
 
 #include <type_traits>
+
+namespace KisColorInputContractTypes
+{
+
+class ColorInputConstructionProbe final : public KisColorInput
+{
+public:
+    using KisColorInput::KisColorInput;
+
+protected:
+    QWidget *createInput() override;
+};
+
+} // namespace KisColorInputContractTypes
 
 class KisVisualColorModelSchemaContractTest : public QObject
 {
@@ -19,6 +34,11 @@ private Q_SLOTS:
     void visualColorModelPolicySchemaRemainsStable();
     void visualColorModelConversionAndMutationSchemaRemainsStable();
     void visualColorModelNotificationSchemaRemainsStable();
+    void colorInputTypeAndConstructionSchemaRemainsStable();
+    void colorInputPercentageAndNotificationSchemaRemainsStable();
+    void colorInputNumericValueSchemaRemainsStable();
+    void colorInputHexValueSchemaRemainsStable();
+    void colorInputHsvValueSchemaRemainsStable();
 };
 
 void KisVisualColorModelSchemaContractTest::visualColorModelTypeAndEnumerationSchemaRemainsStable()
@@ -84,6 +104,81 @@ void KisVisualColorModelSchemaContractTest::visualColorModelNotificationSchemaRe
     static_assert(
         std::is_same_v<decltype(&Model::sigChannelValuesChanged), void (Model::*)(const QVector4D &, quint32)>);
     static_assert(std::is_same_v<decltype(&Model::sigNewColor), void (Model::*)(const KoColor &)>);
+}
+
+void KisVisualColorModelSchemaContractTest::colorInputTypeAndConstructionSchemaRemainsStable()
+{
+    using InputProbe = KisColorInputContractTypes::ColorInputConstructionProbe;
+
+    static_assert(std::is_class_v<KisColorInput>);
+    static_assert(std::is_class_v<KisIntegerColorInput>);
+    static_assert(std::is_class_v<KisFloatColorInput>);
+    static_assert(std::is_class_v<KisHexColorInput>);
+    static_assert(std::is_class_v<KisHsvColorInput>);
+    static_assert(std::is_constructible_v<InputProbe,
+                                          QWidget *,
+                                          const KoChannelInfo *,
+                                          KoColor *,
+                                          KoColorDisplayRendererInterface *,
+                                          bool>);
+    static_assert(std::is_constructible_v<KisIntegerColorInput,
+                                          QWidget *,
+                                          const KoChannelInfo *,
+                                          KoColor *,
+                                          KoColorDisplayRendererInterface *,
+                                          bool>);
+    static_assert(std::is_constructible_v<KisFloatColorInput,
+                                          QWidget *,
+                                          const KoChannelInfo *,
+                                          KoColor *,
+                                          KoColorDisplayRendererInterface *,
+                                          bool>);
+    static_assert(
+        std::is_constructible_v<KisHexColorInput, QWidget *, KoColor *, KoColorDisplayRendererInterface *, bool, bool>);
+    static_assert(std::is_constructible_v<InputProbe, QWidget *, const KoChannelInfo *, KoColor *>);
+    static_assert(std::is_constructible_v<KisIntegerColorInput, QWidget *, const KoChannelInfo *, KoColor *>);
+    static_assert(std::is_constructible_v<KisFloatColorInput, QWidget *, const KoChannelInfo *, KoColor *>);
+    static_assert(std::is_constructible_v<KisHexColorInput, QWidget *, KoColor *>);
+    static_assert(std::is_constructible_v<KisHsvColorInput, QWidget *, KoColor *>);
+}
+
+void KisVisualColorModelSchemaContractTest::colorInputPercentageAndNotificationSchemaRemainsStable()
+{
+    using Input = KisColorInput;
+
+    static_assert(std::is_same_v<decltype(&Input::setPercentageWise), void (Input::*)(bool)>);
+    static_assert(std::is_same_v<decltype(&Input::usePercentage), bool (Input::*)() const>);
+    static_assert(std::is_same_v<decltype(&Input::updated), void (Input::*)()>);
+}
+
+void KisVisualColorModelSchemaContractTest::colorInputNumericValueSchemaRemainsStable()
+{
+    static_assert(std::is_same_v<decltype(&KisIntegerColorInput::setValue), void (KisIntegerColorInput::*)(int)>);
+    static_assert(std::is_same_v<decltype(&KisIntegerColorInput::update), void (KisIntegerColorInput::*)()>);
+    static_assert(std::is_same_v<decltype(&KisFloatColorInput::setValue), void (KisFloatColorInput::*)(double)>);
+    static_assert(std::is_same_v<decltype(&KisFloatColorInput::sliderChanged), void (KisFloatColorInput::*)(int)>);
+    static_assert(std::is_same_v<decltype(&KisFloatColorInput::update), void (KisFloatColorInput::*)()>);
+}
+
+void KisVisualColorModelSchemaContractTest::colorInputHexValueSchemaRemainsStable()
+{
+    static_assert(std::is_same_v<decltype(&KisHexColorInput::setValue), void (KisHexColorInput::*)()>);
+    static_assert(std::is_same_v<decltype(&KisHexColorInput::update), void (KisHexColorInput::*)()>);
+}
+
+void KisVisualColorModelSchemaContractTest::colorInputHsvValueSchemaRemainsStable()
+{
+    using Input = KisHsvColorInput;
+
+    static_assert(std::is_same_v<decltype(&Input::setMixMode), void (Input::*)(KisHsvColorSlider::MIX_MODE)>);
+    static_assert(std::is_same_v<decltype(&Input::setHue), void (Input::*)(double)>);
+    static_assert(std::is_same_v<decltype(&Input::setSaturation), void (Input::*)(double)>);
+    static_assert(std::is_same_v<decltype(&Input::setValue), void (Input::*)(double)>);
+    static_assert(std::is_same_v<decltype(&Input::hueSliderChanged), void (Input::*)(int)>);
+    static_assert(std::is_same_v<decltype(&Input::saturationSliderChanged), void (Input::*)(int)>);
+    static_assert(std::is_same_v<decltype(&Input::valueSliderChanged), void (Input::*)(int)>);
+    static_assert(std::is_same_v<decltype(&Input::update), void (Input::*)()>);
+    static_assert(std::is_same_v<decltype(&Input::updated), void (Input::*)()>);
 }
 
 QTEST_GUILESS_MAIN(KisVisualColorModelSchemaContractTest)
