ef64799a549fd8ff71a71ce0a194c0facffd2b62
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-03 18:02:18 +0900

    図形管理の公開契約を固定

--- libs/flake/tests/KoCanvasBaseSchemaContractTest.cpp
+++ libs/flake/tests/KoCanvasBaseSchemaContractTest.cpp
@@ -4,15 +4,19 @@
  */
 
 #include <KoCanvasBase.h>
+#include <KoShapeManager.h>
 
 #include <QTest>
 
 #include <type_traits>
+#include <utility>
 
 namespace
 {
 #define ASSERT_CANVAS_SIGNATURE(method, signature)                                                                     \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&KoCanvasBase::method)), signature>)
+#define ASSERT_SHAPE_MANAGER_SIGNATURE(method, signature)                                                              \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KoShapeManager::method)), signature>)
 
 class CanvasConstructorProbe : public KoCanvasBase
 {
@@ -29,12 +33,114 @@ class KoCanvasBaseSchemaContractTest : public QObject
     Q_OBJECT
 
 private Q_SLOTS:
+    void shapeManagerTypeLifetimeAndRepaintSchemaRemainStable();
+    void shapeManagerPaintJobValueSchemaRemainStable();
+    void shapeManagerPaintOrderingAndRenderingSignaturesRemainStable();
+    void shapeManagerShapeAndSelectionSignaturesRemainStable();
+    void shapeManagerQueryUpdateAndInterfaceSignaturesRemainStable();
     void canvasIdentityAndLifecycleSignaturesRemainStable();
     void canvasControllerAndResourceSignaturesRemainStable();
     void canvasShapeCommandAndObserverSignaturesRemainStable();
     void canvasWidgetAndViewSignaturesRemainStable();
     void canvasGridAndPresentationSignaturesRemainStable();
 };
+
+void KoCanvasBaseSchemaContractTest::shapeManagerTypeLifetimeAndRepaintSchemaRemainStable()
+{
+    static_assert(std::is_class_v<KoShapeManager>);
+    static_assert(std::is_base_of_v<QObject, KoShapeManager>);
+    static_assert(std::has_virtual_destructor_v<KoShapeManager>);
+    static_assert(std::is_constructible_v<KoShapeManager, KoCanvasBase *>);
+    static_assert(std::is_constructible_v<KoShapeManager, KoCanvasBase *, const QList<KoShape *> &>);
+    static_assert(std::is_destructible_v<KoShapeManager>);
+
+    static_assert(std::is_enum_v<KoShapeManager::Repaint>);
+    static_assert(KoShapeManager::PaintShapeOnAdd == 0);
+    static_assert(KoShapeManager::AddWithoutRepaint == 1);
+}
+
+void KoCanvasBaseSchemaContractTest::shapeManagerPaintJobValueSchemaRemainStable()
+{
+    using PaintJob = KoShapeManager::PaintJob;
+
+    static_assert(std::is_class_v<PaintJob>);
+    static_assert(std::is_same_v<PaintJob::ShapesStorage, std::vector<std::unique_ptr<KoShape>>>);
+    static_assert(std::is_same_v<PaintJob::SharedSafeStorage, std::shared_ptr<PaintJob::ShapesStorage>>);
+    static_assert(std::is_default_constructible_v<PaintJob>);
+    static_assert(std::is_constructible_v<PaintJob, QRectF, QRect>);
+    static_assert(std::is_same_v<decltype(static_cast<bool (PaintJob::*)() const>(&PaintJob::isEmpty)),
+                                 bool (PaintJob::*)() const>);
+    static_assert(std::is_same_v<decltype(PaintJob::docUpdateRect), QRectF>);
+    static_assert(std::is_same_v<decltype(PaintJob::viewUpdateRect), QRect>);
+    static_assert(std::is_same_v<decltype(PaintJob::shapes), QList<KoShape *>>);
+    static_assert(std::is_same_v<decltype(PaintJob::allClonedShapes), PaintJob::SharedSafeStorage>);
+}
+
+void KoCanvasBaseSchemaContractTest::shapeManagerPaintOrderingAndRenderingSignaturesRemainStable()
+{
+    using PaintJob = KoShapeManager::PaintJob;
+    using PaintJobsOrder = KoShapeManager::PaintJobsOrder;
+
+    static_assert(std::is_class_v<PaintJobsOrder>);
+    static_assert(std::is_same_v<decltype(PaintJobsOrder::uncroppedViewUpdateRect), QRect>);
+    static_assert(std::is_same_v<decltype(PaintJobsOrder::jobs), QList<PaintJob>>);
+    static_assert(std::is_same_v<decltype(static_cast<void (PaintJobsOrder::*)()>(&PaintJobsOrder::clear)),
+                                 void (PaintJobsOrder::*)()>);
+    static_assert(std::is_same_v<decltype(static_cast<bool (PaintJobsOrder::*)() const>(&PaintJobsOrder::isEmpty)),
+                                 bool (PaintJobsOrder::*)() const>);
+    ASSERT_SHAPE_MANAGER_SIGNATURE(preparePaintJobs, void (KoShapeManager::*)(PaintJobsOrder &, KoShape *));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(paintJob, void (KoShapeManager::*)(QPainter &, const PaintJob &));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(paint, void (KoShapeManager::*)(QPainter &));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(renderSingleShape, void (*)(KoShape *, QPainter &));
+}
+
+void KoCanvasBaseSchemaContractTest::shapeManagerShapeAndSelectionSignaturesRemainStable()
+{
+    ASSERT_SHAPE_MANAGER_SIGNATURE(setShapes,
+                                   void (KoShapeManager::*)(const QList<KoShape *> &, KoShapeManager::Repaint));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(shapes, QList<KoShape *> (KoShapeManager::*)() const);
+    ASSERT_SHAPE_MANAGER_SIGNATURE(topLevelShapes, QList<KoShape *> (KoShapeManager::*)() const);
+    ASSERT_SHAPE_MANAGER_SIGNATURE(addShape, void (KoShapeManager::*)(KoShape *, KoShapeManager::Repaint));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(remove, void (KoShapeManager::*)(KoShape *));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(selection, KoSelection * (KoShapeManager::*)() const);
+    ASSERT_SHAPE_MANAGER_SIGNATURE(selectionChanged, void (KoShapeManager::*)());
+    ASSERT_SHAPE_MANAGER_SIGNATURE(selectionContentChanged, void (KoShapeManager::*)());
+    ASSERT_SHAPE_MANAGER_SIGNATURE(contentChanged, void (KoShapeManager::*)());
+
+    static_assert(
+        std::is_same_v<decltype(std::declval<KoShapeManager &>().setShapes(std::declval<const QList<KoShape *> &>())),
+                       void>);
+    static_assert(
+        std::is_same_v<decltype(std::declval<KoShapeManager &>().addShape(static_cast<KoShape *>(nullptr))), void>);
+}
+
+void KoCanvasBaseSchemaContractTest::shapeManagerQueryUpdateAndInterfaceSignaturesRemainStable()
+{
+    using ShapeInterface = KoShapeManager::ShapeInterface;
+
+    ASSERT_SHAPE_MANAGER_SIGNATURE(explicitlyIssueShapeChangedSignals, void (KoShapeManager::*)());
+    ASSERT_SHAPE_MANAGER_SIGNATURE(shapeAt,
+                                   KoShape * (KoShapeManager::*)(const QPointF &, KoFlake::ShapeSelection, bool));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(shapesAt, QList<KoShape *> (KoShapeManager::*)(const QRectF &, bool, bool));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(update, void (KoShapeManager::*)(const QRectF &, const KoShape *, bool));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(setUpdatesBlocked, void (KoShapeManager::*)(bool));
+    ASSERT_SHAPE_MANAGER_SIGNATURE(updatesBlocked, bool (KoShapeManager::*)() const);
+    ASSERT_SHAPE_MANAGER_SIGNATURE(notifyShapeChanged, void (KoShapeManager::*)(KoShape *));
+    static_assert(std::is_class_v<ShapeInterface>);
+    static_assert(std::is_constructible_v<ShapeInterface, KoShapeManager *>);
+    static_assert(std::is_same_v<decltype(static_cast<void (ShapeInterface::*)(KoShape *)>(
+                                     &ShapeInterface::notifyShapeDestructed)),
+                                 void (ShapeInterface::*)(KoShape *)>);
+    ASSERT_SHAPE_MANAGER_SIGNATURE(shapeInterface, ShapeInterface * (KoShapeManager::*)());
+    ASSERT_SHAPE_MANAGER_SIGNATURE(forwardUpdate, void (KoShapeManager::*)());
+
+    static_assert(
+        std::is_same_v<decltype(std::declval<KoShapeManager &>().shapeAt(std::declval<const QPointF &>())), KoShape *>);
+    static_assert(std::is_same_v<decltype(std::declval<KoShapeManager &>().shapesAt(std::declval<const QRectF &>())),
+                                 QList<KoShape *>>);
+    static_assert(
+        std::is_same_v<decltype(std::declval<KoShapeManager &>().update(std::declval<const QRectF &>())), void>);
+}
 
 void KoCanvasBaseSchemaContractTest::canvasIdentityAndLifecycleSignaturesRemainStable()
 {
