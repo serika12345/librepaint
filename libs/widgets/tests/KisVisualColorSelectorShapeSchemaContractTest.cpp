2caa6587c2aae926fcfd9282341073a95e6d81a1
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-03 11:50:51 +0900

    Add concrete selector shape API schema contracts

--- libs/widgets/tests/KisVisualColorSelectorShapeSchemaContractTest.cpp
+++ libs/widgets/tests/KisVisualColorSelectorShapeSchemaContractTest.cpp
@@ -3,6 +3,8 @@
  */
 
 #include <KisVisualColorSelectorShape.h>
+#include <KisVisualEllipticalSelectorShape.h>
+#include <KisVisualRectangleSelectorShape.h>
 #include <QTest>
 
 #include <type_traits>
@@ -10,6 +12,9 @@
 
 #define ASSERT_SELECTOR_SHAPE_SIGNATURE(method, signature)                                                             \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&KisVisualColorSelectorShape::method)), signature>)
+
+#define ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(type, method, signature)                                              \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&type::method)), signature>)
 
 class SelectorShapeConstructorProbe : public KisVisualColorSelectorShape
 {
@@ -38,7 +43,98 @@ private Q_SLOTS:
     void visualColorSelectorShapeColorAndImageSchemaRemainsStable();
     void visualColorSelectorShapeGeometrySchemaRemainsStable();
     void visualColorSelectorShapePolicyAndNotificationSchemaRemainsStable();
+    void visualConcreteSelectorShapeTypeLifetimeAndConstructionSchemaRemainStable();
+    void visualConcreteSelectorShapeOneDimensionalTypeValuesRemainStable();
+    void visualConcreteSelectorShapeGeometrySignaturesRemainStable();
+    void visualRectangleSelectorShapeModeSignatureRemainsStable();
+    void visualEllipticalSelectorShapeGamutSignaturesRemainStable();
 };
+
+void KisVisualColorSelectorShapeSchemaContractTest::
+    visualConcreteSelectorShapeTypeLifetimeAndConstructionSchemaRemainStable()
+{
+    using Dimensions = KisVisualColorSelectorShape::Dimensions;
+    using RectangleShape = KisVisualRectangleSelectorShape;
+    using EllipticalShape = KisVisualEllipticalSelectorShape;
+
+    static_assert(std::is_class_v<RectangleShape>);
+    static_assert(std::is_base_of_v<KisVisualColorSelectorShape, RectangleShape>);
+    static_assert(std::is_constructible_v<RectangleShape, KisVisualColorSelector *, Dimensions, int, int>);
+    static_assert(std::is_constructible_v<RectangleShape,
+                                          KisVisualColorSelector *,
+                                          Dimensions,
+                                          int,
+                                          int,
+                                          int,
+                                          RectangleShape::singelDTypes>);
+    static_assert(std::has_virtual_destructor_v<RectangleShape>);
+
+    static_assert(std::is_class_v<EllipticalShape>);
+    static_assert(std::is_base_of_v<KisVisualColorSelectorShape, EllipticalShape>);
+    static_assert(std::is_constructible_v<EllipticalShape, KisVisualColorSelector *, Dimensions, int, int>);
+    static_assert(std::is_constructible_v<EllipticalShape,
+                                          KisVisualColorSelector *,
+                                          Dimensions,
+                                          int,
+                                          int,
+                                          int,
+                                          EllipticalShape::singelDTypes>);
+    static_assert(std::has_virtual_destructor_v<EllipticalShape>);
+}
+
+void KisVisualColorSelectorShapeSchemaContractTest::visualConcreteSelectorShapeOneDimensionalTypeValuesRemainStable()
+{
+    using RectangleShape = KisVisualRectangleSelectorShape;
+    using EllipticalShape = KisVisualEllipticalSelectorShape;
+
+    static_assert(std::is_enum_v<RectangleShape::singelDTypes>);
+    static_assert(RectangleShape::vertical == 0);
+    static_assert(RectangleShape::horizontal == 1);
+    static_assert(RectangleShape::border == 2);
+    static_assert(RectangleShape::borderMirrored == 3);
+
+    static_assert(std::is_enum_v<EllipticalShape::singelDTypes>);
+    static_assert(EllipticalShape::border == 0);
+    static_assert(EllipticalShape::borderMirrored == 1);
+}
+
+void KisVisualColorSelectorShapeSchemaContractTest::visualConcreteSelectorShapeGeometrySignaturesRemainStable()
+{
+    using RectangleShape = KisVisualRectangleSelectorShape;
+    using RectangleGeometryQuery = QRect (RectangleShape::*)(QRect);
+    using RectangleBorderSetter = void (RectangleShape::*)(int);
+    using EllipticalShape = KisVisualEllipticalSelectorShape;
+    using EllipticalGeometryQuery = QRect (EllipticalShape::*)(QRect);
+    using EllipticalBorderSetter = void (EllipticalShape::*)(int);
+
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(RectangleShape, getSpaceForSquare, RectangleGeometryQuery);
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(RectangleShape, getSpaceForCircle, RectangleGeometryQuery);
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(RectangleShape, getSpaceForTriangle, RectangleGeometryQuery);
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(RectangleShape, setBorderWidth, RectangleBorderSetter);
+
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(EllipticalShape, getSpaceForSquare, EllipticalGeometryQuery);
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(EllipticalShape, getSpaceForCircle, EllipticalGeometryQuery);
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(EllipticalShape, getSpaceForTriangle, EllipticalGeometryQuery);
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(EllipticalShape, setBorderWidth, EllipticalBorderSetter);
+}
+
+void KisVisualColorSelectorShapeSchemaContractTest::visualRectangleSelectorShapeModeSignatureRemainsStable()
+{
+    using RectangleShape = KisVisualRectangleSelectorShape;
+    using ModeSetter = void (RectangleShape::*)(RectangleShape::singelDTypes);
+
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(RectangleShape, setOneDimensionalType, ModeSetter);
+}
+
+void KisVisualColorSelectorShapeSchemaContractTest::visualEllipticalSelectorShapeGamutSignaturesRemainStable()
+{
+    using EllipticalShape = KisVisualEllipticalSelectorShape;
+    using GamutSupportQuery = bool (EllipticalShape::*)() const;
+    using GamutUpdater = void (EllipticalShape::*)();
+
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(EllipticalShape, supportsGamutMask, GamutSupportQuery);
+    ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE(EllipticalShape, updateGamutMask, GamutUpdater);
+}
 
 void KisVisualColorSelectorShapeSchemaContractTest::visualColorSelectorShapeTypeAndDimensionSchemaRemainsStable()
 {
@@ -92,6 +188,9 @@ void KisVisualColorSelectorShapeSchemaContractTest::visualColorSelectorShapePoli
     ASSERT_SELECTOR_SHAPE_SIGNATURE(setAcceptTabletEvents, void (KisVisualColorSelectorShape::*)(bool));
     ASSERT_SELECTOR_SHAPE_SIGNATURE(sigCursorMoved, void (KisVisualColorSelectorShape::*)(QPointF));
 }
+
+#undef ASSERT_CONCRETE_SELECTOR_SHAPE_SIGNATURE
+#undef ASSERT_SELECTOR_SHAPE_SIGNATURE
 
 QTEST_GUILESS_MAIN(KisVisualColorSelectorShapeSchemaContractTest)
 #include "KisVisualColorSelectorShapeSchemaContractTest.moc"
