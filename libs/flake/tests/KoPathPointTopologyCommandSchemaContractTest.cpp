fa4341d01aa1c95bc5a7b3678e10f24ee8c719f8
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-05 04:29:16 +0900

    パス図形編集コマンドの公開契約を固定

--- libs/flake/tests/KoPathPointTopologyCommandSchemaContractTest.cpp
+++ libs/flake/tests/KoPathPointTopologyCommandSchemaContractTest.cpp
@@ -3,10 +3,14 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include <commands/KoMultiPathPointJoinCommand.h>
 #include <commands/KoMultiPathPointMergeCommand.h>
+#include <commands/KoPathCombineCommand.h>
+#include <commands/KoPathControlPointMoveCommand.h>
 #include <commands/KoPathPointInsertCommand.h>
 #include <commands/KoPathPointMergeCommand.h>
 #include <commands/KoPathPointRemoveCommand.h>
+#include <commands/KoShapeUnclipCommand.h>
 
 #include <QTest>
 
@@ -25,6 +29,12 @@ using PointDataList = QList<KoPathPointData>;
     static_assert(std::is_same_v<decltype(static_cast<signature>(&KoPathPointMergeCommand::method)), signature>)
 #define ASSERT_MULTI_MERGE_SIGNATURE(method, signature)                                                                \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&KoMultiPathPointMergeCommand::method)), signature>)
+#define ASSERT_PATH_COMBINE_SIGNATURE(method, signature)                                                               \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KoPathCombineCommand::method)), signature>)
+#define ASSERT_CONTROL_POINT_MOVE_SIGNATURE(method, signature)                                                         \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KoPathControlPointMoveCommand::method)), signature>)
+#define ASSERT_SHAPE_UNCLIP_SIGNATURE(method, signature)                                                               \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KoShapeUnclipCommand::method)), signature>)
 } // namespace
 
 class KoPathPointTopologyCommandSchemaContractTest : public QObject
@@ -37,6 +47,11 @@ private Q_SLOTS:
     void pathPointInsertionResultSignatureRemainsStable();
     void pathPointRemovalFactorySignatureRemainsStable();
     void pathPointMergeResultSignaturesRemainStable();
+    void pathCombineCommandTypeAndConstructionSchemaRemainStable();
+    void pathCombineExecutionAndCorrespondenceSignaturesRemainStable();
+    void pathControlPointMoveCommandSchemaRemainStable();
+    void multiPathPointJoinCommandSchemaRemainStable();
+    void shapeUnclipCommandSchemaRemainStable();
 };
 
 void KoPathPointTopologyCommandSchemaContractTest::pathPointTopologyCommandTypeAndLifetimeSchemaRemainStable()
@@ -121,6 +136,96 @@ void KoPathPointTopologyCommandSchemaContractTest::pathPointMergeResultSignature
 {
     ASSERT_MERGE_SIGNATURE(mergedPointData, KoPathPointData (KoPathPointMergeCommand::*)() const);
     ASSERT_MULTI_MERGE_SIGNATURE(testingCombinedPath, KoPathShape * (KoMultiPathPointMergeCommand::*)() const);
+}
+
+void KoPathPointTopologyCommandSchemaContractTest::pathCombineCommandTypeAndConstructionSchemaRemainStable()
+{
+    using PathList = QList<KoPathShape *>;
+
+    static_assert(std::is_class_v<KoPathCombineCommand>);
+    static_assert(std::is_base_of_v<KUndo2Command, KoPathCombineCommand>);
+    static_assert(std::is_constructible_v<KoPathCombineCommand, KoShapeControllerBase *, const PathList &>);
+    static_assert(
+        std::is_constructible_v<KoPathCombineCommand, KoShapeControllerBase *, const PathList &, KUndo2Command *>);
+    static_assert(std::is_same_v<decltype(KoPathCombineCommand(std::declval<KoShapeControllerBase *>(),
+                                                               std::declval<const PathList &>())),
+                                 KoPathCombineCommand>);
+    static_assert(std::has_virtual_destructor_v<KoPathCombineCommand>);
+}
+
+void KoPathPointTopologyCommandSchemaContractTest::pathCombineExecutionAndCorrespondenceSignaturesRemainStable()
+{
+    ASSERT_PATH_COMBINE_SIGNATURE(combinedPath, KoPathShape * (KoPathCombineCommand::*)() const);
+    ASSERT_PATH_COMBINE_SIGNATURE(originalToCombined, KoPathPointData (KoPathCombineCommand::*)(KoPathPointData) const);
+    ASSERT_PATH_COMBINE_SIGNATURE(redo, void (KoPathCombineCommand::*)());
+    ASSERT_PATH_COMBINE_SIGNATURE(undo, void (KoPathCombineCommand::*)());
+}
+
+void KoPathPointTopologyCommandSchemaContractTest::pathControlPointMoveCommandSchemaRemainStable()
+{
+    static_assert(std::is_class_v<KoPathControlPointMoveCommand>);
+    static_assert(std::is_base_of_v<KUndo2Command, KoPathControlPointMoveCommand>);
+    static_assert(std::is_constructible_v<KoPathControlPointMoveCommand,
+                                          const KoPathPointData &,
+                                          const QPointF &,
+                                          KoPathPoint::PointType>);
+    static_assert(std::is_constructible_v<KoPathControlPointMoveCommand,
+                                          const KoPathPointData &,
+                                          const QPointF &,
+                                          KoPathPoint::PointType,
+                                          KUndo2Command *>);
+    static_assert(std::is_same_v<decltype(KoPathControlPointMoveCommand(std::declval<const KoPathPointData &>(),
+                                                                        std::declval<const QPointF &>(),
+                                                                        std::declval<KoPathPoint::PointType>())),
+                                 KoPathControlPointMoveCommand>);
+    ASSERT_CONTROL_POINT_MOVE_SIGNATURE(id, int (KoPathControlPointMoveCommand::*)() const);
+    ASSERT_CONTROL_POINT_MOVE_SIGNATURE(mergeWith, bool (KoPathControlPointMoveCommand::*)(const KUndo2Command *));
+    ASSERT_CONTROL_POINT_MOVE_SIGNATURE(redo, void (KoPathControlPointMoveCommand::*)());
+    ASSERT_CONTROL_POINT_MOVE_SIGNATURE(undo, void (KoPathControlPointMoveCommand::*)());
+}
+
+void KoPathPointTopologyCommandSchemaContractTest::multiPathPointJoinCommandSchemaRemainStable()
+{
+    static_assert(std::is_class_v<KoMultiPathPointJoinCommand>);
+    static_assert(std::is_base_of_v<KoMultiPathPointMergeCommand, KoMultiPathPointJoinCommand>);
+    static_assert(std::is_constructible_v<KoMultiPathPointJoinCommand,
+                                          const KoPathPointData &,
+                                          const KoPathPointData &,
+                                          KoShapeControllerBase *,
+                                          KoSelection *>);
+    static_assert(std::is_constructible_v<KoMultiPathPointJoinCommand,
+                                          const KoPathPointData &,
+                                          const KoPathPointData &,
+                                          KoShapeControllerBase *,
+                                          KoSelection *,
+                                          KUndo2Command *>);
+    static_assert(std::is_same_v<decltype(KoMultiPathPointJoinCommand(std::declval<const KoPathPointData &>(),
+                                                                      std::declval<const KoPathPointData &>(),
+                                                                      std::declval<KoShapeControllerBase *>(),
+                                                                      std::declval<KoSelection *>())),
+                                 KoMultiPathPointJoinCommand>);
+}
+
+void KoPathPointTopologyCommandSchemaContractTest::shapeUnclipCommandSchemaRemainStable()
+{
+    using ShapeList = QList<KoShape *>;
+
+    static_assert(std::is_class_v<KoShapeUnclipCommand>);
+    static_assert(std::is_base_of_v<KUndo2Command, KoShapeUnclipCommand>);
+    static_assert(std::is_constructible_v<KoShapeUnclipCommand, KoShapeControllerBase *, KoShape *>);
+    static_assert(std::is_constructible_v<KoShapeUnclipCommand, KoShapeControllerBase *, KoShape *, KUndo2Command *>);
+    static_assert(std::is_same_v<decltype(KoShapeUnclipCommand(std::declval<KoShapeControllerBase *>(),
+                                                               std::declval<KoShape *>())),
+                                 KoShapeUnclipCommand>);
+    static_assert(std::is_constructible_v<KoShapeUnclipCommand, KoShapeControllerBase *, const ShapeList &>);
+    static_assert(
+        std::is_constructible_v<KoShapeUnclipCommand, KoShapeControllerBase *, const ShapeList &, KUndo2Command *>);
+    static_assert(std::is_same_v<decltype(KoShapeUnclipCommand(std::declval<KoShapeControllerBase *>(),
+                                                               std::declval<const ShapeList &>())),
+                                 KoShapeUnclipCommand>);
+    static_assert(std::has_virtual_destructor_v<KoShapeUnclipCommand>);
+    ASSERT_SHAPE_UNCLIP_SIGNATURE(redo, void (KoShapeUnclipCommand::*)());
+    ASSERT_SHAPE_UNCLIP_SIGNATURE(undo, void (KoShapeUnclipCommand::*)());
 }
 
 QTEST_APPLESS_MAIN(KoPathPointTopologyCommandSchemaContractTest)
