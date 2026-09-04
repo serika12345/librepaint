af72932df3cef82fac1b7d2e6d8eb9c5cdf3185d
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-05 03:05:59 +0900

    図形補助状態の公開契約を固定

--- libs/flake/tests/KoShapeStateSchemaContractTest.cpp
+++ libs/flake/tests/KoShapeStateSchemaContractTest.cpp
@@ -4,6 +4,7 @@
  */
 
 #include <KoShape.h>
+#include <KoShapeLayer.h>
 
 #include <QTest>
 
@@ -26,6 +27,11 @@ private Q_SLOTS:
     void shapeProtectionSignaturesRemainStable();
     void shapePresentationPolicySignaturesRemainStable();
     void shapeEditabilityAndReadinessSignaturesRemainStable();
+    void shapeAttachmentOwnershipSignaturesRemainStable();
+    void shapeCloneAndSettingsSignaturesRemainStable();
+    void shapeDelegationAndSnapSignaturesRemainStable();
+    void shapeHandlePainterHelperSignaturesRemainStable();
+    void shapeLayerTypeConstructionAndPaintingSchemaRemainStable();
 };
 
 void KoShapeStateSchemaContractTest::shapeIdentityAndLinkSignaturesRemainStable()
@@ -75,6 +81,65 @@ void KoShapeStateSchemaContractTest::shapeEditabilityAndReadinessSignaturesRemai
 
     static_assert(std::is_same_v<decltype(std::declval<const KoShape &>().isShapeEditable()), bool>);
     static_assert(std::is_same_v<decltype(std::declval<const KoShape &>().waitUntilReady()), void>);
+}
+
+void KoShapeStateSchemaContractTest::shapeAttachmentOwnershipSignaturesRemainStable()
+{
+    ASSERT_SHAPE_STATE_SIGNATURE(anchor, KoShapeAnchor * (KoShape::*)() const);
+    ASSERT_SHAPE_STATE_SIGNATURE(setAnchor, void (KoShape::*)(KoShapeAnchor *));
+    ASSERT_SHAPE_STATE_SIGNATURE(clipMask, KoClipMask * (KoShape::*)() const);
+    ASSERT_SHAPE_STATE_SIGNATURE(setClipMask, void (KoShape::*)(KoClipMask *));
+    ASSERT_SHAPE_STATE_SIGNATURE(clipPath, KoClipPath * (KoShape::*)() const);
+    ASSERT_SHAPE_STATE_SIGNATURE(setClipPath, void (KoShape::*)(KoClipPath *));
+    ASSERT_SHAPE_STATE_SIGNATURE(userData, KoShapeUserData * (KoShape::*)() const);
+    ASSERT_SHAPE_STATE_SIGNATURE(setUserData, void (KoShape::*)(KoShapeUserData *));
+}
+
+void KoShapeStateSchemaContractTest::shapeCloneAndSettingsSignaturesRemainStable()
+{
+    ASSERT_SHAPE_STATE_SIGNATURE(cloneShape, KoShape * (KoShape::*)() const);
+    ASSERT_SHAPE_STATE_SIGNATURE(cloneShapeAndBakeAbsoluteTransform, KoShape * (KoShape::*)() const);
+    ASSERT_SHAPE_STATE_SIGNATURE(copySettings, void (KoShape::*)(const KoShape *));
+}
+
+void KoShapeStateSchemaContractTest::shapeDelegationAndSnapSignaturesRemainStable()
+{
+    ASSERT_SHAPE_STATE_SIGNATURE(setToolDelegates, void (KoShape::*)(const QSet<KoShape *> &));
+    ASSERT_SHAPE_STATE_SIGNATURE(toolDelegates, QSet<KoShape *> (KoShape::*)() const);
+    ASSERT_SHAPE_STATE_SIGNATURE(snapData, KoSnapData (KoShape::*)() const);
+}
+
+void KoShapeStateSchemaContractTest::shapeHandlePainterHelperSignaturesRemainStable()
+{
+    using ViewHelperSignature = KisHandlePainterHelper (*)(QPainter *, KoShape *, const KoViewConverter &, qreal, int);
+    using DocumentHelperSignature = KisHandlePainterHelper (*)(QPainter *, KoShape *, qreal, int);
+
+    static_assert(std::is_same_v<decltype(static_cast<ViewHelperSignature>(&KoShape::createHandlePainterHelperView)),
+                                 ViewHelperSignature>);
+    static_assert(
+        std::is_same_v<decltype(static_cast<DocumentHelperSignature>(&KoShape::createHandlePainterHelperDocument)),
+                       DocumentHelperSignature>);
+    static_assert(
+        std::is_same_v<decltype(KoShape::createHandlePainterHelperView(std::declval<QPainter *>(),
+                                                                       std::declval<KoShape *>(),
+                                                                       std::declval<const KoViewConverter &>())),
+                       KisHandlePainterHelper>);
+}
+
+void KoShapeStateSchemaContractTest::shapeLayerTypeConstructionAndPaintingSchemaRemainStable()
+{
+    static_assert(std::is_class_v<KoShapeLayer>);
+    static_assert(std::is_base_of_v<KoShapeContainer, KoShapeLayer>);
+    static_assert(std::is_constructible_v<KoShapeLayer>);
+    static_assert(std::is_constructible_v<KoShapeLayer, KoShapeContainerModel *>);
+    static_assert(std::is_same_v<decltype(static_cast<QRectF (KoShapeLayer::*)() const>(&KoShapeLayer::boundingRect)),
+                                 QRectF (KoShapeLayer::*)() const>);
+    static_assert(
+        std::is_same_v<decltype(static_cast<bool (KoShapeLayer::*)(const QPointF &) const>(&KoShapeLayer::hitTest)),
+                       bool (KoShapeLayer::*)(const QPointF &) const>);
+    static_assert(
+        std::is_same_v<decltype(static_cast<void (KoShapeLayer::*)(QPainter &) const>(&KoShapeLayer::paintComponent)),
+                       void (KoShapeLayer::*)(QPainter &) const>);
 }
 
 QTEST_APPLESS_MAIN(KoShapeStateSchemaContractTest)
