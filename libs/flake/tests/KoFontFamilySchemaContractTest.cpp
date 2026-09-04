9a5076989133da27a700b28ac8509682781d5ea2
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-04 21:06:51 +0900

    書体解決と保存の公開契約を固定

--- libs/flake/tests/KoFontFamilySchemaContractTest.cpp
+++ libs/flake/tests/KoFontFamilySchemaContractTest.cpp
@@ -4,10 +4,13 @@
  */
 
 #include "resources/KoFontFamily.h"
+#include "resources/KoFontStorage.h"
+#include "text/KoFontRegistry.h"
 
 #include <QTest>
 
 #include <type_traits>
+#include <utility>
 
 namespace
 {
@@ -30,6 +33,11 @@ private Q_SLOTS:
     void fontFamilyAxesAndStylesSignaturesRemainStable();
     void fontFamilyCapabilitySignaturesRemainStable();
     void fontFamilyResourcePersistenceSignaturesRemainStable();
+    void fontRegistryTypeLifetimeAndAccessSchemaRemainStable();
+    void fontRegistryFaceResolutionSignaturesRemainStable();
+    void fontRegistryRepresentationAndMetricsSignaturesRemainStable();
+    void fontStorageTypeLifetimeAndValiditySchemaRemainStable();
+    void fontStorageResourceAccessSignaturesRemainStable();
 };
 
 void KoFontFamilySchemaContractTest::fontFamilyTypeLifetimeAndConstructionSchemaRemainStable()
@@ -73,6 +81,96 @@ void KoFontFamilySchemaContractTest::fontFamilyResourcePersistenceSignaturesRema
     ASSERT_FONT_FAMILY_SIGNATURE(isSerializable, bool (FontFamily::*)() const);
     ASSERT_FONT_FAMILY_SIGNATURE(resourceType, ResourceTypePair (FontFamily::*)() const);
     ASSERT_FONT_FAMILY_SIGNATURE(updateThumbnail, void (FontFamily::*)());
+}
+
+void KoFontFamilySchemaContractTest::fontRegistryTypeLifetimeAndAccessSchemaRemainStable()
+{
+    using InstanceSignature = KoFontRegistry *(*)();
+
+    static_assert(std::is_base_of_v<QObject, KoFontRegistry>);
+    static_assert(std::is_constructible_v<KoFontRegistry, QObject *>);
+    static_assert(std::is_default_constructible_v<KoFontRegistry>);
+    static_assert(std::has_virtual_destructor_v<KoFontRegistry>);
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::instance), InstanceSignature>);
+}
+
+void KoFontFamilySchemaContractTest::fontRegistryFaceResolutionSignaturesRemainStable()
+{
+    using FacesSignature = std::vector<FT_FaceSP> (
+        KoFontRegistry::*)(QVector<int> &, KoCSSFontInfo, const QString &, quint32, quint32, bool, const QString &);
+    using ConfigureSignature = bool (KoFontRegistry::*)(const std::vector<FT_FaceSP> &,
+                                                        qreal,
+                                                        qreal,
+                                                        quint32,
+                                                        quint32,
+                                                        const QMap<QString, qreal> &);
+    using CssDataSignature = KoCSSFontInfo (KoFontRegistry::*)(const QString, QString *);
+    using SlantSignature = QFont::Style (*)(FT_FaceSP);
+    using LoadFlagsSignature = int32_t (*)(FT_Face, bool, int32_t, KoSvgText::TextRendering);
+
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::facesForCSSValues), FacesSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::configureFaces), ConfigureSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::getCssDataForPostScriptName), CssDataSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::slantMode), SlantSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::loadFlagsForFace), LoadFlagsSignature>);
+    static_assert(
+        std::is_same_v<decltype(std::declval<KoFontRegistry &>().facesForCSSValues(std::declval<QVector<int> &>())),
+                       std::vector<FT_FaceSP>>);
+    static_assert(std::is_same_v<decltype(KoFontRegistry::loadFlagsForFace(std::declval<FT_Face>())), int32_t>);
+}
+
+void KoFontFamilySchemaContractTest::fontRegistryRepresentationAndMetricsSignaturesRemainStable()
+{
+    using CollectSignature = QList<KoFontFamilyWWSRepresentation> (KoFontRegistry::*)() const;
+    using RepresentationSignature =
+        std::optional<KoFontFamilyWWSRepresentation> (KoFontRegistry::*)(const QString &) const;
+    using WwsNameSignature = std::optional<QString> (KoFontRegistry::*)(const QString) const;
+    using MetricsSignature = KoSvgText::FontMetrics (KoFontRegistry::*)(KoCSSFontInfo,
+                                                                        bool,
+                                                                        KoSvgText::TextRendering,
+                                                                        const QString &,
+                                                                        quint32,
+                                                                        quint32,
+                                                                        bool,
+                                                                        const QString &);
+    using GenerateMetricsSignature = KoSvgText::FontMetrics (*)(FT_FaceSP, bool, QString, KoSvgText::TextRendering);
+
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::collectRepresentations), CollectSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::representationByFamilyName), RepresentationSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::wwsNameByFamilyName), WwsNameSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::fontMetricsForCSSValues), MetricsSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontRegistry::generateFontMetrics), GenerateMetricsSignature>);
+    static_assert(
+        std::is_same_v<decltype(std::declval<KoFontRegistry &>().fontMetricsForCSSValues()), KoSvgText::FontMetrics>);
+    static_assert(std::is_same_v<decltype(KoFontRegistry::generateFontMetrics(std::declval<FT_FaceSP>())),
+                                 KoSvgText::FontMetrics>);
+}
+
+void KoFontFamilySchemaContractTest::fontStorageTypeLifetimeAndValiditySchemaRemainStable()
+{
+    using ValiditySignature = bool (KoFontStorage::*)() const;
+
+    static_assert(std::is_base_of_v<KisStoragePlugin, KoFontStorage>);
+    static_assert(std::is_constructible_v<KoFontStorage, const QString &>);
+    static_assert(std::is_default_constructible_v<KoFontStorage>);
+    static_assert(std::has_virtual_destructor_v<KoFontStorage>);
+    static_assert(std::is_same_v<decltype(&KoFontStorage::isValid), ValiditySignature>);
+    static_assert(std::is_same_v<decltype(&KoFontStorage::supportsVersioning), ValiditySignature>);
+}
+
+void KoFontFamilySchemaContractTest::fontStorageResourceAccessSignaturesRemainStable()
+{
+    using LoadVersionedSignature = bool (KoFontStorage::*)(KoResourceSP);
+    using ResourceSignature = KoResourceSP (KoFontStorage::*)(const QString &);
+    using ResourceItemSignature = KisResourceStorage::ResourceItem (KoFontStorage::*)(const QString &);
+    using ResourcesSignature = QSharedPointer<KisResourceStorage::ResourceIterator> (KoFontStorage::*)(const QString &);
+    using TagsSignature = QSharedPointer<KisResourceStorage::TagIterator> (KoFontStorage::*)(const QString &);
+
+    static_assert(std::is_same_v<decltype(&KoFontStorage::loadVersionedResource), LoadVersionedSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontStorage::resource), ResourceSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontStorage::resourceItem), ResourceItemSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontStorage::resources), ResourcesSignature>);
+    static_assert(std::is_same_v<decltype(&KoFontStorage::tags), TagsSignature>);
 }
 
 QTEST_APPLESS_MAIN(KoFontFamilySchemaContractTest)
