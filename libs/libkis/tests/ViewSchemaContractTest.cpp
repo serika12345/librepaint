5fb677fa9c80d6c43d8d01c7b66bbb0e192f94ec
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-04 09:02:10 +0900

    Scratchpad操作の公開契約を固定

--- libs/libkis/tests/ViewSchemaContractTest.cpp
+++ libs/libkis/tests/ViewSchemaContractTest.cpp
@@ -2,11 +2,16 @@
  * SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include <QTest>
+#include <Scratchpad.h>
 #include <View.h>
 #include <type_traits>
+#include <utility>
 
 #define ASSERT_VIEW_SIGNATURE(method, signature)                                                                       \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&View::method)), signature>)
+
+#define ASSERT_SCRATCHPAD_SIGNATURE(method, signature)                                                                 \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&Scratchpad::method)), signature>)
 
 class ViewSchemaContractTest : public QObject
 {
@@ -22,6 +27,11 @@ private Q_SLOTS:
     void viewColorStateAndNotificationSignaturesRemainStable();
     void viewCoordinateTransformSignaturesRemainStable();
     void viewInteractionNotificationSignaturesRemainStable();
+    void scratchpadTypeLifetimeAndModeSchemaRemainStable();
+    void scratchpadFillSignaturesRemainStable();
+    void scratchpadZoomAndScaleSignaturesRemainStable();
+    void scratchpadPanImageAndBoundsSignaturesRemainStable();
+    void scratchpadNotificationSignaturesRemainStable();
 };
 
 void ViewSchemaContractTest::viewOwnershipLifetimeAndEqualitySchemaRemainsStable()
@@ -119,6 +129,66 @@ void ViewSchemaContractTest::viewInteractionNotificationSignaturesRemainStable()
     ASSERT_VIEW_SIGNATURE(currentBrushPresetChanged, void (View::*)());
     ASSERT_VIEW_SIGNATURE(currentToolChanged, void (View::*)(const QString &));
     ASSERT_VIEW_SIGNATURE(showFloatingMessage, void (View::*)(const QString &, const QIcon &, int, int));
+}
+
+void ViewSchemaContractTest::scratchpadTypeLifetimeAndModeSchemaRemainStable()
+{
+    static_assert(std::is_class_v<Scratchpad>);
+    static_assert(std::is_base_of_v<QWidget, Scratchpad>);
+    static_assert(std::is_constructible_v<Scratchpad, View *, const QColor &>);
+    static_assert(std::is_constructible_v<Scratchpad, View *, const QColor &, QWidget *>);
+    static_assert(std::has_virtual_destructor_v<Scratchpad>);
+    ASSERT_SCRATCHPAD_SIGNATURE(setFillColor, void (Scratchpad::*)(QColor));
+    ASSERT_SCRATCHPAD_SIGNATURE(setMode, void (Scratchpad::*)(QString));
+    ASSERT_SCRATCHPAD_SIGNATURE(setModeManually, void (Scratchpad::*)(bool));
+}
+
+void ViewSchemaContractTest::scratchpadFillSignaturesRemainStable()
+{
+    ASSERT_SCRATCHPAD_SIGNATURE(clear, void (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(fillDefault, void (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(fillBackground, void (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(fillForeground, void (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(fillTransparent, void (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(fillDocument, void (Scratchpad::*)(bool));
+    ASSERT_SCRATCHPAD_SIGNATURE(fillLayer, void (Scratchpad::*)(bool));
+    ASSERT_SCRATCHPAD_SIGNATURE(
+        fillGradient,
+        void (Scratchpad::*)(const QPoint &, const QPoint &, const QString &, const QString &, bool, bool));
+    ASSERT_SCRATCHPAD_SIGNATURE(fillPattern, void (Scratchpad::*)(QTransform));
+
+    static_assert(std::is_same_v<decltype(std::declval<Scratchpad &>().fillDocument()), void>);
+    static_assert(std::is_same_v<decltype(std::declval<Scratchpad &>().fillLayer()), void>);
+    static_assert(std::is_same_v<decltype(std::declval<Scratchpad &>().fillGradient()), void>);
+    static_assert(std::is_same_v<decltype(std::declval<Scratchpad &>().fillPattern()), void>);
+}
+
+void ViewSchemaContractTest::scratchpadZoomAndScaleSignaturesRemainStable()
+{
+    ASSERT_SCRATCHPAD_SIGNATURE(linkCanvasZoom, void (Scratchpad::*)(bool));
+    ASSERT_SCRATCHPAD_SIGNATURE(canvasZoomLink, bool (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(setCanvasZoomLink, void (Scratchpad::*)(bool));
+    ASSERT_SCRATCHPAD_SIGNATURE(scale, qreal (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(setScale, bool (Scratchpad::*)(qreal) const);
+    ASSERT_SCRATCHPAD_SIGNATURE(scaleToFit, void (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(scaleReset, void (Scratchpad::*)());
+}
+
+void ViewSchemaContractTest::scratchpadPanImageAndBoundsSignaturesRemainStable()
+{
+    ASSERT_SCRATCHPAD_SIGNATURE(panTo, void (Scratchpad::*)(qint32, qint32));
+    ASSERT_SCRATCHPAD_SIGNATURE(panCenter, void (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(loadScratchpadImage, void (Scratchpad::*)(QImage));
+    ASSERT_SCRATCHPAD_SIGNATURE(copyScratchpadImageData, QImage (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(viewportBounds, QRect (Scratchpad::*)() const);
+    ASSERT_SCRATCHPAD_SIGNATURE(contentBounds, QRect (Scratchpad::*)() const);
+}
+
+void ViewSchemaContractTest::scratchpadNotificationSignaturesRemainStable()
+{
+    ASSERT_SCRATCHPAD_SIGNATURE(scaleChanged, void (Scratchpad::*)(qreal));
+    ASSERT_SCRATCHPAD_SIGNATURE(contentChanged, void (Scratchpad::*)());
+    ASSERT_SCRATCHPAD_SIGNATURE(viewportChanged, void (Scratchpad::*)(const QRect));
 }
 
 QTEST_GUILESS_MAIN(ViewSchemaContractTest)
