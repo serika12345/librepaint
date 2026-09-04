9bd3d2759db3210229ce749b345bc98b8a72fb64
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-04 13:31:54 +0900

    低水準色選択の公開契約を固定

--- libs/widgets/tests/KisColorSelectionControlSchemaContractTest.cpp
+++ libs/widgets/tests/KisColorSelectionControlSchemaContractTest.cpp
@@ -3,12 +3,37 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "KisColorSelectorInterface.h"
+#include "KisScreenColorSamplerBase.h"
 #include "KoColorPopupAction.h"
 #include "kis_color_button.h"
+#include "kis_spinbox_color_selector.h"
 
 #include <QTest>
 
 #include <type_traits>
+
+namespace KisColorSelectionControlContractTypes
+{
+
+class ColorSelectorInterfaceConstructionProbe final : public KisColorSelectorInterface
+{
+public:
+    using KisColorSelectorInterface::KisColorSelectorInterface;
+
+    KoColor getCurrentColor() const override;
+    void slotSetColor(const KoColor &color) override;
+};
+
+class ScreenColorSamplerConstructionProbe final : public KisScreenColorSamplerBase
+{
+public:
+    using KisScreenColorSamplerBase::KisScreenColorSamplerBase;
+
+    void updateIcons() override;
+};
+
+} // namespace KisColorSelectionControlContractTypes
 
 class KisColorSelectionControlSchemaContractTest : public QObject
 {
@@ -20,6 +45,11 @@ private Q_SLOTS:
     void colorButtonGeometryAndNotificationSignaturesRemainStable();
     void colorPopupActionTypeLifetimeAndColorSchemaRemainStable();
     void colorPopupActionEditingAndNotificationSignaturesRemainStable();
+    void selectorInterfaceTypeAndConfigSchemaRemainStable();
+    void selectorInterfaceColorAndNotificationSchemaRemainStable();
+    void screenSamplerTypeAndNotificationSchemaRemainStable();
+    void spinboxSelectorTypeAndAlphaSchemaRemainStable();
+    void spinboxSelectorColorAndNotificationSchemaRemainStable();
 };
 
 void KisColorSelectionControlSchemaContractTest::colorButtonTypeAndLifetimeSchemaRemainStable()
@@ -83,6 +113,67 @@ void KisColorSelectionControlSchemaContractTest::colorPopupActionEditingAndNotif
     static_assert(std::is_same_v<decltype(static_cast<QColorSetter>(&Action::setCurrentColor)), QColorSetter>);
     static_assert(std::is_same_v<decltype(&Action::updateIcon), void (Action::*)()>);
     static_assert(std::is_same_v<decltype(&Action::colorChanged), void (Action::*)(const KoColor &)>);
+}
+
+void KisColorSelectionControlSchemaContractTest::selectorInterfaceTypeAndConfigSchemaRemainStable()
+{
+    using Selector = KisColorSelectorInterface;
+    using ConstructionProbe = KisColorSelectionControlContractTypes::ColorSelectorInterfaceConstructionProbe;
+
+    static_assert(std::is_class_v<Selector>);
+    static_assert(std::is_base_of_v<QWidget, Selector>);
+    static_assert(std::is_abstract_v<Selector>);
+    static_assert(std::is_default_constructible_v<ConstructionProbe>);
+    static_assert(std::is_constructible_v<ConstructionProbe, QWidget *>);
+    static_assert(std::has_virtual_destructor_v<Selector>);
+    static_assert(std::is_same_v<decltype(&Selector::setConfig), void (Selector::*)(bool, bool)>);
+    static_assert(std::is_same_v<decltype(&Selector::setDisplayRenderer),
+                                 void (Selector::*)(const KoColorDisplayRendererInterface *)>);
+}
+
+void KisColorSelectionControlSchemaContractTest::selectorInterfaceColorAndNotificationSchemaRemainStable()
+{
+    using Selector = KisColorSelectorInterface;
+
+    static_assert(std::is_same_v<decltype(&Selector::getCurrentColor), KoColor (Selector::*)() const>);
+    static_assert(std::is_same_v<decltype(&Selector::sigNewColor), void (Selector::*)(const KoColor &)>);
+    static_assert(std::is_same_v<decltype(&Selector::slotSetColor), void (Selector::*)(const KoColor &)>);
+    static_assert(std::is_same_v<decltype(&Selector::slotSetColorSpace), void (Selector::*)(const KoColorSpace *)>);
+}
+
+void KisColorSelectionControlSchemaContractTest::screenSamplerTypeAndNotificationSchemaRemainStable()
+{
+    using Sampler = KisScreenColorSamplerBase;
+    using ConstructionProbe = KisColorSelectionControlContractTypes::ScreenColorSamplerConstructionProbe;
+
+    static_assert(std::is_class_v<Sampler>);
+    static_assert(std::is_base_of_v<QWidget, Sampler>);
+    static_assert(std::is_abstract_v<Sampler>);
+    static_assert(std::is_default_constructible_v<ConstructionProbe>);
+    static_assert(std::is_constructible_v<ConstructionProbe, QWidget *>);
+    static_assert(std::has_virtual_destructor_v<Sampler>);
+    static_assert(std::is_same_v<decltype(&Sampler::updateIcons), void (Sampler::*)()>);
+    static_assert(std::is_same_v<decltype(&Sampler::sigNewColorSampled), void (Sampler::*)(KoColor)>);
+}
+
+void KisColorSelectionControlSchemaContractTest::spinboxSelectorTypeAndAlphaSchemaRemainStable()
+{
+    using Selector = KisSpinboxColorSelector;
+
+    static_assert(std::is_class_v<Selector>);
+    static_assert(std::is_base_of_v<QWidget, Selector>);
+    static_assert(std::is_constructible_v<Selector, QWidget *>);
+    static_assert(std::has_virtual_destructor_v<Selector>);
+    static_assert(std::is_same_v<decltype(&Selector::chooseAlpha), void (Selector::*)(bool)>);
+}
+
+void KisColorSelectionControlSchemaContractTest::spinboxSelectorColorAndNotificationSchemaRemainStable()
+{
+    using Selector = KisSpinboxColorSelector;
+
+    static_assert(std::is_same_v<decltype(&Selector::sigNewColor), void (Selector::*)(KoColor)>);
+    static_assert(std::is_same_v<decltype(&Selector::slotSetColor), void (Selector::*)(KoColor)>);
+    static_assert(std::is_same_v<decltype(&Selector::slotSetColorSpace), void (Selector::*)(const KoColorSpace *)>);
 }
 
 QTEST_APPLESS_MAIN(KisColorSelectionControlSchemaContractTest)
