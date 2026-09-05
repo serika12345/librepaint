30f30bfe8671c2996c8bcf6964ba7948d8308e85
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-05 17:32:15 +0900

    フォルダー資源保管の公開契約を固定

--- libs/resources/tests/KisMemoryStorageSchemaContractTest.cpp
+++ libs/resources/tests/KisMemoryStorageSchemaContractTest.cpp
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "KisFolderStorage.h"
+
 #include "KisMemoryStorage.h"
 
 #include <QTest>
@@ -12,9 +14,12 @@
 namespace
 {
 using Storage = KisMemoryStorage;
+using FolderStorage = KisFolderStorage;
 
 #define ASSERT_MEMORY_STORAGE_SIGNATURE(method, signature)                                                             \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&Storage::method)), signature>)
+#define ASSERT_FOLDER_STORAGE_SIGNATURE(method, signature)                                                             \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&FolderStorage::method)), signature>)
 } // namespace
 
 class KisMemoryStorageSchemaContractTest : public QObject
@@ -27,6 +32,11 @@ private Q_SLOTS:
     void memoryStorageResourceTransferAndLookupSignaturesRemainStable();
     void memoryStorageMetadataSignaturesRemainStable();
     void memoryStorageIteratorSignaturesRemainStable();
+    void folderStorageTypeLifetimeAndConstructionSchemaRemainStable();
+    void folderStorageResourceMutationSignaturesRemainStable();
+    void folderStorageImportExportAndPathSignaturesRemainStable();
+    void folderStorageLookupAndIteratorSignaturesRemainStable();
+    void folderStorageMetadataSignaturesRemainStable();
 };
 
 void KisMemoryStorageSchemaContractTest::memoryStorageTypeLifetimeAndConstructionSchemaRemainStable()
@@ -68,6 +78,47 @@ void KisMemoryStorageSchemaContractTest::memoryStorageIteratorSignaturesRemainSt
                                     QSharedPointer<KisResourceStorage::ResourceIterator> (Storage::*)(const QString &));
     ASSERT_MEMORY_STORAGE_SIGNATURE(tags,
                                     QSharedPointer<KisResourceStorage::TagIterator> (Storage::*)(const QString &));
+}
+
+void KisMemoryStorageSchemaContractTest::folderStorageTypeLifetimeAndConstructionSchemaRemainStable()
+{
+    static_assert(std::is_class_v<FolderStorage>);
+    static_assert(std::is_base_of_v<KisStoragePlugin, FolderStorage>);
+    static_assert(std::is_constructible_v<FolderStorage, const QString &>);
+    static_assert(std::is_destructible_v<FolderStorage>);
+    static_assert(std::has_virtual_destructor_v<FolderStorage>);
+}
+
+void KisMemoryStorageSchemaContractTest::folderStorageResourceMutationSignaturesRemainStable()
+{
+    ASSERT_FOLDER_STORAGE_SIGNATURE(addResource, bool (FolderStorage::*)(const QString &, KoResourceSP));
+    ASSERT_FOLDER_STORAGE_SIGNATURE(loadVersionedResource, bool (FolderStorage::*)(KoResourceSP));
+    ASSERT_FOLDER_STORAGE_SIGNATURE(saveAsNewVersion, bool (FolderStorage::*)(const QString &, KoResourceSP));
+}
+
+void KisMemoryStorageSchemaContractTest::folderStorageImportExportAndPathSignaturesRemainStable()
+{
+    ASSERT_FOLDER_STORAGE_SIGNATURE(exportResource, bool (FolderStorage::*)(const QString &, QIODevice *));
+    ASSERT_FOLDER_STORAGE_SIGNATURE(importResource, bool (FolderStorage::*)(const QString &, QIODevice *));
+    ASSERT_FOLDER_STORAGE_SIGNATURE(resourceFilePath, QString (FolderStorage::*)(const QString &));
+    ASSERT_FOLDER_STORAGE_SIGNATURE(resourceMd5, QString (FolderStorage::*)(const QString &));
+}
+
+void KisMemoryStorageSchemaContractTest::folderStorageLookupAndIteratorSignaturesRemainStable()
+{
+    ASSERT_FOLDER_STORAGE_SIGNATURE(resourceItem, KisResourceStorage::ResourceItem (FolderStorage::*)(const QString &));
+    ASSERT_FOLDER_STORAGE_SIGNATURE(
+        resources,
+        QSharedPointer<KisResourceStorage::ResourceIterator> (FolderStorage::*)(const QString &));
+    ASSERT_FOLDER_STORAGE_SIGNATURE(
+        tags,
+        QSharedPointer<KisResourceStorage::TagIterator> (FolderStorage::*)(const QString &));
+}
+
+void KisMemoryStorageSchemaContractTest::folderStorageMetadataSignaturesRemainStable()
+{
+    ASSERT_FOLDER_STORAGE_SIGNATURE(metaData, QVariant (FolderStorage::*)(const QString &) const);
+    ASSERT_FOLDER_STORAGE_SIGNATURE(metaDataKeys, QStringList (FolderStorage::*)() const);
 }
 
 QTEST_GUILESS_MAIN(KisMemoryStorageSchemaContractTest)
