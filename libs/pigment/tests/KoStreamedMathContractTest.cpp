2c09807c197800fdc92c64d96946313d2cb79ade
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-06 22:55:04 +0900

    Cover streamed composite entry points

--- libs/pigment/tests/KoStreamedMathContractTest.cpp
+++ libs/pigment/tests/KoStreamedMathContractTest.cpp
@@ -19,6 +19,10 @@ using Math = KoStreamedMath<Architecture>;
 using FloatBatch = Math::float_v;
 using IntBatch = Math::int_v;
 using UintBatch = Math::uint_v;
+struct CompositeProbe;
+using CompositeFunction = void (*)(const KoCompositeOp::ParameterInfo &);
+#define ASSERT_STREAMED_COMPOSITE_SIGNATURE(method, ...)                                                               \
+    static_assert(std::is_same_v<decltype(&Math::template method<__VA_ARGS__>), CompositeFunction>)
 
 template<typename Batch>
 std::array<typename Batch::value_type, Batch::size> batchValues(const Batch &batch)
@@ -59,6 +63,7 @@ private Q_SLOTS:
     void maskAlphaAndChannelsRoundTripExactBytes();
     void pixelWrappersPreserveChannelPolicies();
     void pixelRecoveryRestoresOnlySelectedFloatLanes();
+    void genericCompositeSignaturesRemainStable();
 };
 
 void KoStreamedMathContractTest::scalarAndSimdRoundAndDivisionPreserveValues()
@@ -274,6 +279,18 @@ void KoStreamedMathContractTest::pixelRecoveryRestoresOnlySelectedFloatLanes()
     compareBatch(untouched1, FloatBatch(7.0f));
     compareBatch(untouched2, FloatBatch(8.0f));
     compareBatch(untouched3, FloatBatch(9.0f));
+}
+
+void KoStreamedMathContractTest::genericCompositeSignaturesRemainStable()
+{
+    ASSERT_STREAMED_COMPOSITE_SIGNATURE(genericComposite, false, false, CompositeProbe, 4);
+    ASSERT_STREAMED_COMPOSITE_SIGNATURE(genericComposite32, false, false, CompositeProbe);
+    ASSERT_STREAMED_COMPOSITE_SIGNATURE(genericComposite64, false, false, CompositeProbe);
+    ASSERT_STREAMED_COMPOSITE_SIGNATURE(genericComposite128, false, false, CompositeProbe);
+    ASSERT_STREAMED_COMPOSITE_SIGNATURE(genericComposite_novector, false, false, CompositeProbe, 4);
+    ASSERT_STREAMED_COMPOSITE_SIGNATURE(genericComposite32_novector, false, false, CompositeProbe);
+    ASSERT_STREAMED_COMPOSITE_SIGNATURE(genericComposite64_novector, false, false, CompositeProbe);
+    ASSERT_STREAMED_COMPOSITE_SIGNATURE(genericComposite128_novector, false, false, CompositeProbe);
 }
 
 QTEST_GUILESS_MAIN(KoStreamedMathContractTest)
