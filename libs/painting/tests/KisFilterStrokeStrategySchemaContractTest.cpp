cf3109443274ed24f7632b870a6989cb40412510
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-06 13:46:27 +0900

    Add move stroke public API contracts

--- libs/painting/tests/KisFilterStrokeStrategySchemaContractTest.cpp
+++ libs/painting/tests/KisFilterStrokeStrategySchemaContractTest.cpp
@@ -4,7 +4,9 @@
  */
 
 #include "strokes/kis_filter_stroke_strategy.h"
+#include "strokes/move_stroke_strategy.h"
 
+#include <memory>
 #include <type_traits>
 
 #include <QTest>
@@ -27,6 +29,11 @@ private Q_SLOTS:
     void filterStrokeCancellationStorageSchemaRemainStable();
     void filterStrokeStrategyTypeLifetimeAndConstructionSchemaRemainStable();
     void filterStrokeStrategyCallbackSignaturesRemainStable();
+    void moveStrokeDataValuesAndLodCloneRemainStable();
+    void moveStrokePickLayerDataValuesAndLodCloneRemainStable();
+    void moveStrokeBarrierUpdateDataValuesAndLodCloneRemainStable();
+    void moveStrokeStrategyTypeLifetimeAndConstructionSchemaRemainStable();
+    void moveStrokeStrategyCallbacksAndNotificationsRemainStable();
 };
 
 void KisFilterStrokeStrategySchemaContractTest::filterStrokeJobDataTypeConstructionAndCloneSchemaRemainStable()
@@ -97,6 +104,115 @@ void KisFilterStrokeStrategySchemaContractTest::filterStrokeStrategyCallbackSign
     ASSERT_FILTER_STROKE_MEMBER(Strategy, doStrokeCallback, void (Strategy::*)(KisStrokeJobData *));
     ASSERT_FILTER_STROKE_MEMBER(Strategy, finishStrokeCallback, void (Strategy::*)());
     ASSERT_FILTER_STROKE_MEMBER(Strategy, initStrokeCallback, void (Strategy::*)());
+
+    QVERIFY(true);
+}
+
+void KisFilterStrokeStrategySchemaContractTest::moveStrokeDataValuesAndLodCloneRemainStable()
+{
+    using Data = MoveStrokeStrategy::Data;
+
+    static_assert(std::is_class_v<Data>);
+    static_assert(std::is_base_of_v<KisStrokeJobData, Data>);
+    static_assert(std::is_constructible_v<Data, QPoint>);
+    static_assert(std::is_same_v<decltype(&Data::offset), QPoint Data::*>);
+    ASSERT_FILTER_STROKE_MEMBER(Data, createLodClone, KisStrokeJobData * (Data::*)(int));
+
+    Data source(QPoint(16, -8));
+    QVERIFY(source.isSequential());
+    QVERIFY(!source.isBarrier());
+    QVERIFY(!source.isExclusive());
+    QCOMPARE(source.offset, QPoint(16, -8));
+
+    std::unique_ptr<KisStrokeJobData> cloneBase(source.createLodClone(2));
+    auto *clone = dynamic_cast<Data *>(cloneBase.get());
+    QVERIFY(clone);
+    QCOMPARE(clone->offset, QPoint(4, -2));
+    QVERIFY(clone->isSequential());
+    QVERIFY(!clone->isExclusive());
+
+    clone->offset = QPoint(1, 2);
+    QCOMPARE(source.offset, QPoint(16, -8));
+}
+
+void KisFilterStrokeStrategySchemaContractTest::moveStrokePickLayerDataValuesAndLodCloneRemainStable()
+{
+    using Data = MoveStrokeStrategy::PickLayerData;
+
+    static_assert(std::is_class_v<Data>);
+    static_assert(std::is_base_of_v<KisStrokeJobData, Data>);
+    static_assert(std::is_constructible_v<Data, QPoint>);
+    static_assert(std::is_same_v<decltype(&Data::pos), QPoint Data::*>);
+    ASSERT_FILTER_STROKE_MEMBER(Data, createLodClone, KisStrokeJobData * (Data::*)(int));
+
+    Data source(QPoint(-12, 20));
+    QVERIFY(source.isSequential());
+    QVERIFY(!source.isBarrier());
+    QVERIFY(!source.isExclusive());
+    QCOMPARE(source.pos, QPoint(-12, 20));
+
+    std::unique_ptr<KisStrokeJobData> cloneBase(source.createLodClone(1));
+    auto *clone = dynamic_cast<Data *>(cloneBase.get());
+    QVERIFY(clone);
+    QCOMPARE(clone->pos, QPoint(-6, 10));
+    QVERIFY(clone->isSequential());
+    QVERIFY(!clone->isExclusive());
+
+    clone->pos = QPoint(3, 5);
+    QCOMPARE(source.pos, QPoint(-12, 20));
+}
+
+void KisFilterStrokeStrategySchemaContractTest::moveStrokeBarrierUpdateDataValuesAndLodCloneRemainStable()
+{
+    using Data = MoveStrokeStrategy::BarrierUpdateData;
+
+    static_assert(std::is_class_v<Data>);
+    static_assert(std::is_base_of_v<KisAsynchronousStrokeUpdateHelper::UpdateData, Data>);
+    static_assert(std::is_constructible_v<Data, bool>);
+    ASSERT_FILTER_STROKE_MEMBER(Data, createLodClone, KisStrokeJobData * (Data::*)(int));
+
+    Data source(true);
+    QVERIFY(source.forceUpdate);
+    QVERIFY(source.isBarrier());
+    QVERIFY(source.isExclusive());
+
+    std::unique_ptr<KisStrokeJobData> cloneBase(source.createLodClone(3));
+    auto *clone = dynamic_cast<Data *>(cloneBase.get());
+    QVERIFY(clone);
+    QVERIFY(clone->forceUpdate);
+    QVERIFY(clone->isBarrier());
+    QVERIFY(clone->isExclusive());
+
+    clone->forceUpdate = false;
+    QVERIFY(source.forceUpdate);
+}
+
+void KisFilterStrokeStrategySchemaContractTest::moveStrokeStrategyTypeLifetimeAndConstructionSchemaRemainStable()
+{
+    using Strategy = MoveStrokeStrategy;
+
+    static_assert(std::is_class_v<Strategy>);
+    static_assert(std::is_base_of_v<QObject, Strategy>);
+    static_assert(std::is_base_of_v<KisStrokeStrategyUndoCommandBased, Strategy>);
+    static_assert(std::is_constructible_v<Strategy, KisNodeSelectionRecipe, KisUpdatesFacade *, KisStrokeUndoFacade *>);
+    static_assert(std::is_constructible_v<Strategy, KisNodeList, KisUpdatesFacade *, KisStrokeUndoFacade *>);
+    static_assert(std::has_virtual_destructor_v<Strategy>);
+
+    QVERIFY(true);
+}
+
+void KisFilterStrokeStrategySchemaContractTest::moveStrokeStrategyCallbacksAndNotificationsRemainStable()
+{
+    using Strategy = MoveStrokeStrategy;
+
+    ASSERT_FILTER_STROKE_MEMBER(Strategy, cancelStrokeCallback, void (Strategy::*)());
+    ASSERT_FILTER_STROKE_MEMBER(Strategy, createLodClone, KisStrokeStrategy * (Strategy::*)(int));
+    ASSERT_FILTER_STROKE_MEMBER(Strategy, doStrokeCallback, void (Strategy::*)(KisStrokeJobData *));
+    ASSERT_FILTER_STROKE_MEMBER(Strategy, finishStrokeCallback, void (Strategy::*)());
+    ASSERT_FILTER_STROKE_MEMBER(Strategy, initStrokeCallback, void (Strategy::*)());
+    ASSERT_FILTER_STROKE_MEMBER(Strategy, sigHandlesRectCalculated, void (Strategy::*)(const QRect &));
+    ASSERT_FILTER_STROKE_MEMBER(Strategy, sigLayersPicked, void (Strategy::*)(const KisNodeList &));
+    ASSERT_FILTER_STROKE_MEMBER(Strategy, sigStrokeStartedEmpty, void (Strategy::*)());
 
     QVERIFY(true);
 }
