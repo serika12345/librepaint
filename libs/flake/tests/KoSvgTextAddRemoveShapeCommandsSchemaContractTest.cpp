9b375224ac975bb14a32ef33ee3a773198fe0cef
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-06 21:00:37 +0900

    Cover SVG text edit command public API

--- libs/flake/tests/KoSvgTextAddRemoveShapeCommandsSchemaContractTest.cpp
+++ libs/flake/tests/KoSvgTextAddRemoveShapeCommandsSchemaContractTest.cpp
@@ -3,7 +3,10 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include <commands/KoShapeMergeTextPropertiesCommand.h>
+#include <commands/KoSvgConvertTextTypeCommand.h>
 #include <commands/KoSvgTextAddRemoveShapeCommands.h>
+#include <commands/KoSvgTextPathInfoChangeCommand.h>
 
 #include <QTest>
 
@@ -19,6 +22,11 @@ private Q_SLOTS:
     void svgTextContourCommandPhaseSignaturesRemainStable();
     void svgTextContourSpecializedLifecycleSignaturesRemainStable();
     void svgTextContourRemovalOperationSignatureRemainsStable();
+    void mergeTextPropertiesTypeSchema();
+    void mergeTextPropertiesHistorySchema();
+    void textPathInfoTypeSchema();
+    void textPathInfoHistorySchema();
+    void convertTextTypeSchema();
 };
 
 void KoSvgTextAddRemoveShapeCommandsSchemaContractTest::svgTextContourCommandTypeHierarchyRemainsStable()
@@ -81,6 +89,69 @@ void KoSvgTextAddRemoveShapeCommandsSchemaContractTest::svgTextContourRemovalOpe
 {
     using RemoveFunction = void (*)(KoSvgTextShape *, KUndo2Command *, bool, bool);
     static_assert(std::is_same_v<decltype(&KoSvgTextRemoveShapeCommand::removeContourShapesFromFlow), RemoveFunction>);
+}
+
+void KoSvgTextAddRemoveShapeCommandsSchemaContractTest::mergeTextPropertiesTypeSchema()
+{
+    using Command = KoShapeMergeTextPropertiesCommand;
+    using PropertyIds = QSet<KoSvgTextProperties::PropertyId>;
+
+    static_assert(std::is_class_v<Command>);
+    static_assert(std::is_base_of_v<KUndo2Command, Command>);
+    static_assert(std::has_virtual_destructor_v<Command>);
+    static_assert(std::is_constructible_v<Command,
+                                          const QList<KoShape *> &,
+                                          const KoSvgTextProperties &,
+                                          PropertyIds,
+                                          KUndo2Command *>);
+    static_assert(std::is_constructible_v<Command, const QList<KoShape *> &, const KoSvgTextProperties &>);
+}
+
+void KoSvgTextAddRemoveShapeCommandsSchemaContractTest::mergeTextPropertiesHistorySchema()
+{
+    using Command = KoShapeMergeTextPropertiesCommand;
+
+    static_assert(
+        std::is_same_v<decltype(static_cast<int (Command::*)() const>(&Command::id)), int (Command::*)() const>);
+    static_assert(std::is_same_v<decltype(static_cast<bool (Command::*)(const KUndo2Command *)>(&Command::mergeWith)),
+                                 bool (Command::*)(const KUndo2Command *)>);
+    static_assert(std::is_same_v<decltype(static_cast<void (Command::*)()>(&Command::redo)), void (Command::*)()>);
+    static_assert(std::is_same_v<decltype(static_cast<void (Command::*)()>(&Command::undo)), void (Command::*)()>);
+}
+
+void KoSvgTextAddRemoveShapeCommandsSchemaContractTest::textPathInfoTypeSchema()
+{
+    using Command = KoSvgTextPathInfoChangeCommand;
+
+    static_assert(std::is_class_v<Command>);
+    static_assert(std::is_base_of_v<KUndo2Command, Command>);
+    static_assert(std::is_constructible_v<Command, KoSvgTextShape *, int, KoSvgText::TextOnPathInfo, KUndo2Command *>);
+    static_assert(std::is_constructible_v<Command, KoSvgTextShape *, int, KoSvgText::TextOnPathInfo>);
+}
+
+void KoSvgTextAddRemoveShapeCommandsSchemaContractTest::textPathInfoHistorySchema()
+{
+    using Command = KoSvgTextPathInfoChangeCommand;
+
+    static_assert(
+        std::is_same_v<decltype(static_cast<int (Command::*)() const>(&Command::id)), int (Command::*)() const>);
+    static_assert(std::is_same_v<decltype(static_cast<bool (Command::*)(const KUndo2Command *)>(&Command::mergeWith)),
+                                 bool (Command::*)(const KUndo2Command *)>);
+    static_assert(std::is_same_v<decltype(static_cast<void (Command::*)()>(&Command::redo)), void (Command::*)()>);
+    static_assert(std::is_same_v<decltype(static_cast<void (Command::*)()>(&Command::undo)), void (Command::*)()>);
+}
+
+void KoSvgTextAddRemoveShapeCommandsSchemaContractTest::convertTextTypeSchema()
+{
+    using Command = KoSvgConvertTextTypeCommand;
+
+    static_assert(std::is_class_v<Command>);
+    static_assert(std::is_base_of_v<KUndo2Command, Command>);
+    static_assert(std::has_virtual_destructor_v<Command>);
+    static_assert(std::is_constructible_v<Command, KoSvgTextShape *, KoSvgTextShape::TextType, int, KUndo2Command *>);
+    static_assert(std::is_constructible_v<Command, KoSvgTextShape *, KoSvgTextShape::TextType, int>);
+    static_assert(std::is_same_v<decltype(static_cast<void (Command::*)()>(&Command::redo)), void (Command::*)()>);
+    static_assert(std::is_same_v<decltype(static_cast<void (Command::*)()>(&Command::undo)), void (Command::*)()>);
 }
 
 QTEST_APPLESS_MAIN(KoSvgTextAddRemoveShapeCommandsSchemaContractTest)
