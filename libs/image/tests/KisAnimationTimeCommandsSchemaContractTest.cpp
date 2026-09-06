02811eef178a30ac71d9c51fc2dc22b9546a3fa5
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-06 19:36:48 +0900

    Fix animation auto-key contracts

--- libs/image/tests/KisAnimationTimeCommandsSchemaContractTest.cpp
+++ libs/image/tests/KisAnimationTimeCommandsSchemaContractTest.cpp
@@ -3,14 +3,18 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include <KisAnimAutoKey.h>
 #include <commands_new/kis_switch_current_time_command.h>
 
 #include <QTest>
 
 #include <type_traits>
+#include <utility>
 
 #define ASSERT_COMMAND_SIGNATURE(command, method, signature)                                                           \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&command::method)), signature>)
+#define ASSERT_FUNCTION_SIGNATURE(function, signature)                                                                 \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&function)), signature>)
 
 class KisAnimationTimeCommandsSchemaContractTest : public QObject
 {
@@ -20,6 +24,9 @@ private Q_SLOTS:
     void currentTimeCommandTypeConstructionAndLifetimeSchemaRemainStable();
     void currentTimeCommandHistoryAndExecutionSchemaRemainStable();
     void keyframeTimeCommandSchemaRemainStable();
+    void autoKeyModesAndFlagsRemainStable();
+    void autoKeyModeAccessSchemaRemainStable();
+    void autoKeyFrameCreationSchemaRemainStable();
 };
 
 void KisAnimationTimeCommandsSchemaContractTest::currentTimeCommandTypeConstructionAndLifetimeSchemaRemainStable()
@@ -70,6 +77,41 @@ void KisAnimationTimeCommandsSchemaContractTest::keyframeTimeCommandSchemaRemain
     QVERIFY(true);
 }
 
+void KisAnimationTimeCommandsSchemaContractTest::autoKeyModesAndFlagsRemainStable()
+{
+    static_assert(std::is_enum_v<KisAutoKey::Mode>);
+    static_assert(KisAutoKey::NONE == 0);
+    static_assert(KisAutoKey::BLANK == 1);
+    static_assert(KisAutoKey::DUPLICATE == 2);
+    static_assert(std::is_enum_v<KisAutoKey::AutoCreateKeyframeFlag>);
+    static_assert(KisAutoKey::None == 0x0);
+    static_assert(KisAutoKey::AllowBlankMode == 0x1);
+    static_assert(KisAutoKey::SupportsLod == 0x2);
+    static_assert(std::is_same_v<KisAutoKey::AutoCreateKeyframeFlags, QFlags<KisAutoKey::AutoCreateKeyframeFlag>>);
+
+    QVERIFY(true);
+}
+
+void KisAnimationTimeCommandsSchemaContractTest::autoKeyModeAccessSchemaRemainStable()
+{
+    ASSERT_FUNCTION_SIGNATURE(KisAutoKey::activeMode, KisAutoKey::Mode (*)());
+    ASSERT_FUNCTION_SIGNATURE(KisAutoKey::testingSetActiveMode, void (*)(KisAutoKey::Mode));
+
+    QVERIFY(true);
+}
+
+void KisAnimationTimeCommandsSchemaContractTest::autoKeyFrameCreationSchemaRemainStable()
+{
+    using CreateFunction = KUndo2Command *(*)(KisPaintDeviceSP, KisAutoKey::AutoCreateKeyframeFlags);
+
+    ASSERT_FUNCTION_SIGNATURE(KisAutoKey::tryAutoCreateDuplicatedFrame, CreateFunction);
+    static_assert(std::is_same_v<decltype(KisAutoKey::tryAutoCreateDuplicatedFrame(std::declval<KisPaintDeviceSP>())),
+                                 KUndo2Command *>);
+
+    QVERIFY(true);
+}
+
+#undef ASSERT_FUNCTION_SIGNATURE
 #undef ASSERT_COMMAND_SIGNATURE
 
 QTEST_APPLESS_MAIN(KisAnimationTimeCommandsSchemaContractTest)
