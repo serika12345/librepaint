36dd54ac5c4c9e9cc8ef2e7310f1dcebd36f17d5
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-07 06:31:21 +0900

    Cover guides manager public APIs

--- libs/ui/tests/KisGuidesConfigSchemaContractTest.cpp
+++ libs/ui/tests/KisGuidesConfigSchemaContractTest.cpp
@@ -9,6 +9,7 @@
 #include <QTransform>
 
 #include <canvas/kis_guides_config.h>
+#include <canvas/kis_guides_manager.h>
 
 #include <type_traits>
 
@@ -16,6 +17,8 @@ namespace
 {
 #define ASSERT_GUIDES_CONFIG_SIGNATURE(method, signature)                                                              \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&KisGuidesConfig::method)), signature>)
+#define ASSERT_GUIDES_MANAGER_SIGNATURE(method, signature)                                                             \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KisGuidesManager::method)), signature>)
 } // namespace
 
 class KisGuidesConfigSchemaContractTest : public QObject
@@ -28,6 +31,11 @@ private Q_SLOTS:
     void guideVisibilityInteractionSignaturesRemainStable();
     void guideUnitAndLineTypeSignaturesRemainStable();
     void guideColorAndPenSignaturesRemainStable();
+    void guidesManagerTypeLifetimeAndEventSchemaRemainStable();
+    void guidesManagerViewAndStateQuerySignaturesRemainStable();
+    void guidesManagerConfigurationAndPrimaryStateSignaturesRemainStable();
+    void guidesManagerCreationAndSnappingSignaturesRemainStable();
+    void guidesManagerPersistenceAndNotificationSignaturesRemainStable();
 };
 
 void KisGuidesConfigSchemaContractTest::guideValueIdentityAndEqualitySchemaRemainStable()
@@ -82,6 +90,62 @@ void KisGuidesConfigSchemaContractTest::guideColorAndPenSignaturesRemainStable()
     ASSERT_GUIDES_CONFIG_SIGNATURE(guidesPen, QPen (KisGuidesConfig::*)() const);
 }
 
+void KisGuidesConfigSchemaContractTest::guidesManagerTypeLifetimeAndEventSchemaRemainStable()
+{
+    static_assert(std::is_class_v<KisGuidesManager>);
+    static_assert(std::is_base_of_v<QObject, KisGuidesManager>);
+    static_assert(std::is_default_constructible_v<KisGuidesManager>);
+    static_assert(std::is_constructible_v<KisGuidesManager, QObject *>);
+    static_assert(std::has_virtual_destructor_v<KisGuidesManager>);
+    ASSERT_GUIDES_MANAGER_SIGNATURE(eventFilter, bool (KisGuidesManager::*)(QObject *, QEvent *));
+}
+
+void KisGuidesConfigSchemaContractTest::guidesManagerViewAndStateQuerySignaturesRemainStable()
+{
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setup, void (KisGuidesManager::*)(KisActionManager *));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setView, void (KisGuidesManager::*)(QPointer<KisView>));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(showGuides, bool (KisGuidesManager::*)() const);
+    ASSERT_GUIDES_MANAGER_SIGNATURE(lockGuides, bool (KisGuidesManager::*)() const);
+    ASSERT_GUIDES_MANAGER_SIGNATURE(snapToGuides, bool (KisGuidesManager::*)() const);
+    ASSERT_GUIDES_MANAGER_SIGNATURE(rulersMultiple2, bool (KisGuidesManager::*)() const);
+    ASSERT_GUIDES_MANAGER_SIGNATURE(unitType, KoUnit::Type (KisGuidesManager::*)() const);
+}
+
+void KisGuidesConfigSchemaContractTest::guidesManagerConfigurationAndPrimaryStateSignaturesRemainStable()
+{
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setGuidesConfig, void (KisGuidesManager::*)(const KisGuidesConfig &));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(slotDocumentRequestedConfig, void (KisGuidesManager::*)(const KisGuidesConfig &));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setShowGuides, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setLockGuides, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setSnapToGuides, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setRulersMultiple2, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setUnitType, void (KisGuidesManager::*)(KoUnit::Type));
+}
+
+void KisGuidesConfigSchemaContractTest::guidesManagerCreationAndSnappingSignaturesRemainStable()
+{
+    ASSERT_GUIDES_MANAGER_SIGNATURE(slotGuideCreationInProgress,
+                                    void (KisGuidesManager::*)(Qt::Orientation, const QPoint &));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(slotGuideCreationFinished,
+                                    void (KisGuidesManager::*)(Qt::Orientation, const QPoint &));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(slotShowSnapOptions, void (KisGuidesManager::*)());
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setSnapOrthogonal, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setSnapNode, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setSnapExtension, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setSnapIntersection, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setSnapBoundingBox, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setSnapImageBounds, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setSnapImageCenter, void (KisGuidesManager::*)(bool));
+    ASSERT_GUIDES_MANAGER_SIGNATURE(setSnapToPixel, void (KisGuidesManager::*)(bool));
+}
+
+void KisGuidesConfigSchemaContractTest::guidesManagerPersistenceAndNotificationSignaturesRemainStable()
+{
+    ASSERT_GUIDES_MANAGER_SIGNATURE(slotUploadConfigToDocument, void (KisGuidesManager::*)());
+    ASSERT_GUIDES_MANAGER_SIGNATURE(sigRequestUpdateGuidesConfig, void (KisGuidesManager::*)(const KisGuidesConfig &));
+}
+
+#undef ASSERT_GUIDES_MANAGER_SIGNATURE
 #undef ASSERT_GUIDES_CONFIG_SIGNATURE
 
 QTEST_APPLESS_MAIN(KisGuidesConfigSchemaContractTest)
