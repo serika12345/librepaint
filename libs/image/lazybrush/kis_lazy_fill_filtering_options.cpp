32a9c6eee8bc5a004e8176e38cb8b699863c2a93
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-06 11:25:44 +0900

    Separate lazy fill filtering option ownership

---
+++ libs/image/lazybrush/kis_lazy_fill_filtering_options.cpp
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2016 Dmitry Kazakov <dimula73@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "kis_lazy_fill_tools.h"
+
+namespace KisLazyFillTools
+{
+
+FilteringOptions::FilteringOptions(bool _useEdgeDetection,
+                                   qreal _edgeDetectionSize,
+                                   qreal _fuzzyRadius,
+                                   qreal _cleanUpAmount)
+    : useEdgeDetection(_useEdgeDetection)
+    , edgeDetectionSize(_edgeDetectionSize)
+    , fuzzyRadius(_fuzzyRadius)
+    , cleanUpAmount(_cleanUpAmount)
+{
+}
+
+bool operator==(const FilteringOptions &t1, const FilteringOptions &t2)
+{
+    return t1.useEdgeDetection == t2.useEdgeDetection && qFuzzyCompare(t1.edgeDetectionSize, t2.edgeDetectionSize)
+        && qFuzzyCompare(t1.fuzzyRadius, t2.fuzzyRadius) && qFuzzyCompare(t1.cleanUpAmount, t2.cleanUpAmount);
+}
+
+} // namespace KisLazyFillTools
