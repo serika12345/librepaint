527f93df2ba609d5192b55df19d763e7fc68572c
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-07 09:14:50 +0900

    Fix action public contracts

--- libs/application/tests/KisActionEnumContractTest.cpp
+++ libs/application/tests/KisActionEnumContractTest.cpp
@@ -8,7 +8,11 @@
 #include <QTest>
 
 #include <array>
+#include <type_traits>
 #include <utility>
+
+#define ASSERT_ACTION_SIGNATURE(method, signature)                                                                     \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KisAction::method)), signature>)
 
 class KisActionEnumContractTest : public QObject
 {
@@ -17,11 +21,16 @@ class KisActionEnumContractTest : public QObject
 private Q_SLOTS:
     void activationFlagsRemainIndependentBits();
     void activationConditionsRemainIndependentBits();
+    void actionTypeConstructionAndLifetimeSchemaRemainStable();
+    void actionDefaultShortcutSignaturesRemainStable();
+    void actionActivationFlagSignaturesRemainStable();
+    void actionActivationConditionSignaturesRemainStable();
+    void actionNodeOperationAndNotificationSignaturesRemainStable();
 };
 
 void KisActionEnumContractTest::activationFlagsRemainIndependentBits()
 {
-    const std::array<std::pair<KisAction::ActivationFlag, int>, 21> flags {{
+    const std::array<std::pair<KisAction::ActivationFlag, int>, 21> flags{{
         {KisAction::NONE, 0x00000},
         {KisAction::ACTIVE_IMAGE, 0x00001},
         {KisAction::MULTIPLE_IMAGES, 0x00002},
@@ -49,8 +58,7 @@ void KisActionEnumContractTest::activationFlagsRemainIndependentBits()
         QCOMPARE(int(flag.first), flag.second);
     }
 
-    const KisAction::ActivationFlags selection =
-        KisAction::PIXELS_SELECTED | KisAction::SHAPES_SELECTED;
+    const KisAction::ActivationFlags selection = KisAction::PIXELS_SELECTED | KisAction::SHAPES_SELECTED;
     QVERIFY(selection.testFlag(KisAction::PIXELS_SELECTED));
     QVERIFY(selection.testFlag(KisAction::SHAPES_SELECTED));
     QVERIFY(!selection.testFlag(KisAction::ACTIVE_IMAGE));
@@ -58,7 +66,7 @@ void KisActionEnumContractTest::activationFlagsRemainIndependentBits()
 
 void KisActionEnumContractTest::activationConditionsRemainIndependentBits()
 {
-    const std::array<std::pair<KisAction::ActivationCondition, int>, 5> conditions {{
+    const std::array<std::pair<KisAction::ActivationCondition, int>, 5> conditions{{
         {KisAction::NO_CONDITION, 0x0},
         {KisAction::ACTIVE_NODE_EDITABLE, 0x1},
         {KisAction::ACTIVE_NODE_EDITABLE_PAINT_DEVICE, 0x2},
@@ -70,11 +78,47 @@ void KisActionEnumContractTest::activationConditionsRemainIndependentBits()
         QCOMPARE(int(condition.first), condition.second);
     }
 
-    const KisAction::ActivationConditions editable =
-        KisAction::ACTIVE_NODE_EDITABLE | KisAction::SELECTION_EDITABLE;
+    const KisAction::ActivationConditions editable = KisAction::ACTIVE_NODE_EDITABLE | KisAction::SELECTION_EDITABLE;
     QVERIFY(editable.testFlag(KisAction::ACTIVE_NODE_EDITABLE));
     QVERIFY(editable.testFlag(KisAction::SELECTION_EDITABLE));
     QVERIFY(!editable.testFlag(KisAction::OPENGL_ENABLED));
+}
+
+void KisActionEnumContractTest::actionTypeConstructionAndLifetimeSchemaRemainStable()
+{
+    static_assert(std::is_class_v<KisAction>);
+    static_assert(std::is_base_of_v<QWidgetAction, KisAction>);
+    static_assert(std::is_constructible_v<KisAction, QObject *>);
+    static_assert(std::is_constructible_v<KisAction, const QString &, QObject *>);
+    static_assert(std::is_constructible_v<KisAction, const QIcon &, const QString &, QObject *>);
+    static_assert(std::has_virtual_destructor_v<KisAction>);
+}
+
+void KisActionEnumContractTest::actionDefaultShortcutSignaturesRemainStable()
+{
+    ASSERT_ACTION_SIGNATURE(defaultShortcut, QKeySequence (KisAction::*)() const);
+    ASSERT_ACTION_SIGNATURE(setDefaultShortcut, void (KisAction::*)(const QKeySequence &));
+}
+
+void KisActionEnumContractTest::actionActivationFlagSignaturesRemainStable()
+{
+    ASSERT_ACTION_SIGNATURE(activationFlags, KisAction::ActivationFlags (KisAction::*)());
+    ASSERT_ACTION_SIGNATURE(setActivationFlags, void (KisAction::*)(KisAction::ActivationFlags));
+}
+
+void KisActionEnumContractTest::actionActivationConditionSignaturesRemainStable()
+{
+    ASSERT_ACTION_SIGNATURE(activationConditions, KisAction::ActivationConditions (KisAction::*)());
+    ASSERT_ACTION_SIGNATURE(setActivationConditions, void (KisAction::*)(KisAction::ActivationConditions));
+}
+
+void KisActionEnumContractTest::actionNodeOperationAndNotificationSignaturesRemainStable()
+{
+    ASSERT_ACTION_SIGNATURE(excludedNodeTypes, const QStringList &(KisAction::*)() const);
+    ASSERT_ACTION_SIGNATURE(setExcludedNodeTypes, void (KisAction::*)(const QStringList &));
+    ASSERT_ACTION_SIGNATURE(setActionEnabled, void (KisAction::*)(bool));
+    ASSERT_ACTION_SIGNATURE(setOperationID, void (KisAction::*)(const QString &));
+    ASSERT_ACTION_SIGNATURE(sigEnableSlaves, void (KisAction::*)(bool));
 }
 
 QTEST_GUILESS_MAIN(KisActionEnumContractTest)
