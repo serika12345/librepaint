6b5677bf146aad64836bd8e23e0fe927de01fd61
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-08 19:20:52 +0900

    Fix convolution painter public API schema

--- libs/image/tests/KisGaussianKernelSchemaContractTest.cpp
+++ libs/image/tests/KisGaussianKernelSchemaContractTest.cpp
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "kis_convolution_painter.h"
 #include "kis_gaussian_kernel.h"
 
 #include <QTest>
@@ -11,6 +12,8 @@
 
 #define ASSERT_GAUSSIAN_KERNEL_SIGNATURE(method, signature)                                                            \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&KisGaussianKernel::method)), signature>)
+#define ASSERT_CONVOLUTION_PAINTER_SIGNATURE(method, signature)                                                        \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KisConvolutionPainter::method)), signature>)
 
 namespace
 {
@@ -29,6 +32,11 @@ private Q_SLOTS:
     void convolutionKernelFactorySignaturesRemainStable();
     void logAndDilateMatrixApplicationSignaturesRemainStable();
     void gaussianDilateAndErodeApplicationSignaturesRemainStable();
+    void convolutionBorderOperationEnumSchemaRemainsStable();
+    void convolutionPainterTypeAndEngineEnumSchemaRemainStable();
+    void convolutionPainterConstructionSignaturesRemainStable();
+    void convolutionPainterEngineAndCapabilitySignaturesRemainStable();
+    void convolutionPainterApplicationSignaturesRemainStable();
 };
 
 void KisGaussianKernelSchemaContractTest::typeAndRadiusConversionSchemaRemainStable()
@@ -76,6 +84,56 @@ void KisGaussianKernelSchemaContractTest::gaussianDilateAndErodeApplicationSigna
     ASSERT_GAUSSIAN_KERNEL_SIGNATURE(applyGaussian, ApplyGaussian);
     ASSERT_GAUSSIAN_KERNEL_SIGNATURE(applyDilate, ApplyMorphology);
     ASSERT_GAUSSIAN_KERNEL_SIGNATURE(applyErodeU8, ApplyMorphology);
+}
+
+void KisGaussianKernelSchemaContractTest::convolutionBorderOperationEnumSchemaRemainsStable()
+{
+    static_assert(std::is_enum_v<KisConvolutionBorderOp>);
+    static_assert(BORDER_IGNORE == 0);
+    static_assert(BORDER_REPEAT == 1);
+}
+
+void KisGaussianKernelSchemaContractTest::convolutionPainterTypeAndEngineEnumSchemaRemainStable()
+{
+    using EnginePreference = KisConvolutionPainter::EnginePreference;
+
+    static_assert(std::is_class_v<KisConvolutionPainter>);
+    static_assert(std::is_enum_v<EnginePreference>);
+    static_assert(KisConvolutionPainter::NONE == 0);
+    static_assert(KisConvolutionPainter::SPATIAL == 1);
+    static_assert(KisConvolutionPainter::FFTW == 2);
+}
+
+void KisGaussianKernelSchemaContractTest::convolutionPainterConstructionSignaturesRemainStable()
+{
+    using EnginePreference = KisConvolutionPainter::EnginePreference;
+
+    static_assert(std::is_constructible_v<KisConvolutionPainter>);
+    static_assert(std::is_constructible_v<KisConvolutionPainter, KisPaintDeviceSP>);
+    static_assert(std::is_constructible_v<KisConvolutionPainter, KisPaintDeviceSP, KisSelectionSP>);
+    static_assert(std::is_constructible_v<KisConvolutionPainter, KisPaintDeviceSP, EnginePreference>);
+}
+
+void KisGaussianKernelSchemaContractTest::convolutionPainterEngineAndCapabilitySignaturesRemainStable()
+{
+    using EnginePreference = KisConvolutionPainter::EnginePreference;
+
+    ASSERT_CONVOLUTION_PAINTER_SIGNATURE(setEnginePreference, void (KisConvolutionPainter::*)(EnginePreference));
+    ASSERT_CONVOLUTION_PAINTER_SIGNATURE(supportsFFTW, bool (*)());
+}
+
+void KisGaussianKernelSchemaContractTest::convolutionPainterApplicationSignaturesRemainStable()
+{
+    using ApplyMatrix = void (KisConvolutionPainter::*)(const KisConvolutionKernelSP,
+                                                        const KisPaintDeviceSP,
+                                                        QPoint,
+                                                        QPoint,
+                                                        QSize,
+                                                        KisConvolutionBorderOp);
+    using NeedsTransaction = bool (KisConvolutionPainter::*)(const KisConvolutionKernelSP) const;
+
+    ASSERT_CONVOLUTION_PAINTER_SIGNATURE(applyMatrix, ApplyMatrix);
+    ASSERT_CONVOLUTION_PAINTER_SIGNATURE(needsTransaction, NeedsTransaction);
 }
 
 QTEST_APPLESS_MAIN(KisGaussianKernelSchemaContractTest)
