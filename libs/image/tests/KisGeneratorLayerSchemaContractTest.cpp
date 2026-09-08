9c62c70263d4bfe7007876aab0db434aa3e40c19
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-09 03:31:04 +0900

    Fix multiple projection public API schema

--- libs/image/tests/KisGeneratorLayerSchemaContractTest.cpp
+++ libs/image/tests/KisGeneratorLayerSchemaContractTest.cpp
@@ -6,6 +6,7 @@
 #include "generator/kis_generator_layer.h"
 #include "kis_adjustment_layer.h"
 #include "kis_filter_mask.h"
+#include "layerstyles/kis_multiple_projection.h"
 
 #include <QTest>
 
@@ -23,6 +24,9 @@ namespace
 
 #define ASSERT_FILTER_MASK_SIGNATURE(method, signature)                                                                \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&KisFilterMask::method)), signature>)
+
+#define ASSERT_MULTIPLE_PROJECTION_SIGNATURE(method, signature)                                                        \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KisMultipleProjection::method)), signature>)
 
 } // namespace
 
@@ -44,6 +48,10 @@ private Q_SLOTS:
     void filterMaskVisitorSignaturesRemainStable();
     void filterMaskPresentationAndConfigurationSignaturesRemainStable();
     void filterMaskGeometrySignaturesRemainStable();
+    void multipleProjectionTypeConstructionAndLifetimeSchemaRemainStable();
+    void multipleProjectionIdentityAndAcquisitionSignaturesRemainStable();
+    void multipleProjectionReleaseAndClearSignaturesRemainStable();
+    void multipleProjectionApplicationAndStateSignaturesRemainStable();
 };
 
 void KisGeneratorLayerSchemaContractTest::generatorLayerTypeLifetimeAndConstructionSchemaRemainStable()
@@ -178,6 +186,47 @@ void KisGeneratorLayerSchemaContractTest::filterMaskGeometrySignaturesRemainStab
     ASSERT_FILTER_MASK_SIGNATURE(changeRect, DependencyRect);
     ASSERT_FILTER_MASK_SIGNATURE(needRect, DependencyRect);
 }
+
+void KisGeneratorLayerSchemaContractTest::multipleProjectionTypeConstructionAndLifetimeSchemaRemainStable()
+{
+    static_assert(std::is_class_v<KisMultipleProjection>);
+    static_assert(std::is_default_constructible_v<KisMultipleProjection>);
+    static_assert(std::is_copy_constructible_v<KisMultipleProjection>);
+    static_assert(std::is_destructible_v<KisMultipleProjection>);
+}
+
+void KisGeneratorLayerSchemaContractTest::multipleProjectionIdentityAndAcquisitionSignaturesRemainStable()
+{
+    using Projection = KisMultipleProjection;
+
+    ASSERT_MULTIPLE_PROJECTION_SIGNATURE(defaultProjectionId, QString (*)());
+    ASSERT_MULTIPLE_PROJECTION_SIGNATURE(
+        getProjection,
+        KisPaintDeviceSP (
+            Projection::*)(const QString &, const QString &, quint8, const QBitArray &, KisPaintDeviceSP));
+}
+
+void KisGeneratorLayerSchemaContractTest::multipleProjectionReleaseAndClearSignaturesRemainStable()
+{
+    using Projection = KisMultipleProjection;
+
+    ASSERT_MULTIPLE_PROJECTION_SIGNATURE(freeProjection, void (Projection::*)(const QString &));
+    ASSERT_MULTIPLE_PROJECTION_SIGNATURE(freeAllProjections, void (Projection::*)());
+    ASSERT_MULTIPLE_PROJECTION_SIGNATURE(clear, void (Projection::*)(const QRect &));
+}
+
+void KisGeneratorLayerSchemaContractTest::multipleProjectionApplicationAndStateSignaturesRemainStable()
+{
+    using Projection = KisMultipleProjection;
+
+    ASSERT_MULTIPLE_PROJECTION_SIGNATURE(
+        apply,
+        void (Projection::*)(KisPaintDeviceSP, const QRect &, KisLayerStyleFilterEnvironment *));
+    ASSERT_MULTIPLE_PROJECTION_SIGNATURE(getLodCapableDevices, KisPaintDeviceList (Projection::*)() const);
+    ASSERT_MULTIPLE_PROJECTION_SIGNATURE(isEmpty, bool (Projection::*)() const);
+}
+
+#undef ASSERT_MULTIPLE_PROJECTION_SIGNATURE
 
 QTEST_GUILESS_MAIN(KisGeneratorLayerSchemaContractTest)
 
