90b2eed036a0ddb04f3e0a2877d0bfd3218d6c2b
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-08 19:42:28 +0900

    Fix layer node view public API schema

--- libs/ui/tests/KisNodeViewColorSchemeSchemaContractTest.cpp
+++ libs/ui/tests/KisNodeViewColorSchemeSchemaContractTest.cpp
@@ -3,12 +3,16 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include <NodeView.h>
 #include <nodes/kis_node_view_color_scheme.h>
 
 #include <QRect>
 #include <QTest>
 
 #include <type_traits>
+
+#define ASSERT_NODE_VIEW_SIGNATURE(method, signature)                                                                  \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&NodeView::method)), signature>)
 
 class KisNodeViewColorSchemeSchemaContractTest : public QObject
 {
@@ -20,6 +24,11 @@ private Q_SLOTS:
     void nodeViewColorSchemeLayoutSchemaRemainsStable();
     void nodeViewColorSchemeRelativeGeometrySchemaRemainsStable();
     void nodeViewColorSchemeColorSchemaRemainsStable();
+    void nodeViewTypeColumnAndLifetimeSchemaRemainStable();
+    void nodeViewModelAndPaintingSignaturesRemainStable();
+    void nodeViewDragAndMouseEventSignaturesRemainStable();
+    void nodeViewNodeOperationSignaturesRemainStable();
+    void nodeViewNotificationAndUpdateSignaturesRemainStable();
 };
 
 void KisNodeViewColorSchemeSchemaContractTest::nodeViewColorSchemeIdentitySchemaRemainsStable()
@@ -79,6 +88,50 @@ void KisNodeViewColorSchemeSchemaContractTest::nodeViewColorSchemeColorSchemaRem
                                  QColor (Scheme::*)(const QStyleOptionViewItem &, QTreeView *) const>);
     static_assert(std::is_same_v<decltype(&Scheme::colorFromLabelIndex), QColor (Scheme::*)(int) const>);
     static_assert(std::is_same_v<decltype(&Scheme::allColorLabels), QVector<QColor> (Scheme::*)() const>);
+}
+
+void KisNodeViewColorSchemeSchemaContractTest::nodeViewTypeColumnAndLifetimeSchemaRemainStable()
+{
+    static_assert(std::is_class_v<NodeView>);
+    static_assert(std::is_enum_v<NodeView::ColumnIndex>);
+    static_assert(NodeView::DEFAULT_COL == 0);
+    static_assert(NodeView::VISIBILITY_COL == 1);
+    static_assert(NodeView::SELECTED_COL == 2);
+    static_assert(std::is_constructible_v<NodeView, QWidget *>);
+    static_assert(std::has_virtual_destructor_v<NodeView>);
+}
+
+void KisNodeViewColorSchemeSchemaContractTest::nodeViewModelAndPaintingSignaturesRemainStable()
+{
+    ASSERT_NODE_VIEW_SIGNATURE(setModel, void (NodeView::*)(QAbstractItemModel *));
+    ASSERT_NODE_VIEW_SIGNATURE(resizeEvent, void (NodeView::*)(QResizeEvent *));
+    ASSERT_NODE_VIEW_SIGNATURE(paintEvent, void (NodeView::*)(QPaintEvent *));
+    ASSERT_NODE_VIEW_SIGNATURE(drawBranches, void (NodeView::*)(QPainter *, const QRect &, const QModelIndex &) const);
+}
+
+void KisNodeViewColorSchemeSchemaContractTest::nodeViewDragAndMouseEventSignaturesRemainStable()
+{
+    ASSERT_NODE_VIEW_SIGNATURE(dragEnterEvent, void (NodeView::*)(QDragEnterEvent *));
+    ASSERT_NODE_VIEW_SIGNATURE(dragLeaveEvent, void (NodeView::*)(QDragLeaveEvent *));
+    ASSERT_NODE_VIEW_SIGNATURE(dragMoveEvent, void (NodeView::*)(QDragMoveEvent *));
+    ASSERT_NODE_VIEW_SIGNATURE(dropEvent, void (NodeView::*)(QDropEvent *));
+    ASSERT_NODE_VIEW_SIGNATURE(mousePressEvent, void (NodeView::*)(QMouseEvent *));
+}
+
+void KisNodeViewColorSchemeSchemaContractTest::nodeViewNodeOperationSignaturesRemainStable()
+{
+    ASSERT_NODE_VIEW_SIGNATURE(addPropertyActions, void (NodeView::*)(QMenu *, const QModelIndex &));
+    ASSERT_NODE_VIEW_SIGNATURE(toggleSolo, void (NodeView::*)(const QModelIndex &));
+    ASSERT_NODE_VIEW_SIGNATURE(updateNode, void (NodeView::*)(const QModelIndex &));
+}
+
+void KisNodeViewColorSchemeSchemaContractTest::nodeViewNotificationAndUpdateSignaturesRemainStable()
+{
+    ASSERT_NODE_VIEW_SIGNATURE(contextMenuRequested, void (NodeView::*)(const QPoint &, const QModelIndex &));
+    ASSERT_NODE_VIEW_SIGNATURE(selectionChanged, void (NodeView::*)(const QModelIndexList &));
+    ASSERT_NODE_VIEW_SIGNATURE(slotConfigurationChanged, void (NodeView::*)());
+    ASSERT_NODE_VIEW_SIGNATURE(slotScrollerStateChanged, void (NodeView::*)(QScroller::State));
+    ASSERT_NODE_VIEW_SIGNATURE(slotUpdateIcons, void (NodeView::*)());
 }
 
 QTEST_GUILESS_MAIN(KisNodeViewColorSchemeSchemaContractTest)
