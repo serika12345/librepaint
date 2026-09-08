595c996f1be0320a1bdad95a02a97f702a8847c7
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-09 00:05:46 +0900

    Fix async animation renderer public API schema

--- libs/ui/tests/KisAnimationFrameCacheSchemaContractTest.cpp
+++ libs/ui/tests/KisAnimationFrameCacheSchemaContractTest.cpp
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "animation/KisAsyncAnimationRendererBase.h"
 #include "animation/kis_animation_frame_cache.h"
 
 #include <QTest>
@@ -13,6 +14,17 @@ namespace
 {
 #define ASSERT_ANIMATION_FRAME_CACHE_SIGNATURE(method, signature)                                                      \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&KisAnimationFrameCache::method)), signature>)
+#define ASSERT_ASYNC_ANIMATION_RENDERER_SIGNATURE(method, signature)                                                   \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KisAsyncAnimationRendererBase::method)), signature>)
+
+class AsyncRendererConstructorProbe final : public KisAsyncAnimationRendererBase
+{
+public:
+    using KisAsyncAnimationRendererBase::KisAsyncAnimationRendererBase;
+
+    void frameCompletedCallback(int frame, const KisRegion &requestedRegion) override;
+    void frameCancelledCallback(int frame, CancelReason cancelReason) override;
+};
 } // namespace
 
 class KisAnimationFrameCacheSchemaContractTest : public QObject
@@ -25,6 +37,10 @@ private Q_SLOTS:
     void animationFrameCacheUploadAndStatusSignaturesRemainStable();
     void animationFrameCacheFrameDataSignaturesRemainStable();
     void animationFrameCacheRoiAndNotificationSignaturesRemainStable();
+    void asyncRendererTypeAndFlagSchemaRemainStable();
+    void asyncRendererCancellationReasonSchemaRemainStable();
+    void asyncRendererConstructionAndRegenerationSignaturesRemainStable();
+    void asyncRendererStateControlAndNotificationSignaturesRemainStable();
 };
 
 void KisAnimationFrameCacheSchemaContractTest::animationFrameCacheTypeLifetimeAndStatusSchemaRemainStable()
@@ -73,6 +89,54 @@ void KisAnimationFrameCacheSchemaContractTest::animationFrameCacheRoiAndNotifica
                                            bool (KisAnimationFrameCache::*)(const KisTimeSpan &, const QRect &));
     ASSERT_ANIMATION_FRAME_CACHE_SIGNATURE(changed, void (KisAnimationFrameCache::*)());
 }
+
+void KisAnimationFrameCacheSchemaContractTest::asyncRendererTypeAndFlagSchemaRemainStable()
+{
+    using Renderer = KisAsyncAnimationRendererBase;
+
+    static_assert(std::is_class_v<Renderer>);
+    static_assert(std::is_enum_v<Renderer::Flag>);
+    static_assert(std::is_same_v<Renderer::Flags, QFlags<Renderer::Flag>>);
+    static_assert(Renderer::None == 0x0);
+    static_assert(Renderer::Cancellable == 0x1);
+}
+
+void KisAnimationFrameCacheSchemaContractTest::asyncRendererCancellationReasonSchemaRemainStable()
+{
+    using Renderer = KisAsyncAnimationRendererBase;
+
+    static_assert(std::is_enum_v<Renderer::CancelReason>);
+    static_assert(Renderer::UserCancelled == 0);
+    static_assert(Renderer::RenderingFailed == 1);
+    static_assert(Renderer::RenderingTimedOut == 2);
+}
+
+void KisAnimationFrameCacheSchemaContractTest::asyncRendererConstructionAndRegenerationSignaturesRemainStable()
+{
+    using Renderer = KisAsyncAnimationRendererBase;
+
+    static_assert(std::is_abstract_v<Renderer>);
+    static_assert(std::is_constructible_v<AsyncRendererConstructorProbe, QObject *>);
+    static_assert(std::has_virtual_destructor_v<Renderer>);
+    ASSERT_ASYNC_ANIMATION_RENDERER_SIGNATURE(
+        startFrameRegeneration,
+        void (Renderer::*)(KisImageSP, int, const KisRegion &, Renderer::Flags, KisLockFrameGenerationLock &&));
+    ASSERT_ASYNC_ANIMATION_RENDERER_SIGNATURE(
+        startFrameRegeneration,
+        void (Renderer::*)(KisImageSP, int, Renderer::Flags, KisLockFrameGenerationLock &&));
+}
+
+void KisAnimationFrameCacheSchemaContractTest::asyncRendererStateControlAndNotificationSignaturesRemainStable()
+{
+    using Renderer = KisAsyncAnimationRendererBase;
+
+    ASSERT_ASYNC_ANIMATION_RENDERER_SIGNATURE(isActive, bool (Renderer::*)() const);
+    ASSERT_ASYNC_ANIMATION_RENDERER_SIGNATURE(cancelCurrentFrameRendering, void (Renderer::*)(Renderer::CancelReason));
+    ASSERT_ASYNC_ANIMATION_RENDERER_SIGNATURE(sigFrameCompleted, void (Renderer::*)(int));
+    ASSERT_ASYNC_ANIMATION_RENDERER_SIGNATURE(sigFrameCancelled, void (Renderer::*)(int, Renderer::CancelReason));
+}
+
+#undef ASSERT_ASYNC_ANIMATION_RENDERER_SIGNATURE
 
 QTEST_APPLESS_MAIN(KisAnimationFrameCacheSchemaContractTest)
 
