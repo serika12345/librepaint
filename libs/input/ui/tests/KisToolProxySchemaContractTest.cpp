00d04c79060b7f478778636a093ad5ff8060eb06
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-09 19:16:19 +0900

    主設定変更操作の公開契約を固定

--- libs/input/ui/tests/KisToolProxySchemaContractTest.cpp
+++ libs/input/ui/tests/KisToolProxySchemaContractTest.cpp
@@ -4,6 +4,8 @@
 
 #include <kis_tool_proxy.h>
 
+#include "kis_change_primary_setting_action.h"
+
 #include <QTest>
 
 #include <type_traits>
@@ -26,6 +28,10 @@ private Q_SLOTS:
     void toolProxyImageAndActiveToolSignaturesRemainStable();
     void toolProxyEventForwardingAndCapabilitySignaturesRemainStable();
     void toolProxyActionLifecycleAndNotificationSignaturesRemainStable();
+    void primarySettingActionTypeShortcutAndLifetimeSchemaRemainStable();
+    void primarySettingActionShortcutOrdinalsRemainStable();
+    void primarySettingActionLifecycleSignaturesRemainStable();
+    void primarySettingActionEventAndPolicySignaturesRemainStable();
 };
 
 void KisToolProxySchemaContractTest::toolProxyTypeLifetimeAndConstructionSchemaRemainStable()
@@ -65,6 +71,51 @@ void KisToolProxySchemaContractTest::toolProxyActionLifecycleAndNotificationSign
     ASSERT_TOOL_PROXY_SIGNATURE(activateToolAction, void (Proxy::*)(KisTool::ToolAction));
     ASSERT_TOOL_PROXY_SIGNATURE(deactivateToolAction, void (Proxy::*)(KisTool::ToolAction));
     ASSERT_TOOL_PROXY_SIGNATURE(toolPrimaryActionActivated, void (Proxy::*)(bool));
+}
+
+void KisToolProxySchemaContractTest::primarySettingActionTypeShortcutAndLifetimeSchemaRemainStable()
+{
+    using Action = KisChangePrimarySettingAction;
+
+    static_assert(std::is_class_v<Action>);
+    static_assert(std::is_base_of_v<KisAbstractInputAction, Action>);
+    static_assert(std::is_enum_v<Action::Shortcut>);
+    static_assert(std::is_default_constructible_v<Action>);
+    static_assert(std::has_virtual_destructor_v<Action>);
+}
+
+void KisToolProxySchemaContractTest::primarySettingActionShortcutOrdinalsRemainStable()
+{
+    using Shortcut = KisChangePrimarySettingAction::Shortcut;
+
+    static_assert(Shortcut::PrimaryAlternateChangeSizeShortcut == 0);
+    static_assert(Shortcut::SecondaryAlternateChangeSizeShortcut == 1);
+}
+
+void KisToolProxySchemaContractTest::primarySettingActionLifecycleSignaturesRemainStable()
+{
+    using Action = KisChangePrimarySettingAction;
+    using ShortcutOperation = void (Action::*)(int);
+    using BeginOperation = void (Action::*)(int, QEvent *);
+    using EventOperation = void (Action::*)(QEvent *);
+
+    static_assert(std::is_same_v<decltype(static_cast<ShortcutOperation>(&Action::activate)), ShortcutOperation>);
+    static_assert(std::is_same_v<decltype(static_cast<ShortcutOperation>(&Action::deactivate)), ShortcutOperation>);
+    static_assert(std::is_same_v<decltype(static_cast<BeginOperation>(&Action::begin)), BeginOperation>);
+    static_assert(std::is_same_v<decltype(static_cast<EventOperation>(&Action::end)), EventOperation>);
+}
+
+void KisToolProxySchemaContractTest::primarySettingActionEventAndPolicySignaturesRemainStable()
+{
+    using Action = KisChangePrimarySettingAction;
+    using EventOperation = void (Action::*)(QEvent *);
+    using PriorityQuery = int (Action::*)() const;
+    using ShortcutBooleanQuery = bool (Action::*)(int) const;
+
+    static_assert(std::is_same_v<decltype(static_cast<EventOperation>(&Action::inputEvent)), EventOperation>);
+    static_assert(std::is_same_v<decltype(static_cast<PriorityQuery>(&Action::priority)), PriorityQuery>);
+    static_assert(std::is_same_v<decltype(static_cast<ShortcutBooleanQuery>(&Action::supportsHiResInputEvents)),
+                                 ShortcutBooleanQuery>);
 }
 
 QTEST_APPLESS_MAIN(KisToolProxySchemaContractTest)
