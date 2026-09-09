20b3067293c909a0832f340ecd8369708ba6d8f5
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-09 18:52:50 +0900

    レイヤードックの公開契約を固定

---
+++ plugins/dockers/layerdocker/tests/LayerBoxSchemaContractTest.cpp
@@ -0,0 +1,64 @@
+/*
+ * SPDX-FileCopyrightText: 2026 LibrePaint contributors
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "LayerBox.h"
+
+#include <QTest>
+
+#include <type_traits>
+
+class LayerBoxSchemaContractTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void indexListAndLayerBoxLifetimeSchemaRemainStable();
+    void observerAndCanvasSignaturesRemainStable();
+    void imageNotificationAndFactoryLifetimeSchemaRemainStable();
+    void factoryIdentityCreationAndPlacementSchemaRemainStable();
+};
+
+void LayerBoxSchemaContractTest::indexListAndLayerBoxLifetimeSchemaRemainStable()
+{
+    static_assert(std::is_same_v<QModelIndexList, QList<QModelIndex>>);
+    static_assert(std::is_class_v<LayerBox>);
+    static_assert(std::is_base_of_v<QDockWidget, LayerBox>);
+    static_assert(std::is_base_of_v<KisMainwindowObserver, LayerBox>);
+    static_assert(std::is_default_constructible_v<LayerBox>);
+    static_assert(std::has_virtual_destructor_v<LayerBox>);
+}
+
+void LayerBoxSchemaContractTest::observerAndCanvasSignaturesRemainStable()
+{
+    using Box = LayerBox;
+
+    static_assert(std::is_same_v<decltype(&Box::observerName), QString (Box::*)()>);
+    static_assert(std::is_same_v<decltype(&Box::setViewManager), void (Box::*)(KisViewManager *)>);
+    static_assert(std::is_same_v<decltype(&Box::setCanvas), void (Box::*)(KoCanvasBase *)>);
+    static_assert(std::is_same_v<decltype(&Box::unsetCanvas), void (Box::*)()>);
+}
+
+void LayerBoxSchemaContractTest::imageNotificationAndFactoryLifetimeSchemaRemainStable()
+{
+    static_assert(std::is_same_v<decltype(&LayerBox::imageChanged), void (LayerBox::*)()>);
+    static_assert(std::is_class_v<LayerBoxFactory>);
+    static_assert(std::is_base_of_v<KoDockFactoryBase, LayerBoxFactory>);
+    static_assert(std::is_default_constructible_v<LayerBoxFactory>);
+}
+
+void LayerBoxSchemaContractTest::factoryIdentityCreationAndPlacementSchemaRemainStable()
+{
+    using Factory = LayerBoxFactory;
+
+    static_assert(std::is_same_v<decltype(&Factory::id), QString (Factory::*)() const>);
+    static_assert(std::is_same_v<decltype(&Factory::createDockWidget), QDockWidget *(Factory::*)()>);
+    static_assert(
+        std::is_same_v<decltype(&Factory::defaultDockPosition), KoDockFactoryBase::DockPosition (Factory::*)() const>);
+}
+
+QTEST_GUILESS_MAIN(LayerBoxSchemaContractTest)
+
+#include "LayerBoxSchemaContractTest.moc"
