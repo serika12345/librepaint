955e7ef3059bc62b38fb9981dfbee18bb699fb7e
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-09 23:31:17 +0900

    表示filterの公開契約を固定

--- libs/ui/tests/KisOpenGLModeProberSchemaContractTest.cpp
+++ libs/ui/tests/KisOpenGLModeProberSchemaContractTest.cpp
@@ -5,6 +5,7 @@
 
 #include <opengl/KisOpenGLModeProber.h>
 
+#include <canvas/kis_display_filter.h>
 #include <color/KisSurfaceColorSpaceWrapper.h>
 
 #include <QTest>
@@ -15,6 +16,22 @@ namespace
 {
 #define ASSERT_OPENGL_MODE_SIGNATURE(type, method, signature)                                                          \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&type::method)), signature>)
+
+class DisplayFilterConstructionProbe final : public KisDisplayFilter
+{
+public:
+    using KisDisplayFilter::KisDisplayFilter;
+
+    QString program() const override;
+    void setupTextures(GLFunctions *functions, QOpenGLShaderProgram *program) const override;
+    void filter(quint8 *pixels, quint32 numPixels) override;
+    void approximateInverseTransformation(quint8 *pixels, quint32 numPixels) override;
+    void approximateForwardTransformation(quint8 *pixels, quint32 numPixels) override;
+    bool useInternalColorManagement() const override;
+    KisExposureGammaCorrectionInterface *correctionInterface() const override;
+    bool lockCurrentColorVisualRepresentation() const override;
+    bool updateShader() override;
+};
 } // namespace
 
 class KisOpenGLModeProberSchemaContractTest : public QObject
@@ -27,6 +44,9 @@ private Q_SLOTS:
     void resultTypeConstructionAndVersionSchemaRemainStable();
     void resultCapabilitySignaturesRemainStable();
     void resultDriverAndFormatInformationSignaturesRemainStable();
+    void displayFilterTypeAndConstructionSchemaRemainStable();
+    void displayFilterShaderSignaturesRemainStable();
+    void displayFilterPixelTransformationSignaturesRemainStable();
 };
 
 void KisOpenGLModeProberSchemaContractTest::proberTypeConstructionAndStateSchemaRemainStable()
@@ -102,6 +122,42 @@ void KisOpenGLModeProberSchemaContractTest::resultDriverAndFormatInformationSign
     ASSERT_OPENGL_MODE_SIGNATURE(Result,
                                  xcbGlProviderProtocol,
                                  std::optional<KisOpenGL::XcbGLProviderProtocol> (Result::*)());
+}
+
+void KisOpenGLModeProberSchemaContractTest::displayFilterTypeAndConstructionSchemaRemainStable()
+{
+    using Filter = KisDisplayFilter;
+
+    static_assert(std::is_class_v<Filter>);
+    static_assert(std::is_base_of_v<QObject, Filter>);
+    static_assert(std::is_base_of_v<KisDisplayColorFilter, Filter>);
+    static_assert(std::is_abstract_v<Filter>);
+    static_assert(std::is_default_constructible_v<DisplayFilterConstructionProbe>);
+    static_assert(std::is_constructible_v<DisplayFilterConstructionProbe, QObject *>);
+    static_assert(std::has_virtual_destructor_v<Filter>);
+}
+
+void KisOpenGLModeProberSchemaContractTest::displayFilterShaderSignaturesRemainStable()
+{
+    using Filter = KisDisplayFilter;
+
+    ASSERT_OPENGL_MODE_SIGNATURE(Filter, program, QString (Filter::*)() const);
+    ASSERT_OPENGL_MODE_SIGNATURE(Filter, setupTextures, void (Filter::*)(GLFunctions *, QOpenGLShaderProgram *) const);
+    ASSERT_OPENGL_MODE_SIGNATURE(Filter, updateShader, bool (Filter::*)());
+}
+
+void KisOpenGLModeProberSchemaContractTest::displayFilterPixelTransformationSignaturesRemainStable()
+{
+    using Filter = KisDisplayFilter;
+
+    ASSERT_OPENGL_MODE_SIGNATURE(Filter, filter, void (Filter::*)(quint8 *, quint32));
+    ASSERT_OPENGL_MODE_SIGNATURE(Filter, approximateInverseTransformation, void (Filter::*)(quint8 *, quint32));
+    ASSERT_OPENGL_MODE_SIGNATURE(Filter, approximateForwardTransformation, void (Filter::*)(quint8 *, quint32));
+    ASSERT_OPENGL_MODE_SIGNATURE(Filter, useInternalColorManagement, bool (Filter::*)() const);
+    ASSERT_OPENGL_MODE_SIGNATURE(Filter,
+                                 correctionInterface,
+                                 KisExposureGammaCorrectionInterface * (Filter::*)() const);
+    ASSERT_OPENGL_MODE_SIGNATURE(Filter, lockCurrentColorVisualRepresentation, bool (Filter::*)() const);
 }
 
 #undef ASSERT_OPENGL_MODE_SIGNATURE
