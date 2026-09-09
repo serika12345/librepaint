c16f0e06d2a8eca4ecece4e3ef7eca584fe21524
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-09 10:42:35 +0900

    Fix cage transform worker API contracts

--- libs/image/tests/KisTransformWorkerSchemaContractTest.cpp
+++ libs/image/tests/KisTransformWorkerSchemaContractTest.cpp
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "kis_cage_transform_worker.h"
 #include "kis_transform_worker.h"
 
 #include <QTest>
@@ -13,6 +14,8 @@ namespace
 {
 #define ASSERT_TRANSFORM_WORKER_SIGNATURE(method, signature)                                                           \
     static_assert(std::is_same_v<decltype(static_cast<signature>(&KisTransformWorker::method)), signature>)
+#define ASSERT_CAGE_TRANSFORM_WORKER_SIGNATURE(method, signature)                                                      \
+    static_assert(std::is_same_v<decltype(static_cast<signature>(&KisCageTransformWorker::method)), signature>)
 } // namespace
 
 class KisTransformWorkerSchemaContractTest : public QObject
@@ -25,6 +28,9 @@ private Q_SLOTS:
     void offsetAndExecutionSignaturesRemainStable();
     void transformAndSelectionSignaturesRemainStable();
     void subpixelTranslationSignaturesRemainStable();
+    void cageTypeConstructionAndLifetimeSchemaRemainStable();
+    void cageSetupAndExecutionSignaturesRemainStable();
+    void cageRectangleAndImageSignaturesRemainStable();
 };
 
 void KisTransformWorkerSchemaContractTest::typeConstructionAndLifetimeSchemaRemainStable()
@@ -82,7 +88,42 @@ void KisTransformWorkerSchemaContractTest::subpixelTranslationSignaturesRemainSt
     ASSERT_TRANSFORM_WORKER_SIGNATURE(setForceSubPixelTranslation, void (Worker::*)(bool));
 }
 
+void KisTransformWorkerSchemaContractTest::cageTypeConstructionAndLifetimeSchemaRemainStable()
+{
+    using Worker = KisCageTransformWorker;
+
+    static_assert(std::is_class_v<Worker>);
+    static_assert(std::is_constructible_v<Worker, const QRect &, const QVector<QPointF> &, KoUpdater *>);
+    static_assert(std::is_constructible_v<Worker, const QRect &, const QVector<QPointF> &, KoUpdater *, int>);
+    static_assert(
+        std::is_constructible_v<Worker, const QImage &, const QPointF &, const QVector<QPointF> &, KoUpdater *>);
+    static_assert(
+        std::is_constructible_v<Worker, const QImage &, const QPointF &, const QVector<QPointF> &, KoUpdater *, int>);
+    static_assert(std::is_destructible_v<Worker>);
+
+    QVERIFY(true);
+}
+
+void KisTransformWorkerSchemaContractTest::cageSetupAndExecutionSignaturesRemainStable()
+{
+    using Worker = KisCageTransformWorker;
+
+    ASSERT_CAGE_TRANSFORM_WORKER_SIGNATURE(prepareTransform, void (Worker::*)());
+    ASSERT_CAGE_TRANSFORM_WORKER_SIGNATURE(setTransformedCage, void (Worker::*)(const QVector<QPointF> &));
+    ASSERT_CAGE_TRANSFORM_WORKER_SIGNATURE(run, void (Worker::*)(KisPaintDeviceSP, KisPaintDeviceSP));
+}
+
+void KisTransformWorkerSchemaContractTest::cageRectangleAndImageSignaturesRemainStable()
+{
+    using Worker = KisCageTransformWorker;
+
+    ASSERT_CAGE_TRANSFORM_WORKER_SIGNATURE(approxChangeRect, QRect (Worker::*)(const QRect &));
+    ASSERT_CAGE_TRANSFORM_WORKER_SIGNATURE(approxNeedRect, QRect (Worker::*)(const QRect &, const QRect &));
+    ASSERT_CAGE_TRANSFORM_WORKER_SIGNATURE(runOnQImage, QImage (Worker::*)(QPointF *));
+}
+
 #undef ASSERT_TRANSFORM_WORKER_SIGNATURE
+#undef ASSERT_CAGE_TRANSFORM_WORKER_SIGNATURE
 
 QTEST_GUILESS_MAIN(KisTransformWorkerSchemaContractTest)
 
