a2ac4cac0ea975ef99ae6d70aa51244420ded30d
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-12 21:12:19 +0900

    dab cacheの公開契約を固定

--- plugins/paintops/libpaintop/tests/KisDabCacheBaseSchemaContractTest.cpp
+++ plugins/paintops/libpaintop/tests/KisDabCacheBaseSchemaContractTest.cpp
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "kis_dab_cache.h"
 #include "kis_dab_cache_base.h"
 
 #include <QTest>
@@ -17,6 +18,9 @@ private Q_SLOTS:
     void typeConstructionAndLifetimeRemainStable();
     void optionConfigurationSignaturesRemainStable();
     void cacheBehaviorControlSignaturesRemainStable();
+    void dabCacheTypeConstructionAndLifetimeSchemaRemainStable();
+    void dabCacheFetchSignaturesRemainStable();
+    void dabCachePostprocessingSignaturesRemainStable();
 };
 
 void KisDabCacheBaseSchemaContractTest::typeConstructionAndLifetimeRemainStable()
@@ -45,6 +49,61 @@ void KisDabCacheBaseSchemaContractTest::cacheBehaviorControlSignaturesRemainStab
     static_assert(std::is_same_v<decltype(&Cache::disableSubpixelPrecision), void (Cache::*)()>);
     static_assert(std::is_same_v<decltype(&Cache::needSeparateOriginal),
                                  bool (Cache::*)(KisTextureOption *, KisSharpnessOption *) const>);
+
+    QVERIFY(true);
+}
+
+void KisDabCacheBaseSchemaContractTest::dabCacheTypeConstructionAndLifetimeSchemaRemainStable()
+{
+    using Cache = KisDabCache;
+
+    static_assert(std::is_class_v<Cache>);
+    static_assert(std::is_constructible_v<Cache, KisBrushSP>);
+    static_assert(std::is_destructible_v<Cache>);
+
+    QVERIFY(true);
+}
+
+void KisDabCacheBaseSchemaContractTest::dabCacheFetchSignaturesRemainStable()
+{
+    using Cache = KisDabCache;
+    using ColorSourceFetch = KisFixedPaintDeviceSP (Cache::*)(const KoColorSpace *,
+                                                              KisColorSource *,
+                                                              const QPointF &,
+                                                              const KisDabShape &,
+                                                              const KisPaintInformation &,
+                                                              qreal,
+                                                              QRect *,
+                                                              qreal);
+    using ColorFetch = KisFixedPaintDeviceSP (Cache::*)(const KoColorSpace *,
+                                                        const KoColor &,
+                                                        const QPointF &,
+                                                        const KisDabShape &,
+                                                        const KisPaintInformation &,
+                                                        qreal,
+                                                        QRect *,
+                                                        qreal);
+    using NormalizedFetch = KisFixedPaintDeviceSP (Cache::*)(const KoColorSpace *,
+                                                             const QPointF &,
+                                                             const KisDabShape &,
+                                                             const KisPaintInformation &,
+                                                             qreal,
+                                                             QRect *);
+
+    static_assert(std::is_same_v<decltype(static_cast<ColorSourceFetch>(&Cache::fetchDab)), ColorSourceFetch>);
+    static_assert(std::is_same_v<decltype(static_cast<ColorFetch>(&Cache::fetchDab)), ColorFetch>);
+    static_assert(std::is_same_v<decltype(&Cache::fetchNormalizedImageDab), NormalizedFetch>);
+
+    QVERIFY(true);
+}
+
+void KisDabCacheBaseSchemaContractTest::dabCachePostprocessingSignaturesRemainStable()
+{
+    using Cache = KisDabCache;
+
+    static_assert(std::is_same_v<decltype(&Cache::setSharpnessPostprocessing), void (Cache::*)(KisSharpnessOption *)>);
+    static_assert(std::is_same_v<decltype(&Cache::setTexturePostprocessing), void (Cache::*)(KisTextureOption *)>);
+    static_assert(std::is_same_v<decltype(&Cache::needSeparateOriginal), bool (Cache::*)() const>);
 
     QVERIFY(true);
 }
