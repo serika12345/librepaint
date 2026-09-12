d00e5ea8113f4580e88b71a0941264906bde3869
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-13 05:46:14 +0900

    test: cover Android splash APIs

--- libs/ui/tests/KisCursorOverrideHijackerSchemaContractTest.cpp
+++ libs/ui/tests/KisCursorOverrideHijackerSchemaContractTest.cpp
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "application/ui/workspace/KisAndroidSplash.h"
 #include "events/kis_cursor_override_hijacker.h"
 
 #include <QTest>
@@ -15,6 +16,7 @@ class KisCursorOverrideHijackerSchemaContractTest : public QObject
 
 private Q_SLOTS:
     void cursorOverrideHijackerTypeConstructionAndLifetimeSchemaRemainStable();
+    void androidSplashTypeAndStaticApiSchemaRemainStable();
 };
 
 void KisCursorOverrideHijackerSchemaContractTest::cursorOverrideHijackerTypeConstructionAndLifetimeSchemaRemainStable()
@@ -24,6 +26,19 @@ void KisCursorOverrideHijackerSchemaContractTest::cursorOverrideHijackerTypeCons
     static_assert(std::is_class_v<Hijacker>);
     static_assert(std::is_default_constructible_v<Hijacker>);
     static_assert(std::is_destructible_v<Hijacker>);
+}
+
+void KisCursorOverrideHijackerSchemaContractTest::androidSplashTypeAndStaticApiSchemaRemainStable()
+{
+    using Splash = KisAndroidSplash;
+
+    static_assert(std::is_class_v<Splash>);
+    static_assert(std::is_base_of_v<QObject, Splash>);
+    static_assert(std::is_same_v<decltype(&Splash::instance), Splash *(*)()>);
+    static_assert(std::is_same_v<decltype(&Splash::show), void (*)()>);
+    static_assert(std::is_same_v<decltype(&Splash::setLoaded), void (*)(bool)>);
+    static_assert(std::is_same_v<decltype(&Splash::setLoadingText), void (*)(const QString &)>);
+    static_assert(std::is_same_v<decltype(&Splash::sigSplashDialogDismissed), void (Splash::*)()>);
 }
 
 QTEST_APPLESS_MAIN(KisCursorOverrideHijackerSchemaContractTest)
