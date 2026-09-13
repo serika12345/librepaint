c4f6931e7096a505ff1c2d6d27b8ea49f38bd162
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-13 16:32:50 +0900

    test: cover lazy transform mask keyframes command API

---
+++ libs/image/tests/KisLazyCreateTransformMaskKeyframesCommandSchemaContractTest.cpp
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2026 LibrePaint contributors
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include "KisLazyCreateTransformMaskKeyframesCommand.h"
+
+#include <QTest>
+
+#include <type_traits>
+
+class KisLazyCreateTransformMaskKeyframesCommandSchemaContractTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void lazyCreateTransformMaskKeyframesCommandPublicInterfaceRemainsStable();
+};
+
+void KisLazyCreateTransformMaskKeyframesCommandSchemaContractTest::
+    lazyCreateTransformMaskKeyframesCommandPublicInterfaceRemainsStable()
+{
+    using Command = KisLazyCreateTransformMaskKeyframesCommand;
+    using HasAnimation = bool (*)(KisTransformMaskSP);
+
+    static_assert(std::is_base_of_v<KisCommandUtils::AggregateCommand, Command>);
+    static_assert(std::is_constructible_v<Command, KisTransformMaskSP, KUndo2Command *>);
+    static_assert(std::is_same_v<decltype(&Command::maskHasAnimation), HasAnimation>);
+}
+
+QTEST_GUILESS_MAIN(KisLazyCreateTransformMaskKeyframesCommandSchemaContractTest)
+
+#include "KisLazyCreateTransformMaskKeyframesCommandSchemaContractTest.moc"
