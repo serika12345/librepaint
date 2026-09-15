829f00c9ef10dbc4349ab8d97c7b4118621425fe
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-15 14:25:05 +0900

    test: cover guides decoration API

--- libs/ui/tests/KisReferenceImagesDecorationSchemaContractTest.cpp
+++ libs/ui/tests/KisReferenceImagesDecorationSchemaContractTest.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "KisReferenceImagesDecoration.h"
+#include "kis_guides_decoration.h"
 
 #include <QTest>
 
@@ -15,6 +16,7 @@ class KisReferenceImagesDecorationSchemaContractTest : public QObject
 
 private Q_SLOTS:
     void referenceImagesDecorationSchemaRemainStable();
+    void guidesDecorationSchemaRemainStable();
 };
 
 void KisReferenceImagesDecorationSchemaContractTest::referenceImagesDecorationSchemaRemainStable()
@@ -28,6 +30,21 @@ void KisReferenceImagesDecorationSchemaContractTest::referenceImagesDecorationSc
     static_assert(std::has_virtual_destructor_v<Decoration>);
     static_assert(std::is_same_v<decltype(&Decoration::addReferenceImage), void (Decoration::*)(KisReferenceImage *)>);
     static_assert(std::is_same_v<decltype(&Decoration::documentHasReferenceImages), bool (Decoration::*)() const>);
+}
+
+void KisReferenceImagesDecorationSchemaContractTest::guidesDecorationSchemaRemainStable()
+{
+    using Decoration = KisGuidesDecoration;
+
+    static_assert(std::is_class_v<Decoration>);
+    static_assert(std::is_base_of_v<KisCanvasDecoration, Decoration>);
+    static_assert(std::is_constructible_v<Decoration, QPointer<KisView>>);
+    static_assert(std::has_virtual_destructor_v<Decoration>);
+    static_assert(
+        std::is_same_v<decltype(&Decoration::setGuidesConfig), void (Decoration::*)(const KisGuidesConfig &)>);
+    static_assert(std::is_same_v<decltype(&Decoration::guidesConfig), const KisGuidesConfig &(Decoration::*)() const>);
+
+    QCOMPARE(GUIDES_DECORATION_ID, QStringLiteral("guides-decoration"));
 }
 
 QTEST_GUILESS_MAIN(KisReferenceImagesDecorationSchemaContractTest)
