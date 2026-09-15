d37bea3ca4601d91715197a8b1dea14c59c212f7
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-15 14:34:11 +0900

    test: cover image thumbnail stroke strategy API

--- libs/ui/tests/KisReferenceImagesDecorationSchemaContractTest.cpp
+++ libs/ui/tests/KisReferenceImagesDecorationSchemaContractTest.cpp
@@ -3,12 +3,25 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
+#include "KisImageThumbnailStrokeStrategy.h"
 #include "KisReferenceImagesDecoration.h"
 #include "kis_guides_decoration.h"
 
 #include <QTest>
 
 #include <type_traits>
+
+namespace
+{
+class ThumbnailStrategyProbe final : public KisImageThumbnailStrokeStrategyBase
+{
+public:
+    using KisImageThumbnailStrokeStrategyBase::KisImageThumbnailStrokeStrategyBase;
+
+private:
+    void reportThumbnailGenerationCompleted(KisPaintDeviceSP, const QRect &) override;
+};
+} // namespace
 
 class KisReferenceImagesDecorationSchemaContractTest : public QObject
 {
@@ -17,6 +30,7 @@ class KisReferenceImagesDecorationSchemaContractTest : public QObject
 private Q_SLOTS:
     void referenceImagesDecorationSchemaRemainStable();
     void guidesDecorationSchemaRemainStable();
+    void imageThumbnailStrokeStrategySchemaRemainStable();
 };
 
 void KisReferenceImagesDecorationSchemaContractTest::referenceImagesDecorationSchemaRemainStable()
@@ -45,6 +59,27 @@ void KisReferenceImagesDecorationSchemaContractTest::guidesDecorationSchemaRemai
     static_assert(std::is_same_v<decltype(&Decoration::guidesConfig), const KisGuidesConfig &(Decoration::*)() const>);
 
     QCOMPARE(GUIDES_DECORATION_ID, QStringLiteral("guides-decoration"));
+}
+
+void KisReferenceImagesDecorationSchemaContractTest::imageThumbnailStrokeStrategySchemaRemainStable()
+{
+    using Base = KisImageThumbnailStrokeStrategyBase;
+    using Strategy = KisImageThumbnailStrokeStrategy;
+
+    static_assert(std::is_class_v<Base>);
+    static_assert(std::is_base_of_v<KisIdleTaskStrokeStrategy, Base>);
+    static_assert(std::is_constructible_v<ThumbnailStrategyProbe,
+                                          KisPaintDeviceSP,
+                                          const QRect &,
+                                          const QSize &,
+                                          bool,
+                                          const KoColorProfile *,
+                                          KoColorConversionTransformation::Intent,
+                                          KoColorConversionTransformation::ConversionFlags>);
+    static_assert(std::has_virtual_destructor_v<Base>);
+    static_assert(std::is_class_v<Strategy>);
+    static_assert(std::is_base_of_v<Base, Strategy>);
+    static_assert(std::is_same_v<decltype(&Strategy::thumbnailUpdated), void (Strategy::*)(QImage)>);
 }
 
 QTEST_GUILESS_MAIN(KisReferenceImagesDecorationSchemaContractTest)
