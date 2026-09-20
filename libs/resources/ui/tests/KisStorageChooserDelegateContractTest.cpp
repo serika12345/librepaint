f54d521b7207dbd4313af39f2fe7a1d7feab2eea
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-20 14:24:32 +0900

    Focus storage chooser rendering contracts

--- libs/resources/ui/tests/KisStorageChooserDelegateContractTest.cpp
+++ libs/resources/ui/tests/KisStorageChooserDelegateContractTest.cpp
@@ -8,96 +8,51 @@
 #include <KisStorageModel.h>
 
 #include <QApplication>
-#include <QIcon>
+#include <QImage>
 #include <QPainter>
-#include <QPointer>
-#include <QProxyStyle>
 #include <QStandardItemModel>
-#include <QStyleFactory>
+#include <QStyleOptionViewItem>
 #include <QTest>
 
-namespace KisIconUtils
+namespace
 {
-QIcon loadIcon(const QString &)
+QStyleOptionViewItem delegateOption()
 {
-    return QIcon();
+    QStyleOptionViewItem option;
+    option.rect = QRect(10, 10, 220, 60);
+    option.decorationSize = QSize(32, 32);
+    option.palette = qApp->palette();
+    option.state = QStyle::State_Enabled;
+    return option;
+}
+
+QImage paintStorage(KisStorageChooserDelegate &delegate,
+                    const QStyleOptionViewItem &option,
+                    const QModelIndex &index)
+{
+    QImage canvas(240, 80, QImage::Format_ARGB32_Premultiplied);
+    canvas.fill(Qt::transparent);
+    QPainter painter(&canvas);
+    delegate.paint(&painter, option, index);
+    return canvas;
 }
 }
-
-class RecordingStyle : public QProxyStyle
-{
-public:
-    RecordingStyle()
-        : QProxyStyle(QStyleFactory::create(QStringLiteral("Fusion")))
-    {
-    }
-
-    void reset() const
-    {
-        panelDraws = 0;
-        checkboxDraws = 0;
-        panelState = {};
-        checkboxState = {};
-    }
-
-    void drawPrimitive(PrimitiveElement element,
-                       const QStyleOption *option,
-                       QPainter *painter,
-                       const QWidget *widget = nullptr) const override
-    {
-        if (element == QStyle::PE_PanelButtonTool) {
-            ++panelDraws;
-            panelState = option->state;
-        }
-        QProxyStyle::drawPrimitive(element, option, painter, widget);
-    }
-
-    void drawControl(ControlElement element,
-                     const QStyleOption *option,
-                     QPainter *painter,
-                     const QWidget *widget = nullptr) const override
-    {
-        if (element == QStyle::CE_CheckBox) {
-            ++checkboxDraws;
-            checkboxState = option->state;
-        }
-        QProxyStyle::drawControl(element, option, painter, widget);
-    }
-
-    mutable int panelDraws = 0;
-    mutable int checkboxDraws = 0;
-    mutable QStyle::State panelState;
-    mutable QStyle::State checkboxState;
-};
 
 class KisStorageChooserDelegateContractTest : public QObject
 {
     Q_OBJECT
 
 private Q_SLOTS:
-    void initTestCase();
-    void constructorUsesParentOwnership();
-    void sizeHintUsesFixedWidthAndDecorationHeight();
-    void paintSkipsInvalidIndexesAndDrawsStorageState();
+    void providesStorageCellDimensions();
+    void paintsThumbnailAndActivationState();
 };
 
-void KisStorageChooserDelegateContractTest::initTestCase()
+void KisStorageChooserDelegateContractTest::providesStorageCellDimensions()
 {
-    QApplication::setStyle(new RecordingStyle);
-}
-
-void KisStorageChooserDelegateContractTest::constructorUsesParentOwnership()
-{
-    QObject *parent = new QObject;
-    QPointer<KisStorageChooserDelegate> delegate = new KisStorageChooserDelegate(parent);
-
-    QCOMPARE(delegate->parent(), parent);
-    delete parent;
-    QVERIFY(delegate.isNull());
-}
-
-void KisStorageChooserDelegateContractTest::sizeHintUsesFixedWidthAndDecorationHeight()
-{
+    // Consumer: Resource chooser users opening the storage selection popup.
+    // Operation: The popup asks its delegate for the cell size of a storage entry.
+    // Observable result: The storage name and activation control receive a 200-pixel-wide cell below its preview.
+    // Failure impact: Storage entries are clipped or overlap in the chooser popup.
     KisStorageChooserDelegate delegate;
     QStyleOptionViewItem option;
     option.decorationSize = QSize(64, 32);
@@ -105,26 +60,14 @@ void KisStorageChooserDelegateContractTest::sizeHintUsesFixedWidthAndDecorationH
     QCOMPARE(delegate.sizeHint(option, QModelIndex()), QSize(200, 40));
 }
 
-void KisStorageChooserDelegateContractTest::paintSkipsInvalidIndexesAndDrawsStorageState()
+void KisStorageChooserDelegateContractTest::paintsThumbnailAndActivationState()
 {
-    auto *style = dynamic_cast<RecordingStyle *>(QApplication::style());
-    QVERIFY(style);
+    // Consumer: Resource manager users enabling or disabling a storage.
+    // Operation: The storage popup paints the same storage while inactive and active.
+    // Observable result: The thumbnail remains visible and the cell rendering changes with the activation state.
+    // Failure impact: Users cannot tell whether a bundle or folder is active before choosing resources from it.
     KisStorageChooserDelegate delegate;
-    QStyleOptionViewItem option;
-    option.rect = QRect(10, 10, 220, 60);
-    option.decorationSize = QSize(32, 32);
-    option.palette = QApplication::palette();
-    option.state = QStyle::State_Enabled;
-    QImage canvas(240, 80, QImage::Format_ARGB32_Premultiplied);
-    canvas.fill(Qt::transparent);
-    QPainter painter(&canvas);
-
-    style->reset();
-    delegate.paint(&painter, option, QModelIndex());
-    QCOMPARE(style->panelDraws, 0);
-    QCOMPARE(style->checkboxDraws, 0);
-    QCOMPARE(canvas.pixelColor(14, 14), QColor(Qt::transparent));
-
+    const QStyleOptionViewItem option = delegateOption();
     QStandardItemModel model(1, KisStorageModel::DisplayName + 1);
     const QModelIndex index = model.index(0, 0);
     QImage thumbnail(4, 4, QImage::Format_ARGB32);
@@ -132,29 +75,18 @@ void KisStorageChooserDelegateContractTest::paintSkipsInvalidIndexesAndDrawsStor
     model.setData(model.index(0, KisStorageModel::DisplayName),
                   QStringLiteral("Bundle_Name"),
                   Qt::DisplayRole);
-    model.setData(model.index(0, KisStorageModel::Location),
-                  QStringLiteral("bundle.asl"),
-                  Qt::DisplayRole);
     model.setData(index, QStringLiteral("Bundle"),
                   Qt::UserRole + KisStorageModel::StorageType);
     model.setData(index, thumbnail, Qt::UserRole + KisStorageModel::Thumbnail);
-    model.setData(index, false, Qt::UserRole + KisStorageModel::Active);
 
-    style->reset();
-    delegate.paint(&painter, option, index);
-    QCOMPARE(style->panelDraws, 1);
-    QVERIFY(!style->panelState.testFlag(QStyle::State_Sunken));
-    QCOMPARE(style->checkboxDraws, 1);
-    QVERIFY(style->checkboxState.testFlag(QStyle::State_Off));
-    QCOMPARE(canvas.pixelColor(14, 14), QColor(Qt::red));
+    model.setData(index, false, Qt::UserRole + KisStorageModel::Active);
+    const QImage inactive = paintStorage(delegate, option, index);
+    QCOMPARE(inactive.pixelColor(14, 14), QColor(Qt::red));
 
     model.setData(index, true, Qt::UserRole + KisStorageModel::Active);
-    style->reset();
-    delegate.paint(&painter, option, index);
-    QCOMPARE(style->panelDraws, 1);
-    QVERIFY(style->panelState.testFlag(QStyle::State_Sunken));
-    QCOMPARE(style->checkboxDraws, 1);
-    QVERIFY(style->checkboxState.testFlag(QStyle::State_On));
+    const QImage active = paintStorage(delegate, option, index);
+    QCOMPARE(active.pixelColor(14, 14), QColor(Qt::red));
+    QVERIFY(active != inactive);
 }
 
 QTEST_MAIN(KisStorageChooserDelegateContractTest)
