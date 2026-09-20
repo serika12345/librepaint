387326b05abd8a7dab6f1679908c6f13ad0410b2
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-09-20 13:19:34 +0900

    Focus local stroke resource resolution

--- libs/resources/tests/KisLocalStrokeResourcesContractTest.cpp
+++ libs/resources/tests/KisLocalStrokeResourcesContractTest.cpp
@@ -7,12 +7,8 @@
 
 #include <QTest>
 
-#include <memory>
-
 namespace
 {
-int safeAssertCount = 0;
-
 class TestResource final : public KoResource
 {
 public:
@@ -66,7 +62,7 @@ KoResourceSP makeResource(const QString &type,
 
 void kis_safe_assert_recoverable(const char *, const char *, int)
 {
-    ++safeAssertCount;
+    qFatal("unexpected recoverable assertion");
 }
 
 void kis_assert_exception(const char *assertion, const char *file, int line)
@@ -79,83 +75,8 @@ class KisLocalStrokeResourcesContractTest : public QObject
     Q_OBJECT
 
 private Q_SLOTS:
-    void startsEmptyAndSanitizesInitialResources();
-    void addsAndRemovesResources();
-    void clonesAnIndependentContainerWithSharedResources();
     void providesLiveTypeFilteredSources();
 };
-
-void KisLocalStrokeResourcesContractTest::startsEmptyAndSanitizesInitialResources()
-{
-    safeAssertCount = 0;
-    KisLocalStrokeResources empty;
-    QVERIFY(empty.resources().isEmpty());
-
-    KoResourceSP first = makeResource(QStringLiteral("brushes"),
-                                      QStringLiteral("first-md5"),
-                                      QStringLiteral("first.res"),
-                                      QStringLiteral("First"));
-    KoResourceSP second = makeResource(QStringLiteral("presets"),
-                                       QStringLiteral("second-md5"),
-                                       QStringLiteral("second.res"),
-                                       QStringLiteral("Second"));
-    KisLocalStrokeResources initialized({first, KoResourceSP(), second});
-
-    QCOMPARE(safeAssertCount, 1);
-    QCOMPARE(initialized.resources(), QList<KoResourceSP>({first, second}));
-}
-
-void KisLocalStrokeResourcesContractTest::addsAndRemovesResources()
-{
-    safeAssertCount = 0;
-    KoResourceSP first = makeResource(QStringLiteral("brushes"),
-                                      QStringLiteral("first-md5"),
-                                      QStringLiteral("first.res"),
-                                      QStringLiteral("First"));
-    KoResourceSP second = makeResource(QStringLiteral("brushes"),
-                                       QStringLiteral("second-md5"),
-                                       QStringLiteral("second.res"),
-                                       QStringLiteral("Second"));
-    KisLocalStrokeResources resources;
-
-    resources.addResource(first);
-    resources.addResource(second);
-    resources.addResource(first);
-    QCOMPARE(resources.resources(), QList<KoResourceSP>({first, second, first}));
-
-    resources.removeResource(first);
-    QCOMPARE(resources.resources(), QList<KoResourceSP>({second}));
-    resources.removeResource(first);
-    QCOMPARE(resources.resources(), QList<KoResourceSP>({second}));
-
-    resources.addResource(KoResourceSP());
-    QCOMPARE(safeAssertCount, 1);
-    QCOMPARE(resources.resources(), QList<KoResourceSP>({second}));
-}
-
-void KisLocalStrokeResourcesContractTest::clonesAnIndependentContainerWithSharedResources()
-{
-    KoResourceSP resource = makeResource(QStringLiteral("brushes"),
-                                         QStringLiteral("shared-md5"),
-                                         QStringLiteral("shared.res"),
-                                         QStringLiteral("Shared"));
-    KisLocalStrokeResources original({resource});
-    std::unique_ptr<KisLocalStrokeResources> clone(original.clone());
-
-    QVERIFY(clone);
-    QCOMPARE(clone->resources(), QList<KoResourceSP>({resource}));
-    clone->removeResource(resource);
-    QVERIFY(clone->resources().isEmpty());
-    QCOMPARE(original.resources(), QList<KoResourceSP>({resource}));
-
-    KoResourceSP cloneOnly = makeResource(QStringLiteral("brushes"),
-                                          QStringLiteral("clone-md5"),
-                                          QStringLiteral("clone.res"),
-                                          QStringLiteral("Clone"));
-    clone->addResource(cloneOnly);
-    QCOMPARE(clone->resources(), QList<KoResourceSP>({cloneOnly}));
-    QCOMPARE(original.resources(), QList<KoResourceSP>({resource}));
-}
 
 void KisLocalStrokeResourcesContractTest::providesLiveTypeFilteredSources()
 {
